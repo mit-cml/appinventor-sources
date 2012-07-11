072f1f7128f1c37bd274024e3ffdcf27918e7c0d
Sharon Perl <sharon@google>
2012-07-11 14:17:21 -0700

    Add project saved status message

--- appinventor/appengine/src/com/google/appinventor/client/explorer/commands/SaveAllEditorsCommand.java
+++ appinventor/appengine/src/com/google/appinventor/client/explorer/commands/SaveAllEditorsCommand.java
@@ -2,9 +2,14 @@
 
 package com.google.appinventor.client.explorer.commands;
 
+import com.google.appinventor.client.ErrorReporter;
 import com.google.appinventor.client.Ode;
+import static com.google.appinventor.client.Ode.MESSAGES;
 import com.google.appinventor.shared.rpc.project.ProjectNode;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.Command;
+
+import java.util.Date;
 
 /**
  * Command for saving all editors.
@@ -33,6 +38,8 @@ public class SaveAllEditorsCommand extends ChainableCommand {
     Ode.getInstance().getEditorManager().saveDirtyEditorsAndBlocksEditors(new Command() {
       @Override
       public void execute() {
+        ErrorReporter.reportInfo(MESSAGES.savedProject(
+            DateTimeFormat.getMediumDateTimeFormat().format(new Date())));
         executeNextCommand(node);
       }
     });
