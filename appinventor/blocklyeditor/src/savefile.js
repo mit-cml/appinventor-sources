9ee68b4b63626080e25ef6e356fea7b7a2d2c35e
Sharon Perl <sharon@google>
2012-07-12 07:42:28 -0700

    add missing javascript file

---
+++ appinventor/blocklyeditor/src/savefile.js
@@ -0,0 +1,28 @@
+// Copyright 2012 Massachusetts Institute of Technology. All rights reserved.
+
+/**
+ * @fileoverview Visual blocks editor for App Inventor
+ * Methods to handle serialization of the blocks workspace
+ * 
+ * @author sharon@google.com (Sharon Perl)
+ */
+
+Blockly.SaveFile = {};
+
+Blockly.SaveFile.load = function(blocksContent) {
+  // TODO(sharon): deal with errors
+  // Deal with old codeblocks code, in a YACodeBlocks element (if present)
+  if (blocksContent.length != 0) {
+    var xml = Blockly.Xml.textToDom(blocksContent);
+    var firstChild = xml.childNodes[0];
+    if (firstChild.nodeName && firstChild.nodeName == 'YACodeBlocks') {
+      console.log("Warning: don't know how to convert old codeblocks blocks  yet. Ignoring!")
+    }
+    Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xml);
+  }
+};
+
+Blockly.SaveFile.get = function() {
+  return Blockly.Xml.domToPrettyText(Blockly.Xml.workspaceToDom(Blockly.mainWorkspace));
+};
+
