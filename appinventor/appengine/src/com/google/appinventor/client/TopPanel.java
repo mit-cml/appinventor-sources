adcdeca068bc646eacba7088c0edde40bb907fdf
halatmit <hal@mit.edu>
2012-11-04 16:14:07 -0500

    Add Gallery link to designer page
    Change-Id: Ice9d69516b1ec6e54800eabe95e736c53fd06e36

--- appinventor/appengine/src/com/google/appinventor/client/TopPanel.java
+++ appinventor/appengine/src/com/google/appinventor/client/TopPanel.java
@@ -11,7 +11,6 @@ import com.google.appinventor.client.boxes.MotdBox;
 import com.google.appinventor.common.version.AppInventorFeatures;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTML;
@@ -29,24 +28,28 @@ public class TopPanel extends Composite {
   private static final String KNOWN_ISSUES_LINK_URL =
       Ode.APP_INVENTOR_DOCS_URL + "/knownIssues.html";
   private static final String RELEASE_NOTES_LINK_URL =
-	  Ode.APP_INVENTOR_DOCS_URL + "/ReleaseNotes.html";
+      Ode.APP_INVENTOR_DOCS_URL + "/ReleaseNotes.html";
   private static final String KNOWN_ISSUES_LINK_AND_TEXT =
       "<a href=\"" + KNOWN_ISSUES_LINK_URL + "\" target=\"_blank\">known issues</a>" ;
   private static final String RELEASE_NOTES_LINK_AND_TEXT =
-	  "<a href=\"" + RELEASE_NOTES_LINK_URL + "\" target=\"_blank\">release notes</a>" ;
+      "<a href=\"" + RELEASE_NOTES_LINK_URL + "\" target=\"_blank\">release notes</a>" ;
+  private static final String GALLERY_LINK_AND_TEXT =
+      "<a href=\"http://gallery.appinventor.mit.edu\" target=\"_blank\">" +
+      "Try the App Inventor Community Gallery (Beta)</a>";
 
   private static final String LOGO_IMAGE_URL = "/images/logo.png";
 
   private final HTML userEmail = new HTML();
   private final VerticalPanel rightPanel;  // remember this so we can add MOTD later if needed
 
-  private final HTML welcome = new HTML("Welcome to the App Inventor beta preview release." +
-      " Be sure to check the list of " + KNOWN_ISSUES_LINK_AND_TEXT + " and " + RELEASE_NOTES_LINK_AND_TEXT + ".");
+  private final HTML welcome = new HTML("Welcome to the App Inventor beta release." +
+      " Be sure to check the list of " + KNOWN_ISSUES_LINK_AND_TEXT + " and " +
+      RELEASE_NOTES_LINK_AND_TEXT + ".  " + GALLERY_LINK_AND_TEXT);
 
 
     //This is an experimental version of App Inventor. "
     //      + "IT IS FOR TESTING ONLY, NOT FOR GENERAL USE! ");
-  
+
   private HTML divider() {
     return new HTML("<span class='linkdivider'>&nbsp;|&nbsp;</span>");
   }
@@ -60,7 +63,7 @@ public class TopPanel extends Composite {
      *
      *  +-- topPanel ------------------------------+
      *  |+-- logo --++--middleLinks--++--account--+|
-     *  ||          ||               ||            | 
+     *  ||          ||               ||            |
      *  |+----------++---------------++-----------+|
      *  +------------------------------------------+
      */
@@ -74,7 +77,7 @@ public class TopPanel extends Composite {
     account.setStyleName("ode-TopPanelAccount");
     account.add(userEmail);
     account.add(divider());
-    
+
     if (AppInventorFeatures.sendBugReports()) {
       HTML reportBugLink =
           new HTML("<a href='" + BugReport.getBugReportLink() + "' target='_blank'>" +
