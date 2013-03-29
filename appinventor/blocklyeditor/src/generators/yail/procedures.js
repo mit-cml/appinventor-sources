ea0bfe8f392910c01f12b08d4d9dd887ed5eef9c
Jeffrey I. Schiller <jis@mit.edu>
2013-03-29 00:44:01 -0400

    Minor bugfix for generating Yail for procedures with return, need to add return statement.
    Change-Id: I97014a61b0647a915bd9409ced81854049c40550

--- appinventor/blocklyeditor/src/generators/yail/procedures.js
+++ appinventor/blocklyeditor/src/generators/yail/procedures.js
@@ -45,7 +45,7 @@ Blockly.Yail.procedures_defnoreturn = function() {
 
 //call the do return in control category
 Blockly.Yail.procedures_do_then_return = function() {
-  Blockly.Yail.controls_do_then_return.call(this);
+  return Blockly.Yail.controls_do_then_return.call(this);
 }
 
 // Generator code for procedure call with return
