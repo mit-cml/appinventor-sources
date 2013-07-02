b97c5cc9706d9d42f1a8a6e43c705d923f9495ff
William-Byrne <Will2596@gmail.com>
2013-07-02 17:53:05 -0400

    I fixed the README for the unit test framework and added a new unit test for a test program called PictureCycle.
    Change-Id: Id4d379386f61359213327cda14e80102e8241a0b

--- appinventor/blocklyeditor/tests/com/google/appinventor/blocklyeditor/BlocklyEvalTest.java
+++ appinventor/blocklyeditor/tests/com/google/appinventor/blocklyeditor/BlocklyEvalTest.java
@@ -90,4 +90,18 @@ public class BlocklyEvalTest extends TestCase {
     assertEquals("true", result.toString());
   }
 
+  public void testPictureCycle() throws Exception {
+
+    String[] params = { "phantomjs", testpath + "/tests/com/google/appinventor/blocklyeditor/pictureCycleTest.js" };
+    String result = "";
+
+    try {
+      result = CodeBlocksProcessHelper.exec(params, true).trim();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    assertEquals("true", result.toString());
+  }
+
 }
