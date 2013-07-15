a6e9bfcd00506c918a1b62f16635d39885681b12
Jose Mª Martin Sanchez <xcitizen.team@gmail.com>
2013-07-15 13:07:03 +0200

    Create files.java

---
+++ appinventor/components/src/com/google/appinventor/components/runtime/files.java
@@ -0,0 +1,21 @@
+
+// @author xcitizen.team@gmail.com (José Mª Martín)
+
+
+package com.google.appinventor.components.runtime;
+
+import java.io.File; 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import java.io.BufferedReader;
+import android.widget.EditText;
+import com.google.appinventor.components.runtime.util.TextViewUtil;
+import com.google.appinventor.components.annotations.SimpleProperty;
+import java.util.zip.*;
+import java.io.*;
