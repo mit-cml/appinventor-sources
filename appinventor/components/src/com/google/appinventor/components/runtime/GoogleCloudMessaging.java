80900325d14b9482d2c9d8554864dd076e89a0bf
Pablo García Lázaro <pablomorpheo@gmail.com>
2013-07-22 14:35:20 +0200

    BUGFIX 27

--- appinventor/components/src/com/google/appinventor/components/runtime/GoogleCloudMessaging.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/GoogleCloudMessaging.java
@@ -368,7 +368,7 @@ public final class GoogleCloudMessaging extends AndroidNonvisibleComponent imple
 			 * */
 			
 			// Showing received message
-			EventDispatcher.dispatchEvent(this, "OnPush", newMessage)
+			EventDispatcher.dispatchEvent(this, "OnPush", newMessage);
 			//lblMessage.append(newMessage + "\n");			
 			///Toast.makeText(getApplicationContext(), "New Message: " + newMessage, Toast.LENGTH_LONG).show();
 			
