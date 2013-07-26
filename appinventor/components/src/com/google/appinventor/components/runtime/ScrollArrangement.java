9b8a21584e6d62af79baf2ca2e1f9e30c4faef22
Pablo García Lázaro <pablomorpheo@gmail.com>
2013-07-26 06:04:19 +0200

    si soy mas tonto reviento

--- appinventor/components/src/com/google/appinventor/components/runtime/ScrollArrangement.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/ScrollArrangement.java
@@ -101,7 +101,7 @@ public class ScrollArrangement extends AndroidViewComponent implements Component
   @Override
   public void setChildWidth(AndroidViewComponent component, int width) {
   
-    android.view.View view = (android.view.View)component;
+    android.view.View view = component.getView();
 	Object layoutParams = view.getLayoutParams();
     if (layoutParams instanceof android.widget.LinearLayout.LayoutParams) {
       android.widget.LinearLayout.LayoutParams linearLayoutParams = (android.widget.LinearLayout.LayoutParams) layoutParams;
@@ -133,7 +133,7 @@ public class ScrollArrangement extends AndroidViewComponent implements Component
   @Override
   public void setChildHeight(AndroidViewComponent component, int height) {
   
-  android.view.View view = (android.view.View)component;
+  android.view.View view = component.getView();
   Object layoutParams = view.getLayoutParams();
     if (layoutParams instanceof android.widget.LinearLayout.LayoutParams) {
       android.widget.LinearLayout.LayoutParams linearLayoutParams = (android.widget.LinearLayout.LayoutParams) layoutParams;
