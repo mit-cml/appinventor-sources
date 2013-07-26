7a3a3d2ea5e2e15e1b8451bc6435298cdff21fba
Pablo García Lázaro <pablomorpheo@gmail.com>
2013-07-26 13:05:05 +0200

    bugfix admob

--- appinventor/buildserver/src/com/google/appinventor/buildserver/Compiler.java
+++ appinventor/buildserver/src/com/google/appinventor/buildserver/Compiler.java
@@ -458,8 +458,7 @@ public final class Compiler {
 	  if (componentTypes.contains("AdMob")) {
         //System.out.println("Android Manifest: including GCM <receiver>&<service> tag");
         out.write(
-            "<activity android:name=\"com.google.ads.AdActivity\" \n" +
-            "android:configChanges=\"keyboard|keyboardHidden|orientation|screenLayout|uiMode|screenSize|smallestScreenSize\"/> \n");
+            "<activity android:name=\"com.google.ads.AdActivity\" /> \n");
       }
 	  
 	  
