55157f1eafa95fb7bbadec305ec44922fee495ba
Pablo García Lázaro <pablomorpheo@gmail.com>
2013-07-28 05:59:46 +0200

    cleanup this thing

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockScrollArrangement.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockScrollArrangement.java
@@ -28,6 +28,7 @@ import com.google.gwt.user.client.ui.AbsolutePanel;
  * Mock HorizontalArrangement component.
  *
  * @author sharon@google.com (Sharon Perl)
+ * @author pablomorpheo@gmail.com  (Pabloko)
  */
 public final class MockScrollArrangement extends MockContainer {
 
@@ -50,24 +51,13 @@ public final class MockScrollArrangement extends MockContainer {
   
   private boolean initialized = false;
   
-  /**
-   * Creates a new MockHorizontalArrangement component.
-   *
-   * @param editor  editor of source file the component belongs to
-   */
+  
   public MockScrollArrangement(SimpleEditor editor) {
   
     super(editor, TYPE, images.horizontal(), MockHVArrangementHelper.makeLayout(0));
-    // Note(hal): There better not be any calls to MockHVArrangementHelper before the
-    // next instruction.  Note that the Helper methods are synchronized to avoid possible
-    // future problems if we ever have threads creating arrangements in parallel.
+
     myLayout = MockHVArrangementHelper.getLayout();
-        
-    /*if (orientation != ComponentConstants.LAYOUT_ORIENTATION_VERTICAL &&
-        orientation != ComponentConstants.LAYOUT_ORIENTATION_HORIZONTAL) {
-      throw new IllegalArgumentException("Illegal orientation: " + orientation);
-    }
-*/
+
     rootPanel.setHeight("100%");
 
     layoutWidget = new AbsolutePanel();
@@ -82,7 +72,7 @@ public final class MockScrollArrangement extends MockContainer {
       OdeLog.log(MESSAGES.badAlignmentPropertyEditorForArrangement());
       return;
     };
-    enableAndDisableDropdowns();
+
     initialized = true;
   }
   
@@ -95,33 +85,8 @@ public final class MockScrollArrangement extends MockContainer {
     } else if (propertyName.equals(PROPERTY_NAME_VERTICAL_ALIGNMENT)) {
       myLayout.setVAlignmentFlags(newValue);
       refreshForm();
-    } else {
-      if (propertyName.equals(PROPERTY_NAME_WIDTH) || propertyName.equals(PROPERTY_NAME_HEIGHT)) {
-        adjustAlignmentDropdowns();
-        refreshForm();
-      }
-    }
+    } 
+    
   }
-
-  // enableAndDisable It should not be called until the component is initialized.
-  // Otherwise, we'll get NPEs in trying to use myAlignmentPropertyEditor.
-  private void adjustAlignmentDropdowns() {
-    if (initialized) enableAndDisableDropdowns();
-  }
-
-  // If the width is automatic, the selector for horizontal alignment should be disabled.  
-  // If the length is automatic, the selector for vertical alignment should be disabled.
-  private void enableAndDisableDropdowns() {
-    /*String width = properties.getProperty(MockVisibleComponent.PROPERTY_NAME_WIDTH).getValue();
-    if (width.equals(YoungAndroidLengthPropertyEditor.CONST_AUTOMATIC)) {
-      myHAlignmentPropertyEditor.disable();
-    } else myHAlignmentPropertyEditor.enable();
-
-    String height = properties.getProperty(MockVisibleComponent.PROPERTY_NAME_HEIGHT).getValue();
-    if (height.equals(YoungAndroidLengthPropertyEditor.CONST_AUTOMATIC)) {
-      myVAlignmentPropertyEditor.disable();
-    } else myVAlignmentPropertyEditor.enable();*/
-  }
-
-
+    
 }
