1b0dd87758e6e2b662c9a477079ab712d4db2d81
wolberd <wolberd@gmail.com>
2013-10-30 20:36:49 -0400

    A fix to translator code for component set-get and some commenting.
    Change-Id: Ie893ba363475e2823bc71e2111235b8f86313b9e

--- appinventor/blocklyeditor/src/versioning/017_blocksOverhaul.js
+++ appinventor/blocklyeditor/src/versioning/017_blocksOverhaul.js
@@ -18,12 +18,14 @@ Blockly.Versioning.blocksOverhaul = function(xmlFromFile) {
     var blockElem=blocks[i];
     var blockType = blockElem.getAttribute('type');
     // all built-in blocks have an entry already in Blockly.Language
-    //  we don't need to translate those, but if null we have some component thing
+    //  we don't need to translate those, so if the following is non-null we ignore
     if (Blockly.Language[blockType] == null)
     {
-      // if above is null we know we don't have a built-in block
-      
-      // add some translations for language changes
+      // add some translations for language changes...
+      //   these should really be in a map or at procedure that checks and
+      //   returns replacement...straight translations...we could also put
+      //   lexical_variable_get and set here so that we don't have to have a special
+      //   case below
       if (blockType=='procedures_do_then_return')
         blockElem.setAttribute('type',"controls_do_then_return");
       else
@@ -32,13 +34,13 @@ Blockly.Versioning.blocksOverhaul = function(xmlFromFile) {
       else
       if (blockType=='for_lexical_variable_get')
         blockElem.setAttribute('type',"lexical_variable_get");
-      else
-      {
-      
-        // 1st check if type has 'any' in it, if so we have a generic method
+      else {
         var splitComponent=blockType.split('_');
+        // there are some blocks that are not built-in but are not component based,
+        //   we want to ignore them
         if (splitComponent[0]!='lexical') {
           // methods on any (generics) have a blocktype of _any_someComponent_method
+          //   so 1st check if type has 'any' in it, if so we have a generic method
           if (splitComponent[1]=='any')  // we have a generic method call
             Blockly.Versioning.translateAnyMethod(blockElem);
           else {
@@ -50,6 +52,10 @@ Blockly.Versioning.blocksOverhaul = function(xmlFromFile) {
             else {
               instance=splitComponent[0];
               var componentType=Blockly.Component.instanceNameToTypeName(instance);
+              // we should really check for null here so if there are blocks that
+              //   are not instance_ we can ignore. Right now the following ifs
+              //   probably make sure of this-- if none of the questions about rightside
+              //    are answered affirmatively, but this should be checked here as well
               rightside=splitComponent[1];
               if (rightside=='setproperty' || rightside=='getproperty')
                 Blockly.Versioning.translateSetGetProperty(blockElem);
@@ -183,7 +189,9 @@ Blockly.Versioning.translateSetGetProperty = function(blockElem) {
   var type=splitComponent[1]; //setproperty or getproperty
   // Paul has a function to convert instance to type
   var componentType=Blockly.Component.instanceNameToTypeName(instance);
-  // grab titles to find the particular property
+  // grab titles to find the particular property. There is a title elem with
+  //   a "PROP" attribute right under and within the block element itself
+  //   There might be many titles, but we grab the first.
   var titles= blockElem.getElementsByTagName('title');
   var propName='unknown';
   for (var i=0, len=titles.length; i<len; i++)
@@ -228,8 +236,10 @@ Blockly.Versioning.translateComponentSetGetProperty = function(blockElem) {
   var propName='unknown';
   for (var i=0, len=titles.length; i<len; i++)
   {
-    if (titles[i].getAttribute('name')=='PROP')
-      propName=titles[i].text;
+    if (titles[i].getAttribute('name')=='PROP') {
+      propName=titles[i].textContent;
+      break;
+    }
   }
   // ok, we have all the info, now we can override the old event attribute with 'event'
   blockElem.setAttribute('type','component_set_get');
