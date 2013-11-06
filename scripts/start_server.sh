e23af7ea798e6a2b4f6f87bca06573a1d5a7450b
Alan Rosenthal <alan@AlanECG.(none)>
2013-11-06 13:22:24 -0500

    updated start script to work with new app engine version

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,1 +1,1 @@
-../../appengine-java-sdk-1.8.4/bin/dev_appserver.sh --port=8888 --address=0.0.0.0 ../appinventor/appengine/build/war/
+~/Documents/appengine/appengine-java-sdk-1.8.7/bin/dev_appserver.sh --port=8888 --address=0.0.0.0 ../appinventor/appengine/build/war/
