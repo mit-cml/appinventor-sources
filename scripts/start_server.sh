e8ad7d2598f1e038ce7f0be7c4a315b984bb966c
Alan Rosenthal <alan.rosenthal@gmail.com>
2013-11-19 16:12:53 -0500

    update script to run aidaemon

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,5 @@
 cd ../appinventor/
+aiDaemon &
 ant
-~/Documents/appengine/appengine-java-sdk-1.8.7/bin/dev_appserver.sh --port=8888 --address=0.0.0.0 ../appinventor/appengine/build/war/
+~/Documents/appengine/appengine-java-sdk-1.8.7/bin/dev_appserver.sh --port=8888 --address=0.0.0.0 ../appinventor/appengine/build/war/ 
+
