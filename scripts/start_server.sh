9e63b3abcb7ecd78d955a3484c99752140d386ff
Alan Rosenthal <alan.rosenthal@gmail.com>
2013-11-19 16:56:16 -0500

    update script to beep after compile

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,6 @@
 cd ../appinventor/
 aiDaemon &
 ant
+beep -l 500
 ~/Documents/appengine/appengine-java-sdk-1.8.7/bin/dev_appserver.sh --port=8888 --address=0.0.0.0 ../appinventor/appengine/build/war/ 
 
