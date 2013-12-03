9180ba9ad1184ec2ca85107891b740f0b05930cd
Alan Rosenthal <alan.rosenthal@gmail.com>
2013-12-03 16:14:51 -0500

    update start script for server to clean before compile

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
 cd ../appinventor/
+ant clean
 aiDaemon &
 ant
 beep -l 500
 ~/Documents/appengine/appengine-java-sdk-1.8.7/bin/dev_appserver.sh --port=8888 --address=0.0.0.0 ../appinventor/appengine/build/war/ 
-
