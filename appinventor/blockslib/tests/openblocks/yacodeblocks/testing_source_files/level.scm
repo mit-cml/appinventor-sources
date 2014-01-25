4744a67c9a31d4996d68b3f9e1bc8dab82982248
Jeffrey I. Schiller <jis@mit.edu>
2014-01-25 12:39:03 -0500

    Remove Obsolete AI1 Code
    Completely remove the blockslib and blockseditor source from the
    source tree. They are AI1 specific and no longer used.
    
    Change-Id: I95de2fd9523d22c468ddd202d754590323ed609a

--- appinventor/blockslib/tests/openblocks/yacodeblocks/testing_source_files/level.scm
+++
@@ -1,4 +0,0 @@
-#|
-$JSON
-{"Source":"Form","Properties":{"$Name":"Level","$Type":"Form","Title":"Level","$Components":[{"$Name":"Canvas1","$Type":"Canvas","Uuid":"-1593113414","BackgroundColor":"&HFF0000FF","Width":"320","Height":"400","$Components":[{"$Name":"Ball1","$Type":"Ball","Uuid":"-320846878","Radius":"40","X":"81","Y":"190"}]},{"$Name":"AccelerometerSensor1","$Type":"AccelerometerSensor","Uuid":"1116314211"}]}}
-|#
--- appinventor/blockslib/tests/openblocks/yacodeblocks/testing_source_files/spriteandlist.scm
+++
@@ -1,4 +0,0 @@
-#|
-$JSON
-{"Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","Uuid":"0","Title":"Screen1","$Components":[{"$Name":"testequalitybutton","$Type":"Button","Uuid":"840261203","Text":"are they equal?"},{"$Name":"result","$Type":"Label","Uuid":"228803176","Text":"Text for Label2"},{"$Name":"Canvas1","$Type":"Canvas","Uuid":"-1912156634","Width":"300","Height":"300","$Components":[{"$Name":"ImageSprite1","$Type":"ImageSprite","Uuid":"1881625338","X":"7","Y":"18"},{"$Name":"ImageSprite2","$Type":"ImageSprite","Uuid":"397558677","X":"149","Y":"67"}]}]}}
-|#
