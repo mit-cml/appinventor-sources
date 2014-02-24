88803356eaa47be54ba17b50940d46bb1fda809e
AlanRosenthal <alan.rosenthal@gmail.com>
2014-02-24 12:09:28 -0500

    add variables to start_server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,12 +1,13 @@
-cd ../appinventor/
+#!/bin/bash
 
-#ant clean
+appinventor=~/Documents/appinventor-sources/appinventor
+appengine=~/Documents/appengine/appengine-java-sdk-1.8.7
 
+cd $appinventor
 
 ant installplay
 aiDaemon &
-
 ant
 beep -l 500
 
-~/Documents/appengine/appengine-java-sdk-1.8.7/bin/dev_appserver.sh --port=8888 --address=0.0.0.0 ../appinventor/appengine/build/war/ 
+$appengine/bin/dev_appserver.sh --port=8888 --address=0.0.0.0 $appinventor/appengine/build/war/
