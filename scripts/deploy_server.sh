fe7748f2a8da4be6cb5d3f4358a8cd09c9c111c7
AlanRosenthal <alan.rosenthal@gmail.com>
2014-02-24 12:19:49 -0500

    updated deploy server to use variables

--- scripts/deploy_server.sh
+++ scripts/deploy_server.sh
@@ -1,7 +1,11 @@
-cd ..
+#!/bin/bash
 
-cd appinventor
+appinventor=~/Documents/appinventor-sources/appinventor
+appengine=~/Documents/appengine/appengine-java-sdk-1.8.7
+appenginename=isense-ai2
+
+cd $appinventor
+
 ant
-cd ..
 
-../appengine/appengine-java-sdk-1.8.8/bin/appcfg.sh -A isense-ai2 update appinventor/appengine/build/war/
+$appengine/bin/appcfg.sh -A $appenginename update $appinventor/appengine/build/war/
