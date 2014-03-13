d55702c69dbf102f176f47bc12da6ba778408eb7
dejavu1988 <rekygx@gmail.com>
2014-03-13 12:52:01 +0200

    Added phonecallstarted and  phonecallended events to PhoneCall component

--- appinventor/components/src/com/google/appinventor/components/runtime/PhoneCall.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/PhoneCall.java
@@ -8,6 +8,7 @@ package com.google.appinventor.components.runtime;
 import com.google.appinventor.components.annotations.DesignerComponent;
 import com.google.appinventor.components.annotations.DesignerProperty;
 import com.google.appinventor.components.annotations.PropertyCategory;
+import com.google.appinventor.components.annotations.SimpleEvent;
 import com.google.appinventor.components.annotations.SimpleFunction;
 import com.google.appinventor.components.annotations.SimpleObject;
 import com.google.appinventor.components.annotations.SimpleProperty;
@@ -17,7 +18,13 @@ import com.google.appinventor.components.common.PropertyTypeConstants;
 import com.google.appinventor.components.common.YaVersion;
 import com.google.appinventor.components.runtime.util.PhoneCallUtil;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Component for making a phone call to a programatically-specified number.
@@ -47,11 +54,13 @@ import android.content.Context;
     nonVisible = true,
     iconName = "images/phoneCall.png")
 @SimpleObject
-@UsesPermissions(permissionNames = "android.permission.CALL_PHONE")
-public class PhoneCall extends AndroidNonvisibleComponent implements Component {
+@UsesPermissions(permissionNames = "android.permission.CALL_PHONE, android.permission.READ_PHONE_STATE, android.permission.PROCESS_OUTGOING_CALLS")
+public class PhoneCall extends AndroidNonvisibleComponent implements Component, OnDestroyListener {
 
   private String phoneNumber;
   private final Context context;
+  private final CallStateReceiver callStateReceiver;
+  private static final String LOG_TAG = "PhoneCallComponent";
 
   /**
    * Creates a Phone Call component.
@@ -61,7 +70,10 @@ public class PhoneCall extends AndroidNonvisibleComponent implements Component {
   public PhoneCall(ComponentContainer container) {
     super(container.$form());
     context = container.$context();
+    form.registerForOnDestroy(this);
     PhoneNumber("");
+    callStateReceiver = new CallStateReceiver();
+    registerCallStateMonitor();
   }
 
   /**
@@ -91,5 +103,96 @@ public class PhoneCall extends AndroidNonvisibleComponent implements Component {
   @SimpleFunction
   public void MakePhoneCall() {
     PhoneCallUtil.makePhoneCall(context, phoneNumber);
+  }
+  
+  /**
+   * Event indicating that a phone call has started.
+   * mode "out":outgoing call; "in":incoming call
+   * 
+   * @param mode "out":outgoing call; "in":incoming call
+   * @param phoneNumber incoming call phone number
+   */
+  @SimpleEvent
+  public void PhoneCallStarted(String mode, String phoneNumber) {
+    // invoke the application's "PhoneCallStarted" event handler.
+    EventDispatcher.dispatchEvent(this, "PhoneCallStarted", mode, phoneNumber);
+  }
+    
+  /**
+   * Event indicating that a phone call has ended.
+   * mode "out":outgoing call; "in":incoming call
+   * 
+   * @param mode "out":outgoing call; "in":incoming call
+   * @param phoneNumber ended call phone number
+   */
+  @SimpleEvent
+  public void PhoneCallEnded(String mode, String phoneNumber) {
+    // invoke the application's "PhoneCallEnded" event handler.
+    EventDispatcher.dispatchEvent(this, "PhoneCallEnded", mode, phoneNumber);
+  }
+  
+  /**
+   * BroadcastReceiver for incomming/outgoing phonecall state changes
+   *
+   */
+  private class CallStateReceiver extends BroadcastReceiver {
+    private String mode;
+    private String number;
+    public CallStateReceiver() {
+      mode = "";
+      number = "";
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+      String action = intent.getAction();
+      if(TelephonyManager.ACTION_PHONE_STATE_CHANGED.equals(action)){
+        String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+        Toast.makeText(context, state, Toast.LENGTH_LONG).show();
+        if(TelephonyManager.EXTRA_STATE_RINGING.equals(state)){
+          // Incoming call
+          mode = "in";
+          number = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+          Log.d(LOG_TAG, "IncommingCall: " + number);
+          PhoneCallStarted(mode, number);
+        }else if(TelephonyManager.EXTRA_STATE_OFFHOOK.equals(state)){
+          // Call offhook
+        }else if(TelephonyManager.EXTRA_STATE_IDLE.equals(state)){
+          // Incomming/Outgoing Call ends          
+          Log.d(LOG_TAG, "EndCall: " + number);
+          PhoneCallEnded(mode, number);
+        }
+      }else if(Intent.ACTION_NEW_OUTGOING_CALL.equals(action)){
+        // Outgoing call
+        Toast.makeText(context, "Outgoing", Toast.LENGTH_LONG).show();
+        mode = "out";
+        number = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+        Log.d(LOG_TAG, "OutgoingCall: " + number);
+        PhoneCallStarted(mode,number);
+      }
+    }
+  }
+    
+  /**
+   * Registers phonecall state monitor
+   */
+  private void registerCallStateMonitor(){
+    IntentFilter intentFilter = new IntentFilter();
+    intentFilter.addAction(Intent.ACTION_NEW_OUTGOING_CALL);
+    intentFilter.addAction(TelephonyManager.ACTION_PHONE_STATE_CHANGED);
+    context.registerReceiver(callStateReceiver, intentFilter);
+  }
+      
+  /**
+   * Unregisters phonecall state monitor
+   */
+  private void unregisterCallStateMonitor(){
+    context.unregisterReceiver(callStateReceiver);
+  }
+
+  @Override
+  public void onDestroy() {
+    // TODO Auto-generated method stub
+    unregisterCallStateMonitor();
   }
 }
