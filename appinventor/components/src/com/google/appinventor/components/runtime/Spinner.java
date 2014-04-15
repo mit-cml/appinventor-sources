02bb6ff445a3f8d6541c1c0d299490d81308ba07
Felix Dumit <felix.dumit@gmail.com>
2014-04-14 23:43:02 -0300

    added descriptions, removed list methods, using adapter directly

--- appinventor/components/src/com/google/appinventor/components/runtime/Spinner.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/Spinner.java
@@ -23,41 +23,28 @@ import com.google.appinventor.components.common.YaVersion;
 import com.google.appinventor.components.runtime.errors.YailRuntimeError;
 import com.google.appinventor.components.runtime.util.YailList;
 
-@DesignerComponent(version = YaVersion.SPINNER_VERSION, 
-  description = "<p>A spinner component, that when clicked on, displays a pop-up"
+@DesignerComponent(version = YaVersion.SPINNER_VERSION, description = "<p>A spinner component, that when clicked on, displays a pop-up"
     + " with a list of elements. These elements can be set in"
     + "the Designer or Blocks Editor by setting the "
     + "<code>ElementsFromString</code> property to their string-separated "
     + "concatenation (for example, <em>choice 1, choice 2, choice 3</em>) or "
-    + "by setting the <code>Elements</code> property to a List in the Blocks " + "editor.</p>", 
-    category = ComponentCategory.USERINTERFACE, 
-    nonVisible = false, 
-    iconName = "images/spinner.png")
+    + "by setting the <code>Elements</code> property to a List in the Blocks " + "editor.</p>", category = ComponentCategory.USERINTERFACE, nonVisible = false, iconName = "images/spinner.png")
 @SimpleObject
 public final class Spinner extends AndroidViewComponent implements OnItemSelectedListener {
 
   private final android.widget.Spinner view;
   private ArrayAdapter<String> adapter;
-  private YailList items;
-  private String selection;
-  private int selectionIndex;
 
-  /**
-   * Creates a new Spinner Component
-   * 
-   * @param container
-   *          container, component will be placed in
-   */
   public Spinner(ComponentContainer container) {
     super(container);
     view = new android.widget.Spinner(container.$context());
 
-     // Adds the component to its designated container
+    // Adds the component to its designated container
     container.$add(this);
 
-    items = new YailList();
-    selection = "";
-    selectionIndex = 0;
+    // items = new YailList();
+    // selection = "";
+    // selectionIndex = 0;
 
     // set regular and dropdown layouts
     adapter = new ArrayAdapter<String>(container.$context(), android.R.layout.simple_spinner_item);
@@ -74,45 +61,27 @@ public final class Spinner extends AndroidViewComponent implements OnItemSelecte
   /**
    * Selection property getter method.
    */
-  @SimpleProperty(description = "The selected item.  When directly changed by the "
-      + "programmer, the SelectionIndex property is also changed to the first "
-      + "item in the Spinner with the given value.  If the value does not "
-      + "appear, SelectionIndex will be set to 0.", category = PropertyCategory.BEHAVIOR)
+  @SimpleProperty(description = "Returns the current selected item in the spinner ", category = PropertyCategory.BEHAVIOR)
   public String Selection(){
-    return selection;
+    return view.getSelectedItem().toString();
   }
 
   /**
    * Selection property setter method.
    */
   @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_STRING, defaultValue = "")
-  @SimpleProperty
+  @SimpleProperty(description = "Set the selected in the spinner", category = PropertyCategory.BEHAVIOR)
   public void Selection(String value){
-    selection = value;
-    // Now, we need to change SelectionIndex to correspond to Selection.
-    // If multiple Selections have the same SelectionIndex, use the first.
-    // If none do, arbitrarily set the SelectionIndex to its default value
-    // of 0.
-    for (int i = 0; i < items.size(); i++) {
-      // The comparison is case-sensitive to be consistent with yail-equal?.
-      if (items.getString(i).equals(value)) {
-        selectionIndex = i + 1;
-        return;
-      }
-    }
-    selectionIndex = 0;
+    view.setSelection(adapter.getPosition(value));
   }
 
   /**
    * Selection index property getter method.
    */
   @SimpleProperty(description = "The index of the currently selected item, starting at "
-      + "1.  If no item is selected, the value will be 0.  If an attempt is "
-      + "made to set this to a number less than 1 or greater than the number "
-      + "of items in the Spinner, SelectionIndex will be set to 0, and "
-      + "Selection will be set to the empty text.", category = PropertyCategory.BEHAVIOR)
+      + "1.  If no item is selected, the value will be 0", category = PropertyCategory.BEHAVIOR)
   public int SelectionIndex(){
-    return selectionIndex;
+    return view.getSelectedItemPosition();
   }
 
   /**
@@ -120,85 +89,71 @@ public final class Spinner extends AndroidViewComponent implements OnItemSelecte
    */
   // Not a designer property, since this could lead to unpredictable
   // results if Selection is set to an incompatible value.
-  @SimpleProperty
+  @SimpleProperty(description = "Set the spinner selection to the element at the given index. If an attempt is "
+      + "made to set this to a number less than 1 or greater than the number "
+      + "of items in the Spinner, SelectionIndex will be set to 0, and "
+      + "Selection will be set to the empty text.", category = PropertyCategory.BEHAVIOR)
   public void SelectionIndex(int index){
-    if (index <= 0 || index > items.size()) {
-      selectionIndex = 0;
-      selection = "";
+    view.setSelection(index);
+    if (index <= 0 || index > adapter.getCount()) {
+      view.setSelection(0);
     } else {
-      selectionIndex = index;
-      // YailLists are 0-based, but we want to be 1-based.
-      selection = items.getString(selectionIndex - 1);
+      view.setSelection(index);
     }
   }
 
   /**
    * Elements property getter method
-   * 
-   * @return a YailList representing the list of strings to be picked from
    */
-  @SimpleProperty(category = PropertyCategory.BEHAVIOR)
+  @SimpleProperty(description = "return a YailList representing the list of strings to be picked from", category = PropertyCategory.BEHAVIOR)
   public YailList Elements(){
+    YailList items = new YailList();
+    for (int i = 0; i < adapter.getCount(); i++) {
+      items.add(adapter.getItem(i));
+    }
     return items;
   }
 
   /**
    * Elements property setter method
-   * 
-   * @param itemList
-   *          - a YailList containing the strings to be added to the Spinner
    */
-  @SimpleProperty
+  @SimpleProperty(description = "adds the passed string to the Spinner list", category = PropertyCategory.BEHAVIOR)
   public void Elements(YailList itemList){
     adapter.clear();
     Object[] objects = itemList.toStringArray();
     for (int i = 0; i < objects.length; i++) {
       if (!(objects[i] instanceof String)) {
-        throw new YailRuntimeError("Items passed to ListPicker must be Strings", "Error");
+        throw new YailRuntimeError("Items passed to Spinner must be Strings", "Error");
       }
     }
-    items = itemList;
     adapter.addAll(itemList.toStringArray());
   }
 
   /**
    * ElementsFromString property setter method
-   * 
-   * @param itemstring
-   *          - a string containing a comma-separated list of the strings to be
-   *          picked from
    */
   @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_STRING, defaultValue = "")
-  @SimpleProperty(category = PropertyCategory.BEHAVIOR)
+  @SimpleProperty(description = "sets the Spinner list to the elements passed in the comma-separated string", category = PropertyCategory.BEHAVIOR)
   public void ElementsFromString(String itemstring){
     adapter.clear();
-    if (itemstring.length() == 0) {
-      items = new YailList();
-    } else {
-      items = YailList.makeList((Object[]) itemstring.split(" *, *"));
-      adapter.addAll(items.toStringArray());
-
+    if (itemstring.length() != 0) {
+      adapter.addAll(itemstring.split(" *, *"));
     }
   }
 
   /**
    * Prompt property getter method
-   * 
-   * @return a String with the current prompt for the Spinner
    */
-  @SimpleProperty(category = PropertyCategory.APPEARANCE)
+  @SimpleProperty(description = "String with the current prompt for the Spinner", category = PropertyCategory.APPEARANCE)
   public String Prompt(){
     return view.getPrompt().toString();
   }
 
   /**
    * Prompt property setter method
-   * 
-   * @param str
-   *          - the string to set as the spinner prompt
    */
   @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_STRING, defaultValue = "")
-  @SimpleProperty(category = PropertyCategory.APPEARANCE)
+  @SimpleProperty(description = "sets the Spinner prompt to the give string", category = PropertyCategory.APPEARANCE)
   public void Prompt(String str){
     view.setPrompt(str);
   }
@@ -206,7 +161,7 @@ public final class Spinner extends AndroidViewComponent implements OnItemSelecte
   /**
    * To display the dropdown list without the user having to click it
    */
-  @SimpleFunction(description = "displays dropdown list, same action as when user clicks")
+  @SimpleFunction(description = "displays the dropdown list for selection, same action as when user clicks")
   public void DisplayDropdown(){
     view.performClick();
   }
@@ -214,29 +169,20 @@ public final class Spinner extends AndroidViewComponent implements OnItemSelecte
   /**
    * Indicates a user has selected an item
    */
-  @SimpleEvent
-  public void DidSelectItem(){
-    EventDispatcher.dispatchEvent(this, "DidSelectItem");
+  @SimpleEvent(description = "Even called when the user selects an item from the dropdown list")
+  public void DidSelectItem(String selection){
+    EventDispatcher.dispatchEvent(this, "DidSelectItem", selection);
   }
 
   public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
-    selection = (String) parent.getItemAtPosition(pos);
-    selectionIndex = pos;
-    DidSelectItem();
+    this.view.setSelection(pos);
+    DidSelectItem(this.view.getSelectedItem().toString());
+
   }
 
-  /**
-   * When a user selects nothing, not sure when this actually gets called
-   */
-  @SimpleEvent
-  public void DidSelectNothing(){
-    EventDispatcher.dispatchEvent(this, "DidSelectNothing");
-  }
-
+  // not sure when gets called, did not add and event for it
   public void onNothingSelected(AdapterView<?> parent){
-    selection = "";
-    selectionIndex = 0;
-    DidSelectNothing();
+    view.setSelection(0);
   }
 
 }
