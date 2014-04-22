3d113d21fe59725acd5df5fd2c5aa89462a7e002
AlanRosenthal <alan.rosenthal@gmail.com>
2014-04-22 10:23:36 -0400

    updated start server script to have clean option

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,9 +5,13 @@ appengine=~/Documents/appengine/appengine-java-sdk-1.9.3
 
 cd $appinventor
 
+if [[ "$1" = "clean" ]]; then
+    echo "Cleaning..."
+	ant clean
+fi
 ant installplay
 aiDaemon &
 ant
-beep -l 500
+beep -l 1000
 
 $appengine/bin/dev_appserver.sh --port=8888 --address=0.0.0.0 $appinventor/appengine/build/war/
