d2d24db4f34100906e19f67df982a5a726bf5537
vaible-hartman <viable.hartman@gmail.com>
2014-05-08 10:32:19 -0700

    @themadrobot provided a cleaner solution to the Press/Release button code, so I modified my project to work as his does

--- appinventor/components/src/com/google/appinventor/components/runtime/Button.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/Button.java
@@ -6,16 +6,10 @@
 package com.google.appinventor.components.runtime;
 
 import com.google.appinventor.components.annotations.DesignerComponent;
-import com.google.appinventor.components.annotations.DesignerProperty;
-import com.google.appinventor.components.annotations.PropertyCategory;
-import com.google.appinventor.components.common.PropertyTypeConstants;
 import com.google.appinventor.components.annotations.SimpleEvent;
 import com.google.appinventor.components.annotations.SimpleObject;
-import com.google.appinventor.components.annotations.SimpleProperty;
 import com.google.appinventor.components.common.ComponentCategory;
 import com.google.appinventor.components.common.YaVersion;
-import android.view.MotionEvent;
-import android.view.View;
 
 /**
  * Button with the ability to launch events on initialization, focus
@@ -32,9 +26,6 @@ import android.view.View;
 @SimpleObject
 public final class Button extends ButtonBase {
 
-  // Used for determining if this button is Press/Release or Click behavior.
-  private boolean isPressRelease=false;
-
   /**
    * Creates a new Button component.
    *
@@ -42,73 +33,6 @@ public final class Button extends ButtonBase {
    */
   public Button(ComponentContainer container) {
     super(container);
-  }
-
-  /**
-   * Specifies if this is a Press/Release type button or a ragular one.
-   *
-   * @param isPressRelease  {@code true} treats onTouch as Press/Release event
-   *                        {@code false} treats onTouch as Click pass through
-   */
-  @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_BOOLEAN,
-          defaultValue = "False")
-  @SimpleProperty(description = "Specifies if this button should be treated " +
-          "as a button that can be pressed and released.")
-
-  public void IsPressRelease(boolean isPressRelease) {
-    this.isPressRelease=isPressRelease;
-  }
-
-  /**
-   * Returns true if the button is of Press/Release type.
-   *
-   * @return {@code true} indicates button is of Press/Release type
-   *         {@code false} indicates button is simple Click type
-   */
-  @SimpleProperty(
-          category = PropertyCategory.BEHAVIOR,
-          description = "Returns the button's usage type.")
-  public boolean IsPressRelease() {
-      return isPressRelease;
-  }
-
-  /**
-   * If this button instance isPressRelease type, dispatch Press Release events
-   */
-  @Override
-  public boolean onTouch(View view, MotionEvent me)
-  {
-     if (!IsPressRelease()) {
-       return super.onTouch(view, me);
-     } else {
-       super.onTouch(view, me);
-       switch ( me.getAction() ) {
-          case MotionEvent.ACTION_DOWN:
-              Press();
-              break;
-          case MotionEvent.ACTION_UP:
-          case MotionEvent.ACTION_CANCEL:
-              Release();
-              break;
-        }
-     }
-     return true;
-  }
-
-  /**
-   * Indicates a user pressed down on the button.
-   */
-  @SimpleEvent
-  public void Press() {
-    EventDispatcher.dispatchEvent(this, "Press");
-  }
-
-  /**
-   * Indicates a user released the button.
-   */
-  @SimpleEvent
-  public void Release() {
-    EventDispatcher.dispatchEvent(this, "Release");
   }
 
  @Override
