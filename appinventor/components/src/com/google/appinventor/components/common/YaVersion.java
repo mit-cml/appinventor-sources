27b5d5e64ccef89f49c0a0e8224b859828b6d32d
Jeffrey I. Schiller <jis@mit.edu>
2014-09-14 23:15:11 -0400

    Update Companion Version to 2.22ai2.
    Change-Id: Icecf2daad3064c28a644221143a7e8d190f28b83

--- appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
+++ appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
@@ -810,10 +810,11 @@ public class YaVersion {
   // key as the Companion it is replacing, as the Package Manager
   // is invoked from the running Companion.
 
-  public static final String PREFERRED_COMPANION = "2.21ai2zx1";
+  public static final String PREFERRED_COMPANION = "2.22ai2zx1";
   public static final String COMPANION_UPDATE_URL = "";
   public static final String COMPANION_UPDATE_URL1 = "";
-  public static final String [] ACCEPTABLE_COMPANIONS = { "2.20ai2", "2.20ai2zx1", "2.21ai2", "2.21ai2zx1" };
+  public static final String [] ACCEPTABLE_COMPANIONS = { "2.20ai2", "2.20ai2zx1", "2.21ai2", "2.21ai2zx1",
+                                                          "2.22ai2", "2.22ai2zx1" };
 
   // Splash Screen Values
   public static final int SPLASH_SURVEY = 1;
