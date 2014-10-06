094c19e4d256a3d74e1d089650305b1c2795edb8
Hossein Amerkashi <kkashi01@gmail.com>
2014-10-06 11:43:06 -0400

    Updated code to prevent auto triggering of AfterPicking on initialization
    Change-Id: I866290fc4d681f8675c916e08d09c758da3244a3

--- appinventor/components/src/com/google/appinventor/components/runtime/Spinner.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/Spinner.java
@@ -41,6 +41,7 @@ public final class Spinner extends AndroidViewComponent implements OnItemSelecte
   private YailList items = new YailList();
   private String selection;
   private int selectionIndex;
+  private boolean isInitialized=false;
 
   public Spinner(ComponentContainer container) {
     super(container);
@@ -122,6 +123,10 @@ public final class Spinner extends AndroidViewComponent implements OnItemSelecte
       category = PropertyCategory.BEHAVIOR)
   public void Elements(YailList itemList){
     items = ElementsUtil.elements(itemList, "Spinner");
+
+    //avoid firing off the OnItemSelect when component is initialized or data is changed
+    isInitialized = false;
+
     setAdapterData(itemList.toStringArray());
   }
 
@@ -180,6 +185,12 @@ public final class Spinner extends AndroidViewComponent implements OnItemSelecte
   }
 
   public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+    //prevent AfterPicking triggering when component has just been instantiated.
+    if (!isInitialized) {
+      isInitialized = true;
+      return;
+    }
+
     SelectionIndex(position + 1); // AI lists are 1-based
     AfterSelecting(selection);
   }
