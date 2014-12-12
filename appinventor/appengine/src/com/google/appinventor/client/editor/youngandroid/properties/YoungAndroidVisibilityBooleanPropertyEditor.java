714923571f363a2d6d1cb49e1e182da5d0589c30
Mark Sherman <msherman@cs.uml.edu>
2014-12-12 12:05:40 -0500

    Designer property for "visible" now uses checkbox
    Replaces pulldown that had text options.
    
    Change-Id: I68a8a202dc24b5638defe9b7df2f3385a830e3c3

--- appinventor/appengine/src/com/google/appinventor/client/editor/youngandroid/properties/YoungAndroidVisibilityBooleanPropertyEditor.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/youngandroid/properties/YoungAndroidVisibilityBooleanPropertyEditor.java
@@ -7,28 +7,21 @@
 package com.google.appinventor.client.editor.youngandroid.properties;
 
 
-import com.google.appinventor.client.widgets.properties.ChoicePropertyEditor;
-
-import static com.google.appinventor.client.Ode.MESSAGES;
+import com.google.appinventor.client.widgets.properties.BooleanPropertyEditor;
 
 
 /**
  * Property editor for visibility: selects showing or hidden
- * 
+ *
  * @author hal@mit.edu (Hal Abelson)
  */
-public class YoungAndroidVisibilityChoicePropertyEditor extends ChoicePropertyEditor {
+public class YoungAndroidVisibilityBooleanPropertyEditor extends BooleanPropertyEditor {
 
   // TODO(hal): The values must be True and False. Lowercase true and false do not work here:
   // they are not transformed from strings to booleans when passed to the property.
   // Figure out why not and explain in a comment.
-  
-  private static final Choice[] visibility = new Choice[] {
-    new Choice(MESSAGES.VisibilityShowingProperties(), "True"),
-    new Choice(MESSAGES.VisibilityHiddenProperties(), "False"),
-  };
 
-  public YoungAndroidVisibilityChoicePropertyEditor() {
-    super(visibility);
+  public YoungAndroidVisibilityBooleanPropertyEditor() {
+    super("True", "False");
   }
 }
