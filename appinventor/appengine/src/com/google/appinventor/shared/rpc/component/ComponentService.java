0092f7e72f684f2539dc1ca1e1402b6dac373795
E-H <e.hon.dev@gmail.com>
2015-06-11 09:48:04 +0800

    Add service for components records

---
+++ appinventor/appengine/src/com/google/appinventor/shared/rpc/component/ComponentService.java
@@ -0,0 +1,24 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2009-2011 Google, All Rights reserved
+// Copyright 2011-2015 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+package com.google.appinventor.shared.rpc.component;
+
+import com.google.appinventor.shared.rpc.ServerLayout;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+import java.util.List;
+
+@RemoteServiceRelativePath(ServerLayout.COMPONENT_SERVICE)
+public interface ComponentService extends RemoteService {
+
+  /**
+   * @return A list of records of user's components
+   */
+  List<ComponentRecord> getComponentRecords();
+
+}
---
+++ appinventor/appengine/src/com/google/appinventor/shared/rpc/component/ComponentServiceAsync.java
@@ -0,0 +1,20 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2009-2011 Google, All Rights reserved
+// Copyright 2011-2015 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+package com.google.appinventor.shared.rpc.component;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import java.util.List;
+
+public interface ComponentServiceAsync {
+
+  /**
+   * @see ComponentService#getComponentRecords()
+   */
+  List<ComponentRecord> getComponentRecords(AsyncCallback<ComponentRecord> callback);
+
+}
