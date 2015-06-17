bcbf92e851e139ace28344367d177a741211e437
William Byrne <williambyrne@Williams-MacBook-Pro.local>
2015-06-17 08:20:10 -0400

    Cleaned up debug code.

--- appinventor/appengine/src/com/google/appinventor/client/utils/PZAwarePositionCallback.java
+++ appinventor/appengine/src/com/google/appinventor/client/utils/PZAwarePositionCallback.java
@@ -58,48 +58,4 @@ public abstract class PZAwarePositionCallback implements PopupPanel.PositionCall
 
         return [left, top];
     }-*/;
-
-    //  Helper used to accurately determine the absolute left and absolute top
-    //  of the Element for use in positioning. Pinch Zooming breaks GWT's built-in
-    //  equivalents, DOMImpl#getSubPixelAbsoluteLeft(Element) and
-    //  DOMImpl#getSubPixelAbsoluteTop(Element).
-//    private native JsArrayInteger getAbsolutePosition(Element elem) /*-{
-//        var box = elem.getBoundingClientRect();
-//
-//        var body = $doc.body;
-//
-//        var docElem = $doc.documentElement;
-//
-//        //var scrollTop = $wnd.pageYOffset || docElem.scrollTop || body.scrollTop;
-//
-//        //var scrollLeft = $wnd.pageXOffset || docElem.scrollLeft || body.scrollLeft;
-//
-//        var clientTop = docElem.clientTop || body.clientTop || 0;
-//
-//        var clientLeft = docElem.clientLeft || body.clientLeft || 0;
-//
-//        var top  = box.top; //- clientTop + $wnd.scrollY;
-//
-//        var left = box.left; //+ scrollLeft - clientLeft;
-//
-//        //Position parameters used for drawing the rectangle
-//        var width = top-box.bottom;
-//        var height = left-box.right;
-//
-//        var rect = box;
-//        var tableRectDiv = document.createElement('div');
-//        tableRectDiv.style.position = 'absolute';
-//        tableRectDiv.style.border = '1px solid red';
-//        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-//        var scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
-//        tableRectDiv.style.margin = tableRectDiv.style.padding = '0';
-//        tableRectDiv.style.top = (rect.top + scrollTop) + 'px';
-//        tableRectDiv.style.left = (rect.left + scrollLeft) + 'px';
-//        // we want rect.width to be the border width, so content width is 2px less.
-//        tableRectDiv.style.width = (rect.width - 2) + 'px';
-//        tableRectDiv.style.height = (rect.height - 2) + 'px';
-//        document.body.appendChild(tableRectDiv);
-//
-//        return [Math.round(left), Math.round(top)];
-//    }-*/;
 }
