94740de53697f054e5f14c7b7ffe113b8ea3c9d7
E-H <e.hon.dev@gmail.com>
2015-06-22 22:06:42 +0800

    Manage component data downloaded from server

---
+++ appinventor/appengine/src/com/google/appinventor/client/explorer/component/ComponentManager.java
@@ -0,0 +1,35 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2009-2011 Google, All Rights reserved
+// Copyright 2011-2015 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+package com.google.appinventor.client.explorer.component;
+
+import com.google.appinventor.client.Ode;
+import com.google.appinventor.client.OdeAsyncCallback;
+import com.google.appinventor.shared.rpc.component.ComponentInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class ComponentManager {
+  private final List<ComponentInfo> compInfos = new ArrayList<ComponentInfo>();
+
+  public ComponentManager() {
+    Ode.getInstance().getComponentService().getComponentInfos(
+      new OdeAsyncCallback<List<ComponentInfo>>() {
+        @Override
+        public void onSuccess(List<ComponentInfo> compInfos) {
+          ComponentManager.this.compInfos.addAll(compInfos);
+        }
+      });
+  }
+
+  /**
+   * Returns a list of all component info already retrived by this manager
+   */
+  public List<ComponentInfo> getRetrivedComponentInfos() {
+    return compInfos;
+  }
+}
