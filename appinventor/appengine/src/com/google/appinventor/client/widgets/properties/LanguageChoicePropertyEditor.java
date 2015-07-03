c743f08d09298f7946ee3e3ee5e040c9dd4aaf89
E-H <e.hon.dev@gmail.com>
2015-07-03 10:17:49 +0800

    Add default choice for the two property editors

--- appinventor/appengine/src/com/google/appinventor/client/widgets/properties/LanguageChoicePropertyEditor.java
+++ appinventor/appengine/src/com/google/appinventor/client/widgets/properties/LanguageChoicePropertyEditor.java
@@ -6,6 +6,8 @@
 
 package com.google.appinventor.client.widgets.properties;
 
+import static com.google.appinventor.client.Ode.MESSAGES;
+
 /**
  * Property editor for text-to-speech languages.
  *
@@ -14,6 +16,7 @@ public class LanguageChoicePropertyEditor extends ChoicePropertyEditor {
 
   // languages supported by AppInventor's Android 2.2 emulator
   private static final Choice[] languages = new Choice[] {
+    new Choice(MESSAGES.defaultText(), ""),
     new Choice("de", "de"),
     new Choice("en", "en"),
     new Choice("es", "es"),
