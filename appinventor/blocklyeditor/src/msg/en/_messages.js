bad59cc34f52fc3e154e8c3e3eb5696611ce4795
Grace <ykmin426@gmail.com>
2015-07-13 18:43:17 -0400

    warning handler for missing component of pasted blocks

--- appinventor/blocklyeditor/src/msg/en/_messages.js
+++ appinventor/blocklyeditor/src/msg/en/_messages.js
@@ -82,6 +82,7 @@ Blockly.Msg.en.switch_language_to_english = {
     Blockly.ERROR_BLOCK_CANNOT_BE_IN_DEFINTION = "This block cannot be in a definition";
     Blockly.ERROR_SELECT_VALID_ITEM_FROM_DROPDOWN = "Select a valid item in the drop down.";
     Blockly.ERROR_DUPLICATE_EVENT_HANDLER = "This is a duplicate event handler for this component.";
+    Blockly.ERROR_COMPONENT_DOES_NOT_EXIST = "Component does not exist";
 
 // Colour Blocks.
     Blockly.Msg.LANG_COLOUR_PICKER_HELPURL = 'http://appinventor.mit.edu/explore/ai2/support/blocks/colors#basic';
