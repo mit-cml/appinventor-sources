80cbee33abd68c699f86a036bd9b0be0acc6d649
E-H <e.hon.dev@gmail.com>
2015-07-23 21:19:11 +0800

    Change to use CellTable

--- appinventor/appengine/src/com/google/appinventor/client/wizards/ComponentImportWizard.java
+++ appinventor/appengine/src/com/google/appinventor/client/wizards/ComponentImportWizard.java
@@ -17,55 +17,53 @@ import com.google.appinventor.shared.rpc.component.ComponentInfo;
 import com.google.appinventor.shared.rpc.project.youngandroid.YoungAndroidAssetsFolder;
 import com.google.appinventor.shared.rpc.project.youngandroid.YoungAndroidProjectNode;
 import com.google.gwt.cell.client.AbstractCell;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.cellview.client.CellList;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.ResizeLayoutPanel;
+import com.google.gwt.user.client.ui.TabLayoutPanel;
 import com.google.gwt.view.client.ListDataProvider;
+import com.google.gwt.view.client.SelectionModel;
 import com.google.gwt.view.client.SingleSelectionModel;
 
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.cell.client.TextCell;
+import com.google.gwt.cell.client.NumberCell;
+import com.google.gwt.cell.client.CheckboxCell;
+
 public class ComponentImportWizard extends Wizard {
-  static class ComponentCell extends AbstractCell<ComponentInfo> {
-    @Override
-    public void render(Context context, ComponentInfo value, SafeHtmlBuilder sb) {
-      sb.appendHtmlConstant("<table>");
-      sb.appendHtmlConstant("<td>");
-      sb.appendEscaped(value.getName());
-      sb.appendHtmlConstant("</td>");
-      sb.appendHtmlConstant("<td>");
-      sb.appendEscaped(new Long(value.getVersion()).toString());
-      sb.appendHtmlConstant("</td>");
-      sb.appendHtmlConstant("</table>");
-    }
-  }
+  private final Ode ode = Ode.getInstance();
 
   public ComponentImportWizard() {
     super(MESSAGES.componentImportWizardCaption(), true, false);
 
-    CellList<ComponentInfo> cellList = new CellList<ComponentInfo>(new ComponentCell());
-    final SingleSelectionModel<ComponentInfo> selectionModel = new SingleSelectionModel<ComponentInfo>();
-    cellList.setSelectionModel(selectionModel);
+    final CellTable compTable = createCompTable();
 
     ListDataProvider<ComponentInfo> dataProvider = new ListDataProvider<ComponentInfo>();
-    for (ComponentInfo compInfo : Ode.getInstance().getComponentManager().getRetrivedComponentInfos()) {
+    for (ComponentInfo compInfo : ode.getComponentManager().getRetrivedComponentInfos()) {
       dataProvider.getList().add(compInfo);
     }
-    dataProvider.addDataDisplay(cellList);
+    dataProvider.addDataDisplay(compTable);
 
     VerticalPanel panel = new VerticalPanel();
-    panel.setVerticalAlignment(VerticalPanel.ALIGN_MIDDLE);
-    panel.add(cellList);
+    panel.add(compTable);
 
     addPage(panel);
 
-    // todo: improve the ui
-    setPagePanelHeight(80);
+    setPagePanelHeight(400);
+    setPixelSize(400, 400);
     setStylePrimaryName("ode-DialogBox");
 
     initFinishCommand(new Command() {
       @Override
       public void execute() {
+        SingleSelectionModel<ComponentInfo> selectionModel =
+            (SingleSelectionModel<ComponentInfo>) compTable.getSelectionModel();
         ComponentInfo toImport = selectionModel.getSelectedObject();
 
         if (toImport == null) {
@@ -74,7 +72,6 @@ public class ComponentImportWizard extends Wizard {
           return;
         }
 
-        final Ode ode = Ode.getInstance();
         final long projectId = ode.getCurrentYoungAndroidProjectId();
         final Project project = ode.getProjectManager().getProject(projectId);
         final YoungAndroidAssetsFolder assetsFolderNode =
@@ -92,5 +89,41 @@ public class ComponentImportWizard extends Wizard {
             });
       }
     });
+  }
+
+  private CellTable createCompTable() {
+    final SelectionModel<ComponentInfo> selectionModel =
+        new SingleSelectionModel<ComponentInfo>();
+
+    CellTable<ComponentInfo> compTable = new CellTable<ComponentInfo>();
+    compTable.setSelectionModel(selectionModel);
+
+    Column<ComponentInfo, Boolean> checkColumn = new Column<ComponentInfo, Boolean>(
+        new CheckboxCell(true, false)) {
+          @Override
+          public Boolean getValue(ComponentInfo object) {
+            return selectionModel.isSelected(object);
+          }
+        };
+    Column<ComponentInfo, String> nameColumn = new Column<ComponentInfo, String>(
+        new TextCell()) {
+          @Override
+          public String getValue(ComponentInfo compInfo) {
+            return compInfo.getName();
+          }
+        };
+    Column<ComponentInfo, Number> versionColumn = new Column<ComponentInfo, Number>(
+        new NumberCell()) {
+          @Override
+          public Number getValue(ComponentInfo compInfo) {
+            return compInfo.getVersion();
+          }
+        };
+
+    compTable.addColumn(checkColumn);
+    compTable.addColumn(nameColumn, "Component");
+    compTable.addColumn(versionColumn, "Version");
+
+    return compTable;
   }
 }
