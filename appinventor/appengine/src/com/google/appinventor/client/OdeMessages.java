cd308c0a3a0738d52185389500577bb3ac6936f6
hal <hal@mit.edu>
2015-07-25 15:47:16 -0400

    Fix Slider for API < 16
    Fix bug in Slider where is does not run in API < 16 and does not even
    load in API < 9.
    
    Change-Id: Iab831b4e056a7ce54de1b0e05e5358e55205277c

--- appinventor/appengine/src/com/google/appinventor/client/OdeMessages.java
+++ appinventor/appengine/src/com/google/appinventor/client/OdeMessages.java
@@ -5139,7 +5139,12 @@ public interface OdeMessages extends Messages {
   @Description("")
   String SharingHelpStringComponentPallette();
 
-  @DefaultMessage("A Slider is a progress bar that adds a draggable thumb. You can touch the thumb and drag left or right to set the slider thumb position. As the Slider thumb is dragged, it will trigger the PositionChanged event, reporting the position of the Slider thumb. The reported position of the Slider thumb can be used to dynamically update another component attribute, such as the font size of a TextBox or the radius of a Ball.")
+  @DefaultMessage("A Slider is a progress bar that adds a draggable thumb. You can touch " +
+        "the thumb and drag left or right to set the slider thumb position. " +
+        "As the Slider thumb is dragged, it will trigger the PositionChanged event, " +
+        "reporting the position of the Slider thumb. The reported position of the " +
+        "Slider thumb can be used to dynamically update another component " +
+        "attribute, such as the font size of a TextBox or the radius of a Ball.")
   @Description("")
   String SliderHelpStringComponentPallette();
 
