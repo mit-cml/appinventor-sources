5f20b73014e58811777e54ea46a31a04fe3c39e2
Momoumar <mouha.oumar@gmail.com>
2015-07-28 19:58:48 -0400

    Added ExternalComponentPackaging.java

---
+++ appinventor/components/src/com/google/appinventor/components/scripts/ExternalComponentPackaging.java
@@ -0,0 +1,39 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2009-2011 Google, All Rights reserved
+// Copyright 2011-2012 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+package com.google.appinventor.components.scripts;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.charset.Charset;
+import java.util.*;
+import java.io.File;
+import org.json.JSONException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+* Tool to package external Component
+*
+* @author mouha.oumar@gmail.com (Mouhamadou O. Sall)
+*/
+
+public class ExternalComponentPackaging {
+
+
+
+}
