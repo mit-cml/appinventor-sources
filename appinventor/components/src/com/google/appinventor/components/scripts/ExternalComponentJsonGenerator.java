f92b94ff50bcdec9abb13a829d0e3463a8e65d9a
Momoumar <mouha.oumar@gmail.com>
2015-08-15 15:00:41 -0400

    Ant script refactored

--- appinventor/components/src/com/google/appinventor/components/scripts/ExternalComponentJsonGenerator.java
+++ appinventor/components/src/com/google/appinventor/components/scripts/ExternalComponentJsonGenerator.java
@@ -21,7 +21,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 /**
-* Tool to generate external component descriptors JSON.
+* Tool to generate component.json for extension.
 *
 * @author mouha.oumar@gmail.com (Mouhamadou O. Sall)
 */
@@ -29,20 +29,21 @@ import org.json.simple.parser.ParseException;
 public class ExternalComponentJsonGenerator {
 
 
+  /**
+  * The definitions of the arguments used by this script
+  *
+  * args[0]: the path to simple_component.json
+  * args[1]: the path to external_components.txt
+  * args[2]: the path to ExternalComponentAsset.dir: "${local.build.dir}/ExternalComponents"
+  */
   public static void main(String[] args) throws IOException, ParseException, JSONException {
-    /*
-    * args[0]: the path to simple_component.json
-    * args[1]: the path to external_components.txt
-    * args[2]: the path to ExternalComponentAsset.dir: "${local.build.dir}/ExternalComponents"
-    */
-
     JSONParser parser = new JSONParser();
-    String jsonText = readFile(args[0],Charset.defaultCharset());
-    Object obj = parser.parse(jsonText);
-    JSONArray array = (JSONArray)obj;
+    String simple_component_text = readFile(args[0],Charset.defaultCharset());
+    Object simple_component_obj = parser.parse(simple_component_text);
+    JSONArray simple_component_array = (JSONArray)simple_component_obj;
     ArrayList<String> components = fileToArray(args[1]);
-    for(int i = 0; i<array.size();i++){
-      JSONObject component = (JSONObject)array.get(i);
+    for(int i = 0; i<simple_component_array.size(); i++){
+      JSONObject component = (JSONObject)simple_component_array.get(i);
       if(components.contains(component.get("name"))){  //TODO(Mos): Should test the external boolean here instead
         new File(args[2]+"/"+component.get("name").toString()).mkdirs();
         FileWriter file = new FileWriter(args[2]+"/"+component.get("name")+"/"+"component.json");
@@ -59,11 +60,23 @@ public class ExternalComponentJsonGenerator {
     }
   }
 
+  /**
+  * Read a file and returns its content
+  *
+  * @param path the path of the file to be read
+  * @param encoding the encoding system
+  */
   private static String readFile(String path, Charset encoding) throws IOException{
-      byte[] encoded = Files.readAllBytes(Paths.get(path));
-      return new String(encoded, encoding);
+    byte[] encoded = Files.readAllBytes(Paths.get(path));
+    return new String(encoded, encoding);
   }
 
+  /**
+  * Read external_components.txt (a file containing the name of the extension)
+  * and returns an ArrayList containing those components
+  *
+  * @param fileName the path of the file to be read
+  */
   private static ArrayList<String> fileToArray(String fileName) throws FileNotFoundException{
     Scanner sc = new Scanner(new File(fileName));
     ArrayList<String> components = new ArrayList<String>();
