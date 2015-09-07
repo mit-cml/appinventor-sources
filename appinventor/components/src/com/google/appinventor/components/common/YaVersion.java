64e601971618c6f8b4f8a35026e1c15d2747a1fd
Francesco Montefoschi <francesco.monte@gmail.com>
2015-09-07 11:37:49 +0200

    Saparated Camera and UdooCamera

--- appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
+++ appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
@@ -938,6 +938,7 @@ public class YaVersion {
   // Splash Screen Values
   public static final int SPLASH_SURVEY = 1;
 
+  public static final int UDOO_CAMERA_COMPONENT_VERSION = 1;
   public static final int UDOO_ARDUINO_COMPONENT_VERSION = 1;
   public static final int UDOO_TEMPERATURE_HUMIDITY_SENSOR_COMPONENT_VERSION = 1;
 }
