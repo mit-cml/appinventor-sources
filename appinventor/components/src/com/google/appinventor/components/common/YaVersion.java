ab9886696ba9c016744b18f536ae0309df963116
Thomas Oropeza <thomasoropeza@gmail.com>
2015-09-29 12:41:57 -0700

    side bar fix

--- appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
+++ appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
@@ -966,10 +966,10 @@ public class YaVersion {
   // key as the Companion it is replacing, as the Package Manager
   // is invoked from the running Companion.
 
-  public static final String PREFERRED_COMPANION = "2.32";
+  public static final String PREFERRED_COMPANION = "2.33";
   public static final String COMPANION_UPDATE_URL = "";
   public static final String COMPANION_UPDATE_URL1 = "";
-  public static final String [] ACCEPTABLE_COMPANIONS = { "2.32" };
+  public static final String [] ACCEPTABLE_COMPANIONS = { "2.33" };
 
   // Splash Screen Values
   public static final int SPLASH_SURVEY = 1;
