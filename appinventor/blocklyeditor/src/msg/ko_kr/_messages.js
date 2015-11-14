0f9036056a16042183718f6de581b897dc53a6ad
Byeongju Lee <pillarlee16@gmail.com>
2015-11-14 12:46:56 +0000

    added comments on _meesages.js file

--- appinventor/blocklyeditor/src/msg/ko_kr/_messages.js
+++ appinventor/blocklyeditor/src/msg/ko_kr/_messages.js
@@ -1,8 +1,41 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2012 Massachusetts Institute of Technology. All rights reserved.
+
+/**
+ * Visual Blocks Language
+ *
+ * Copyright 2012 Google Inc.
+ * http://blockly.googlecode.com/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Korean strings.
+ * @author pillarlee16@gmail.com (Byeongju Lee)
+ */
+
 'use strict'
 
 goog.provide('Blockly.Msg.ko_kr');
 
+/**
+ * Due to the frequency of long strings, the 80-column wrap rule need not apply
+ * to message files.
+ */
+
 Blockly.Msg.ko_kr.switch_language_to_korean = {
+  // Switch language to Korean.
   category: '',
   helpUrl: '',
   init: function() {
