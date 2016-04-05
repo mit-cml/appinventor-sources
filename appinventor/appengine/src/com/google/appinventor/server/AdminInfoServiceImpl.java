31b2860bd6cd38f7f08b16b074358e0f204d0915
Jeffrey I. Schiller <jis@mit.edu>
2016-04-04 22:21:06 -0400

    Add Support for Local Logins
    This change adds support for local logins as well as maintaining
    compatibility with Google authentication (when running on App Engine).
    
    A “local login” is an account whose login name is an email address and
    which has a password which we store in the UserData object (hashed via
    PBKDF2). Two new flags “auth.usegoogle” and “auth.uselocal” control
    whether or not Google Logins and/or Local logins are accepted.
    
    If auth.usegoogle is false and auth.uselocal is true, a login page is
    displayed which prompts for a person’s email address and password. A
    “set/recovery password” link is also displayed which can be used to
    change a person’s password or set a new password. This link is how local
    login accounts are created. When clicked, the person is prompted for
    their e-mail address. We then e-mail them a link which they click on to
    confirm their account. This takes them to a page where they can set
    their password.
    
    Note: Because of restrictions imposed by Google, we cannot send e-mail
    directly from App Engine. Instead we use a “helper” server which accepts
    an HTTP POST operation from us (using the Urlfetch functionality of App
    Engine). This helper does the actual sending of e-mail. This also
    permits us to control where the email originates from (i.e., we can use
    a server at MIT instead of a server at Google as some locations care
    from where the mail comes). The source code for the “helper” server is
    included in this commit (in the misc/passwordmail directory. See the
    README file there for more details).
    
    If auth.usegoogle and auth.uselocal are both set, the login page has a
    link labeled “Use Google Account” which will then cause their Google
    account to be used to login.
    
    If auth.usegoogle is true and auth.uselocal is false, then the system
    behaves as it does today. No local login page is shown, instead Google
    accounts are used (which means that if someone isn’t logged in they see
    Google’s account page as they do today).
    
    We also add a “User Admin” feature that permits authorized users to
    change the password (but not a Google password) for other users. Admins
    can also “become” another user for purposes of examining or downloading
    their projects. When in this “become” mode, the system is read-only. You
    can look at other user’s project, and interact with them, but you cannot
    save any work.
    
    Change-Id: Id20c49b3dadbb5e5f2180364a928991dd31343bd

---
+++ appinventor/appengine/src/com/google/appinventor/server/AdminInfoServiceImpl.java
@@ -0,0 +1,89 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2015 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+package com.google.appinventor.server;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpSession;
+
+import com.google.appinventor.server.flags.Flag;
+import com.google.appinventor.server.storage.StorageIo;
+import com.google.appinventor.server.storage.StorageIoInstanceHolder;
+import com.google.appinventor.shared.rpc.AdminInterfaceException;
+import com.google.appinventor.shared.rpc.user.Config;
+import com.google.appinventor.shared.rpc.admin.AdminUser;
+import com.google.appinventor.shared.rpc.admin.AdminInfoService;
+import com.google.appinventor.server.util.PasswordHash;
+
+/**
+ * Implementation of the user information service.
+ *
+ * <p>Note that this service must be state-less so that it can be run on
+ * multiple servers.
+ *
+ */
+public class AdminInfoServiceImpl extends OdeRemoteServiceServlet implements AdminInfoService {
+
+  // Storage of user settings
+  private final transient StorageIo storageIo = StorageIoInstanceHolder.INSTANCE;
+
+  // Access the higher level session object
+  // Note: If we want to do this from any other service, we should move this
+  // to OdeRemoveServiceServlet (our parent class)
+  protected final LocalSession localSession = LocalSession.getInstance();
+
+  /**
+   * Returns a list of AdminUsers, up to 20, based on the starting
+   * point.
+   */
+
+  @Override
+  public List<AdminUser> searchUsers(String startingPoint) {
+    if (!userInfoProvider.getIsAdmin()) {
+      throw new IllegalArgumentException("Unauthorized.");
+    }
+    return storageIo.searchUsers(startingPoint);
+  }
+
+  @Override
+  public void storeUser(AdminUser user) throws AdminInterfaceException {
+    if (!userInfoProvider.getIsAdmin()) {
+      throw new IllegalArgumentException("Unauthorized.");
+    }
+    // This is a bit of a kludge
+    // We hash the password here, replacing it in place
+    String password = user.getPassword();
+    String hashedPassword = "";
+    if (password != null && !password.equals("")) {
+      try {
+        hashedPassword = PasswordHash.createHash(password);
+        user.setPassword(hashedPassword);
+      } catch (NoSuchAlgorithmException e) {
+        throw new IllegalArgumentException("Error hashing password");
+      } catch (InvalidKeySpecException e) {
+        throw new IllegalArgumentException("Error hashing password");
+      }
+    }
+    storageIo.storeUser(user);
+  }
+
+  @Override
+  public void switchUser(AdminUser user) throws AdminInterfaceException {
+    if (!userInfoProvider.getIsAdmin()) {
+      throw new IllegalArgumentException("Unauthorized.");
+    }
+    // BEWARE THIS IS A HACK
+    // We are going to switch users and set the readOnly flag
+    // When this call returns we depend on the client side doing a complete
+    // reload. It will then get a new User object based on these updated
+    // session fields.
+    HttpSession session = localSession.getSession();
+    session.setAttribute("userid", user.getId());
+    session.setAttribute("readonly", true);
+  }
+}
---
+++ appinventor/appengine/src/com/google/appinventor/shared/rpc/admin/AdminUser.java
@@ -0,0 +1,130 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2015 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+package com.google.appinventor.shared.rpc.admin;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Data Transfer Object representing user data.
+ *
+ */
+public class AdminUser implements IsSerializable, Serializable {
+  // Unique identifier for the user
+  private String id;
+
+  // user email address
+  private String email;
+
+  // user display name
+  private String name;
+
+  // whether user has accepted terms of service
+  private boolean tosAccepted;
+
+  // Are they are Gallery Moderator?
+  private boolean isModerator;
+
+  // whether the user has admin priviledges
+  private boolean isAdmin;
+
+  // which type the user has
+  private int type;
+
+  private Date visited;         // When they last logged in
+
+  private String password;
+
+  /**
+   * Creates a new user data transfer object.
+   *
+   * @param id unique user id (from {@link com.google.appengine.api.users.User#getUserId()}
+   * @param name -- Their full name, if we have it (we often don't)
+   * @param email user email address
+   * @param tosAccepted TOS accepted?
+   * @param isAdmin -- are they an admin
+   * @param isModerator -- are they a gallery moderator
+   * @param visited -- when they last logged in.
+   */
+  public AdminUser(String id, String name, String email, boolean tosAccepted,
+    boolean isAdmin, boolean isModerator, Date visited) {
+    this.id = id;
+    this.email = email;
+    this.name = name;
+    this.tosAccepted = tosAccepted;
+    this.isAdmin = isAdmin;
+    this.isModerator = isModerator;
+    this.type = type;
+    this.visited = visited;
+  }
+
+  /**
+   * Default constructor. This constructor is required by GWT.
+   */
+  @SuppressWarnings("unused")
+  private AdminUser() {
+  }
+
+  /**
+   * Returns the user's unique id.
+   *
+   * @return user id
+   */
+
+  public String getId() {
+    return id;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public boolean getTosaccepted() {
+    return this.tosAccepted;
+  }
+
+  public boolean getIsAdmin() {
+    return this.isAdmin;
+  }
+
+  public void setIsAdmin(boolean value) {
+    this.isAdmin = value;
+  }
+
+  public boolean getIsModerator() {
+    return this.isModerator;
+  }
+
+  public void setIsModerator(boolean value) {
+    this.isModerator = value;
+  }
+
+  public int getType() {
+    return this.type;
+  }
+
+  public Date getVisited() {
+    return this.visited;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+}
