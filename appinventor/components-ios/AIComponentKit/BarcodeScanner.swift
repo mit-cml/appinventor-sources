e08f26ce64d3771a6c60ea9f8d55a412e0d83faa
Evan W. Patton <ewpatton@gmail.com>
2016-11-30 02:47:32 -0500

    Fix CPU background usage in BarcodeScanner
    Change-Id: I871167c73f8f892db34a58d07ccb54e9ee0a1614

--- appinventor/components-ios/AIComponentKit/BarcodeScanner.swift
+++ appinventor/components-ios/AIComponentKit/BarcodeScanner.swift
@@ -16,7 +16,6 @@ public protocol BarcodeScannerDelegate {
 class BarcodeScannerViewController: UIViewController, ZXCaptureDelegate {
   private var _capture: ZXCapture!
   private var _barcodeDelegate: BarcodeScannerDelegate!
-  private var _started = false
   private var _captureSizeTransform: CGAffineTransform
   private var _scanViewRect: UIView
 
@@ -56,11 +55,14 @@ class BarcodeScannerViewController: UIViewController, ZXCaptureDelegate {
 
   public override func viewDidLoad() {
     super.viewDidLoad()
-    NSLog("View did load")
     _capture = ZXCapture()
     _capture.camera = self._capture.back()
     _capture.focusMode = AVCaptureFocusMode.continuousAutoFocus
-    NSLog("sessionPreset = \(_capture.sessionPreset)")
+    _capture.delegate = self
+  }
+
+  public override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
     _scanViewRect.frame = CGRect(x: (3.0/32.0)*self.view.frame.width,
                                  y: (11.0/48.0)*self.view.frame.height,
                                  width: (13.0/16.0)*self.view.frame.width,
@@ -68,42 +70,42 @@ class BarcodeScannerViewController: UIViewController, ZXCaptureDelegate {
     _scanViewRect.autoresizingMask = UIViewAutoresizing.flexibleHeight.union(.flexibleWidth).union(.flexibleTopMargin).union(.flexibleBottomMargin)
     view.layer.addSublayer(self._capture.layer)
     self.view.addSubview(_scanViewRect)
-  }
-
-  public override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    NSLog("View will appear")
-    _capture.delegate = self
     applyOrientation()
-    _started = true
   }
 
   public override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
-    NSLog("View did appear")
-    if (!_started) {
-      self._capture.start()
+    if !self._capture.running {
+      _capture.delegate = self
+      _capture.start()
     }
   }
 
   public override func viewWillDisappear(_ animated: Bool) {
     super.viewWillDisappear(animated)
-    self._capture.stop()
-    _started = false
+    if self._capture.running {
+      _capture.stop()
+      _capture.delegate = nil
+      _capture.layer.removeFromSuperlayer()
+      _scanViewRect.removeFromSuperview()
+    }
   }
 
   public func captureResult(_ capture: ZXCapture!, result: ZXResult!) {
-    self._capture.stop()
-    _started = false
-    NSLog("Captured result: \(result)")
+    if self._capture.running {
+      _capture.stop()
+      _capture.delegate = nil
+    }
     AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
     self.dismiss(animated: true, completion: {})
     _barcodeDelegate.receivedResult(result: result.text)
   }
 
   func cancel() {
-    self._capture.stop()
-    _started = false
+    if self._capture.running {
+      _capture.stop()
+      _capture.delegate = nil
+    }
     self.dismiss(animated: true, completion: {})
     _barcodeDelegate.canceled()
   }
@@ -156,7 +158,6 @@ class BarcodeScannerViewController: UIViewController, ZXCaptureDelegate {
     var scaleVideo: CGFloat, scaleVideoX: CGFloat, scaleVideoY: CGFloat
     var videoSizeX: CGFloat, videoSizeY: CGFloat
     var transformedVideoRect = self._scanViewRect.frame;
-    NSLog("sessionPreset = \(self._capture.sessionPreset)")
     if(self._capture.sessionPreset == AVCaptureSessionPreset1920x1080) {
       videoSizeX = 1080;
       videoSizeY = 1920;
@@ -164,9 +165,6 @@ class BarcodeScannerViewController: UIViewController, ZXCaptureDelegate {
       videoSizeX = 720;
       videoSizeY = 1280;
     }
-    NSLog("videoSizeX = \(videoSizeX)   videoSizeY = \(videoSizeY)")
-    NSLog("self.view.frame = \(self.view.frame)")
-    NSLog("self._scanView.frame = \(self._scanViewRect.frame)")
     if(UIInterfaceOrientationIsPortrait(orientation)) {
       scaleVideoX = self.view.frame.size.width / videoSizeX;
       scaleVideoY = self.view.frame.size.height / videoSizeY;
@@ -187,9 +185,7 @@ class BarcodeScannerViewController: UIViewController, ZXCaptureDelegate {
       }
     }
     _captureSizeTransform = CGAffineTransform(scaleX: 1/scaleVideo, y: 1/scaleVideo);
-    NSLog("_captureSizeTransform = \(_captureSizeTransform)")
     self._capture.scanRect = transformedVideoRect.applying(_captureSizeTransform);
-    NSLog("Capture rect: \(_capture.scanRect)")
   }
 }
 
@@ -234,7 +230,6 @@ public class BarcodeScanner: NonvisibleComponent, BarcodeScannerDelegate {
   }
 
   public func receivedResult(result: String) {
-    NSLog("received result \(result)")
     _result = result
     self.performSelector(onMainThread: #selector(BarcodeScanner.AfterScan(result:)), with: _result, waitUntilDone: false)
   }
