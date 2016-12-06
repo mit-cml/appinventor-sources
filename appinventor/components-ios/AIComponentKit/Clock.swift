c64a5de4226954d48c3e8c24236aa2aeba7eff87
Evan W. Patton <ewpatton@gmail.com>
2016-12-06 12:02:18 -0500

    Add timer behavior for Clock component
    Change-Id: I06505e274e83a38381be4d3dedeb7e55f2ab56f5

--- appinventor/components-ios/AIComponentKit/Clock.swift
+++ appinventor/components-ios/AIComponentKit/Clock.swift
@@ -14,11 +14,11 @@ public class Clock: NonvisibleComponent {
   private var _enabled = true
   private var _alwaysFires = true
   private var _onScreen = false
-  
+
   public override init(_ container: ComponentContainer) {
     super.init(container)
   }
-  
+
   // MARK: Clock Properties
   public var TimerInterval: Int32 {
     get {
@@ -28,16 +28,25 @@ public class Clock: NonvisibleComponent {
       _interval = interval
     }
   }
-  
+
   public var TimerEnabled: Bool {
     get {
       return _enabled
     }
     set(enabled) {
       _enabled = enabled
+      if _enabled {
+        if let timer = _timer {
+          timer.invalidate()
+        }
+        _timer = Foundation.Timer(timeInterval: TimeInterval(Double(_interval) / 1000.0), target: self, selector: #selector(self.timerFired(_:)), userInfo: nil, repeats: true)
+      } else if let timer = _timer {
+        timer.invalidate()
+        _timer = nil
+      }
     }
   }
-  
+
   public var TimeAlwaysFires: Bool {
     get {
       return _alwaysFires
@@ -46,20 +55,24 @@ public class Clock: NonvisibleComponent {
       _alwaysFires = always
     }
   }
-  
+
   // MARK: Clock Methods
   public func SystemTime() -> Int64 {
     return Int64(Date().timeIntervalSince1970 * 1000.0)
   }
-  
+
   public func Now() -> Date {
     return Date()
   }
-  
+
   // MARK: Clock Events
   public func Timer() {
     if (_alwaysFires || _onScreen) {
       EventDispatcher.dispatchEvent(of: self, called: "Timer")
     }
+  }
+
+  func timerFired(_ timer: Timer) {
+    self.performSelector(onMainThread: #selector(self.Timer), with: nil, waitUntilDone: false)
   }
 }
