ea800e079efb6ce6dfb20ccdcdb001af3dc04fc6
Evan W. Patton <ewpatton@gmail.com>
2016-12-16 19:55:29 -0500

    Add rgbArrayToInt32 to ColorUtil
    Change-Id: Iefa401f197bee3eb61e78fe8220618e0ca4debe7

--- appinventor/components-ios/AIComponentKit/ColorUtil.swift
+++ appinventor/components-ios/AIComponentKit/ColorUtil.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-func argbToColor( _ argb: Int32) -> UIColor {
+public func argbToColor( _ argb: Int32) -> UIColor {
   var argb = argb
   var r: Float = 0.0, g: Float = 0.0, b: Float = 0.0, a: Float = 0.0
   b = Float(argb & 0xFF) / 255.0
@@ -21,7 +21,7 @@ func argbToColor( _ argb: Int32) -> UIColor {
   return UIColor(colorLiteralRed: r, green: g, blue: b, alpha: a)
 }
 
-func colorToArgb(_ color: UIColor) -> Int32 {
+public func colorToArgb(_ color: UIColor) -> Int32 {
   var argb: Int32 = 0
   var r: CGFloat = 0.0, g: CGFloat = 0.0, b: CGFloat = 0.0, a: CGFloat = 0.0
   color.getRed(&r, green: &g, blue: &b, alpha: &a)
@@ -32,5 +32,20 @@ func colorToArgb(_ color: UIColor) -> Int32 {
   argb |= ((Int32)(255 * g))
   argb <<= 8
   argb |= ((Int32)(255 * b))
+  return argb
+}
+
+public func rgbArrayToInt32(_ rgb: [Int]) -> Int32 {
+  let a = rgb.count == 4 ? rgb[0] : 0xFF
+  let rgb = rgb.suffix(from: 1)
+  let r = rgb[0], g = rgb[1], b = rgb[2]
+  var argb: Int32 = 0
+  argb |= ((Int32)(a & 0xFF))
+  argb <<= 8
+  argb |= ((Int32)(r & 0xFF))
+  argb <<= 8
+  argb |= ((Int32)(g & 0xFF))
+  argb <<= 8
+  argb |= ((Int32)(b & 0xFF))
   return argb
 }
