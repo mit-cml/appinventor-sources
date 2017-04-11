b88659f72bb1de042f535431fd9d50d3a0f3db4b
Klomi <2947133416@qq.com>
2017-04-11 17:51:28 +0800

    Ensure key "package" is defined in the JSON before using the corresponding value

--- appinventor/blocklyeditor/src/replmgr.js
+++ appinventor/blocklyeditor/src/replmgr.js
@@ -373,7 +373,7 @@ Blockly.ReplMgr.putYail = (function() {
                         return;
                     } else {
                         var json = goog.json.parse(this.response);
-                        if (!Blockly.ReplMgr.acceptablePackage(json["package"])) {
+                        if (json["package"] != undefined && !Blockly.ReplMgr.acceptablePackage(json["package"])) {
                             dialog = new Blockly.Util.Dialog(Blockly.Msg.REPL_COMPANION_VERSION_CHECK,
                                                              Blockly.Msg.REPL_COMPANION_WRONG_PACKAGE,
                                                              Blockly.Msg.REPL_OK, null, 0, function() {
