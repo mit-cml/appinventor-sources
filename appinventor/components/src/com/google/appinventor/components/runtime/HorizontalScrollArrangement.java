5cfb1af80df76e2119c9c8937fe6f3a22579d999
ColinTree <502470184@qq.com>
2017-09-25 22:04:11 +0800

    added scrolling handlers of ScrollArrangement & Chinese Translation of it
    - Add events and methods that listens to & control the ScrollView (for both VerticalScrollArrangement and HorizontalScrollArrangement)
    - Component version upgraded (1->2 no block need to upgrade)
    - Chinese Simplifed Translation for these is ready

--- appinventor/components/src/com/google/appinventor/components/runtime/HorizontalScrollArrangement.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/HorizontalScrollArrangement.java
@@ -7,17 +7,27 @@ package com.google.appinventor.components.runtime;
 
 import com.google.appinventor.components.annotations.DesignerComponent;
 import com.google.appinventor.components.annotations.SimpleObject;
+import com.google.appinventor.components.annotations.SimpleFunction;
+import com.google.appinventor.components.annotations.SimpleEvent;
+import com.google.appinventor.components.annotations.SimpleProperty;
+import com.google.appinventor.components.annotations.PropertyCategory;
 import com.google.appinventor.components.common.ComponentCategory;
 import com.google.appinventor.components.common.ComponentConstants;
 import com.google.appinventor.components.common.YaVersion;
+
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.view.ViewTreeObserver.*;
 
 /**
  * A horizontal arrangement of components
  * @author sharon@google.com (Sharon Perl)
  * @author jis@mit.edu (Jeffrey I. Schiller)
  *
+ * events and methods about scrolling
+ * @author 502470184@qq.com (ColinTree, YANG)
  */
-@DesignerComponent(version = YaVersion.HORIZONTALSCROLLARRANGEMENT_COMPONENT_VERSION,
+@DesignerComponent(version = YaVersion.HORIZONTALSCROLLARRANGEMENT_COMPONENT_VERSION, 
     description = "<p>A formatting element in which to place components " +
     "that should be displayed from left to right.  If you wish to have " +
     "components displayed one over another, use " +
@@ -25,10 +35,134 @@ import com.google.appinventor.components.common.YaVersion;
     "scrollable.",
     category = ComponentCategory.LAYOUT)
 @SimpleObject
-public class HorizontalScrollArrangement extends HVArrangement {
-  public HorizontalScrollArrangement(ComponentContainer container) {
-    super(container, ComponentConstants.LAYOUT_ORIENTATION_HORIZONTAL,
-      ComponentConstants.SCROLLABLE_ARRANGEMENT);
-  }
+public class HorizontalScrollArrangement extends HVArrangement implements OnScrollChangedListener {
+    public HorizontalScrollArrangement(ComponentContainer container) {
+        super(container, ComponentConstants.LAYOUT_ORIENTATION_HORIZONTAL,
+            ComponentConstants.SCROLLABLE_ARRANGEMENT);
+        getView().getViewTreeObserver().addOnScrollChangedListener(this);
+    }
 
+    private float deviceDensity() {
+        return container.$form().deviceDensity();
+    }
+
+    private int px2dx(int px) {
+        return Math.round(px * deviceDensity());
+    }
+    
+    private int dx2px(int dx) {
+        return Math.round(dx / deviceDensity());
+    }
+
+    private HorizontalScrollView getScrollView() {
+        return (HorizontalScrollView) getView();
+    }
+
+    private int oldScrollX = 0;
+
+    @Override
+    public void onScrollChanged() {
+        int scrollX = dx2px(getView().getScrollX());
+        if (scrollX < 0) {
+            scrollX = 0;
+        }
+        if (oldScrollX != scrollX) {
+            ScrollChanged(scrollX);
+            oldScrollX = scrollX;
+        }
+    }
+
+    @SimpleEvent(description = "Notice that if blocks like \"ScrollBy\" is excuted, this event would be called")
+    public void ScrollChanged(int scrollPosition) {
+        EventDispatcher.dispatchEvent(this, "ScrollChanged", scrollPosition);
+        if (scrollPosition == 0) {
+            ReachLeftEnd();
+        } else {
+            if (MaxScrollPosition() - ScrollPosition() <= 0) {
+                ReachRightEnd();
+            }
+        }
+    }
+
+    @SimpleEvent(description = "Notice that if blocks like \"ScrollLeftEnd\" is excuted, this event would be called")
+    public void ReachLeftEnd() {
+        EventDispatcher.dispatchEvent(this, "ReachLeftEnd");
+    }
+
+    @SimpleEvent(description = "Notice that if blocks like \"ScrollRightEnd\" is excuted, this event would be called")
+    public void ReachRightEnd() {
+        EventDispatcher.dispatchEvent(this, "ReachRightEnd");
+    }
+
+    @SimpleProperty(category = PropertyCategory.APPEARANCE, 
+        description = "The scroll position is the same as the number of pixels that "
+            + "are hidden from view above the scrollable area. "
+            + "If the scroll bar is at the very left, or if the element is not scrollable, this number will be 0.")
+    public int ScrollPosition() {
+        int dxPosition = getView().getScrollX();
+        if (dxPosition < 0) {
+            return 0;
+        } else if (dxPosition > MaxScrollPosition()) {
+            return MaxScrollPosition();
+        }
+        return dx2px(dxPosition);
+    }
+
+    @SimpleProperty(category = PropertyCategory.APPEARANCE, 
+        description = "Return the maximum position that the ScrollArrangement can reach")
+    public int MaxScrollPosition() {
+        View view = (View) getScrollView().getChildAt(getScrollView().getChildCount() - 1);
+        return dx2px(view.getRight() - getView().getWidth());
+    }
+
+    @SimpleFunction(description = "Scroll to the left end of the scroll arrangement (defaut with animation)")
+    public void ScrollLeftEnd() {
+        getScrollView().fullScroll(HorizontalScrollView.FOCUS_LEFT);
+    }
+
+    @SimpleFunction(description = "Scroll to the right end of the scroll arrangement (defaut with animation)")
+    public void ScrollRightEnd() {
+        getScrollView().fullScroll(HorizontalScrollView.FOCUS_RIGHT);
+    }
+
+    @SimpleFunction(description = "Scroll leftward for half a page (defaut with animation)")
+    public void ArrowScrollLeftward() {
+        getScrollView().arrowScroll(HorizontalScrollView.FOCUS_LEFT);
+    }
+
+    @SimpleFunction(description = "Scroll rightward for half a page (defaut with animation)")
+    public void ArrowScrollRightward() {
+        getScrollView().arrowScroll(HorizontalScrollView.FOCUS_RIGHT);
+    }
+
+    @SimpleFunction(description = "Scroll leftward for a full page (defaut with animation)")
+    public void PageScrollLeftward() {
+        getScrollView().pageScroll(HorizontalScrollView.FOCUS_LEFT);
+    }
+
+    @SimpleFunction(description = "Scroll rightward for a full page (defaut with animation)")
+    public void PageScrollRightward() {
+        getScrollView().pageScroll(HorizontalScrollView.FOCUS_RIGHT);
+    }
+
+    @SimpleFunction(description = "Scroll to a specific position (without animation)")
+    public void ScrollTo(int position) {
+        getScrollView().scrollTo(px2dx(position), 0);
+    }
+
+    @SimpleFunction(description = "Scroll to a specific position (with animation)")
+    public void SmoothScrollTo(int position) {
+        getScrollView().smoothScrollTo(px2dx(position), 0);
+    }
+
+    @SimpleFunction(description = "Scroll rightward so for a specific displacement, scroll leftward if diaplacement is negative (without animation)")
+    public void ScrollBy(int displacement) {
+        getScrollView().scrollBy(px2dx(displacement), 0);
+    }
+
+    @SimpleFunction(description = "Scroll rightward so for a specific displacement, scroll leftward if diaplacement is negative (with animation)")
+    public void SmoothScrollBy(int displacement) {
+        getScrollView().smoothScrollBy(px2dx(displacement), 0);
+    }
+    
 }
--- appinventor/components/src/com/google/appinventor/components/runtime/VerticalScrollArrangement.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/VerticalScrollArrangement.java
@@ -7,15 +7,25 @@ package com.google.appinventor.components.runtime;
 
 import com.google.appinventor.components.annotations.DesignerComponent;
 import com.google.appinventor.components.annotations.SimpleObject;
+import com.google.appinventor.components.annotations.SimpleFunction;
+import com.google.appinventor.components.annotations.SimpleEvent;
+import com.google.appinventor.components.annotations.SimpleProperty;
+import com.google.appinventor.components.annotations.PropertyCategory;
 import com.google.appinventor.components.common.ComponentCategory;
 import com.google.appinventor.components.common.ComponentConstants;
 import com.google.appinventor.components.common.YaVersion;
+
+import android.view.View;
+import android.widget.ScrollView;
+import android.view.ViewTreeObserver.*;
 
 /**
  * A vertical arrangement of components
  * @author sharon@google.com (Sharon Perl)
  * @author jis@mit.edu (Jeffrey I. Schiller)
  *
+ * events and methods about scrolling
+ * @author 502470184@qq.com (ColinTree, YANG)
  */
 
 @DesignerComponent(version = YaVersion.VERTICALSCROLLARRANGEMENT_COMPONENT_VERSION,
@@ -27,11 +37,134 @@ import com.google.appinventor.components.common.YaVersion;
     "This version is scrollable",
     category = ComponentCategory.LAYOUT)
 @SimpleObject
-public class VerticalScrollArrangement extends HVArrangement {
+public class VerticalScrollArrangement extends HVArrangement implements OnScrollChangedListener {
+    public VerticalScrollArrangement(ComponentContainer container) {
+        super(container, ComponentConstants.LAYOUT_ORIENTATION_VERTICAL,
+            ComponentConstants.SCROLLABLE_ARRANGEMENT);
+        getView().getViewTreeObserver().addOnScrollChangedListener(this);
+    }
 
-  public VerticalScrollArrangement(ComponentContainer container) {
-    super(container, ComponentConstants.LAYOUT_ORIENTATION_VERTICAL,
-      ComponentConstants.SCROLLABLE_ARRANGEMENT);
-  }
+    private float deviceDensity() {
+        return container.$form().deviceDensity();
+    }
+
+    private int px2dx(int px) {
+        return Math.round(px * deviceDensity());
+    }
+    
+    private int dx2px(int dx) {
+        return Math.round(dx / deviceDensity());
+    }
+
+    private ScrollView getScrollView() {
+        return (ScrollView) getView();
+    }
+
+    private int oldScrollY = 0;
+
+    @Override
+    public void onScrollChanged() {
+        int scrollY = dx2px(getView().getScrollY());
+        if (scrollY < 0) {
+            scrollY = 0;
+        }
+        if (oldScrollY != scrollY) {
+            ScrollChanged(scrollY);
+            oldScrollY = scrollY;
+        }
+    }
+
+    @SimpleEvent(description = "Notice that if blocks like \"ScrollBy\" is excuted, this event would be called")
+    public void ScrollChanged(int scrollPosition) {
+        EventDispatcher.dispatchEvent(this, "ScrollChanged", scrollPosition);
+        if (scrollPosition == 0) {
+            ReachTop();
+        } else {
+            if (MaxScrollPosition() - ScrollPosition() <= 0) {
+                ReachBottom();
+            }
+        }
+    }
+
+    @SimpleEvent(description = "Notice that if blocks like \"ScrollTop\" is excuted, this event would be called")
+    public void ReachTop() {
+        EventDispatcher.dispatchEvent(this, "ReachTop");
+    }
+
+    @SimpleEvent(description = "Notice that if blocks like \"ScrollBottom\" is excuted, this event would be called")
+    public void ReachBottom() {
+        EventDispatcher.dispatchEvent(this, "ReachBottom");
+    }
+
+    @SimpleProperty(category = PropertyCategory.APPEARANCE, 
+        description = "The scroll position is the same as the number of pixels that "
+            + "are hidden from view above the scrollable area. "
+            + "If the scroll bar is at the very top, or if the element is not scrollable, this number will be 0.")
+    public int ScrollPosition() {
+        int dxPosition = getView().getScrollY();
+        if (dxPosition < 0) {
+            return 0;
+        } else if (dxPosition > MaxScrollPosition()) {
+            return MaxScrollPosition();
+        }
+        return dx2px(dxPosition);
+    }
+
+    @SimpleProperty(category = PropertyCategory.APPEARANCE, 
+        description = "Return the maximum position that the ScrollArrangement can reach")
+    public int MaxScrollPosition() {
+        View view = (View) getScrollView().getChildAt(getScrollView().getChildCount() - 1);
+        return dx2px(view.getBottom() - getView().getHeight());
+    }
+
+    @SimpleFunction(description = "Scroll to the top of the scroll arrangement (defaut with animation)")
+    public void ScrollTop() {
+        getScrollView().fullScroll(ScrollView.FOCUS_UP);
+    }
+
+    @SimpleFunction(description = "Scroll to the bottom of the scroll arrangement (defaut with animation)")
+    public void ScrollBottom() {
+        getScrollView().fullScroll(ScrollView.FOCUS_DOWN);
+    }
+
+    @SimpleFunction(description = "Scroll upward for half a page (defaut with animation)")
+    public void ArrowScrollUpward() {
+        getScrollView().arrowScroll(ScrollView.FOCUS_UP);
+    }
+
+    @SimpleFunction(description = "Scroll downward for half a page (defaut with animation)")
+    public void ArrowScrollDownward() {
+        getScrollView().arrowScroll(ScrollView.FOCUS_DOWN);
+    }
+
+    @SimpleFunction(description = "Scroll upward for a full page (defaut with animation)")
+    public void PageScrollUpward() {
+        getScrollView().pageScroll(ScrollView.FOCUS_UP);
+    }
+
+    @SimpleFunction(description = "Scroll downward for a full page (defaut with animation)")
+    public void PageScrollDownward() {
+        getScrollView().pageScroll(ScrollView.FOCUS_DOWN);
+    }
+
+    @SimpleFunction(description = "Scroll to a specific position (without animation)")
+    public void ScrollTo(int position) {
+        getScrollView().scrollTo(0, px2dx(position));
+    }
+
+    @SimpleFunction(description = "Scroll to a specific position (with animation)")
+    public void SmoothScrollTo(int position) {
+        getScrollView().smoothScrollTo(0, px2dx(position));
+    }
+
+    @SimpleFunction(description = "Scroll downward so for a specific displacement, scroll upward if diaplacement is negative (without animation)")
+    public void ScrollBy(int displacement) {
+        getScrollView().scrollBy(0, px2dx(displacement));
+    }
+
+    @SimpleFunction(description = "Scroll downward so for a specific displacement, scroll upward if diaplacement is negative (with animation)")
+    public void SmoothScrollBy(int displacement) {
+        getScrollView().smoothScrollBy(0, px2dx(displacement));
+    }
 
 }
