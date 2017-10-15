18296e18c0c74e4a625fa7efb44f64382fa029e5
kgarner <usernameyoudonthave@gmail.com>
2017-10-15 16:03:00 -0400

    Add PhoneNumberPicker
    Change-Id: I88f9cfde9bc929b517bf9afb56623c4fa7d02478

---
+++ appinventor/components-ios/AIComponentKit/PhoneNumberPicker.swift
@@ -0,0 +1,12 @@
+//
+//  PhoneNumberPicker.swift
+//  AIComponentKit
+//
+//  Created by Kendall Garner on 10/12/17.
+//  Copyright © 2017 MIT Center for Mobile Learning. All rights reserved.
+//
+
+
+open class PhoneNumberPicker: ContactPicker {
+    
+}
