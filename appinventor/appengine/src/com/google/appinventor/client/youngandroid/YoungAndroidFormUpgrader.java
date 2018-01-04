c7fecd2f0717b7f15c4f755b5139db3abbb279f0
ColinTree <502470184@qq.com>
2018-01-04 21:18:28 +0800

    remove false as it is a new default value in upgrader

--- appinventor/appengine/src/com/google/appinventor/client/youngandroid/YoungAndroidFormUpgrader.java
+++ appinventor/appengine/src/com/google/appinventor/client/youngandroid/YoungAndroidFormUpgrader.java
@@ -1142,7 +1142,11 @@ public final class YoungAndroidFormUpgrader {
     }
     if (srcCompVersion < 4) {
       // Make location sensor disable by default
-      if (!componentProperties.containsKey("Enabled")) {
+      if (componentProperties.containsKey("Enabled")) {
+        if (componentProperties.get("Enabled").asString().getString().toLowerCase().equals("false")) {
+          componentProperties.remove("Enabled");
+        }
+      } else {
         componentProperties.put("Enabled", new ClientJsonString("True"));
       }
       srcCompVersion = 4;
