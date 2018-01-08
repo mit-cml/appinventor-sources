2a6b00f9147b11349aae89521f310b5956957831
Jeffrey I. Schiller <jis@mit.edu>
2018-01-08 15:50:51 -0500

    Update Companion Version to 2.46
    Change-Id: Ib0a330ed53fb3fed1f8a128908026602fc058d74

--- appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
+++ appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
@@ -1155,10 +1155,10 @@ public class YaVersion {
 
   public static final String ACCEPTABLE_COMPANION_PACKAGE = "edu.mit.appinventor.aicompanion3";
 
-  public static final String PREFERRED_COMPANION = "2.45";
+  public static final String PREFERRED_COMPANION = "2.46";
   public static final String COMPANION_UPDATE_URL = "";
   public static final String COMPANION_UPDATE_URL1 = "";
-  public static final String [] ACCEPTABLE_COMPANIONS = { "2.45" };
+  public static final String [] ACCEPTABLE_COMPANIONS = { "2.46" };
 
   // Splash Screen Values
   public static final int SPLASH_SURVEY = 1;
