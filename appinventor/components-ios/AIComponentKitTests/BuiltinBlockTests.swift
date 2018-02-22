a9136e9befa7f563b6bec5181747485f5a8fccfb
Evan W. Patton <ewpatton@mit.edu>
2018-02-22 11:42:47 -0500

    Add block test apps
    Closes mit-cml/appinventor-sources-ios#35
    
    Change-Id: Ifc33d2b8d49a91ec651b549e3c8b03ae5b5af958

---
+++ appinventor/components-ios/AIComponentKitTests/BuiltinBlockTests.swift
@@ -0,0 +1,113 @@
+// -*- mode: swift; swift-mode:basic-offset: 2; -*-
+// Copyright © 2017 Massachusetts Institute of Technology, All rights reserved.
+
+import XCTest
+@testable import AIComponentKit
+
+/**
+ * Reads the contents of the file at the given pathname into the given interpreter. The list of
+ * defined test procedure names is returned to facilitate testing.
+ *
+ * @param path The path to the test YAIL file
+ * @param interpreter The interpreter that will receive and evaluate the YAIL
+ * @returns A list of the procedure names for evaluation
+ */
+public func loadTestYail(path: String, into interpreter: SCMInterpreter) throws -> [String] {
+  let subpath = path.substring(to: path.index(path.endIndex, offsetBy: -5))
+  if let url = Bundle(for: BuiltinBlockTests.self).url(forResource: subpath, withExtension: "yail") {
+    do {
+      let yail = try String(contentsOf: url, encoding: String.Encoding.utf8)
+      interpreter.evalForm(yail)
+      if let exception = interpreter.exception {
+        XCTFail("Exception: \(exception.name.rawValue) (\(exception))")
+        throw TestFailure()
+      }
+      let regex = try! NSRegularExpression(pattern: "p\\$test_[^\\s)]+", options: [])
+      var tests = [String]()
+      let matches = regex.matches(in: yail, options: [], range: NSRange(location: 0, length: yail.count))
+      for match in matches {
+        tests.append(yail[Range(match.range, in: yail)!])
+      }
+      return tests
+    } catch {
+      XCTFail("Unable to load \(path)")
+      throw TestFailure()
+    }
+  } else {
+    throw TestFailure()
+  }
+}
+
+func optionallyPrint(_ exception: NSException?) -> String {
+  if let exception = exception {
+    return " Exception was thrown: \(exception.name.rawValue) (\(exception))"
+  } else {
+    return ""
+  }
+}
+
+var coverage = false
+var coverageData = [String: [String: Bool]]()
+
+class BuiltinBlockTests: XCTestCase {
+
+  func runTestsFromYail(path: String) throws {
+    var passing: Int32 = 0, failing: Int32 = 0
+    let interpreter = try getInterpreterForTesting()
+    let testNames = try loadTestYail(path: path, into: interpreter)
+    for test in testNames {
+      let result = interpreter.evalForm("((get-var \(test)))")
+      if result == "#t" && interpreter.exception == nil {
+        passing += 1
+      } else {
+        failing += 1
+      }
+      if coverage {
+        let block = test.suffix(from: test.index(test.startIndex, offsetBy: 7))
+        var parts = block.split(separator: "_", maxSplits: 1, omittingEmptySubsequences: false)
+        if parts.count == 1 {
+          parts.append("text")
+        } else if parts[0] == "obfuscated" {
+          parts[1] = "obfuscated"
+          parts[0] = "text"
+        }
+        let key = String(parts[0])
+        if var data = coverageData[key] {
+          data[String(block)] = result == "#t" && interpreter.exception == nil
+        } else {
+          coverageData[key] = [String(block): result == "#t" && interpreter.exception == nil]
+        }
+      } else {
+        XCTAssertTrue(result == "#t" && interpreter.exception == nil,
+                      "\(test) failed." + optionallyPrint(interpreter.exception))
+      }
+      interpreter.clearException()
+    }
+    print("Passed: \(passing)")
+    print("Failed: \(failing)")
+  }
+
+  func testControlBlocks() throws {
+    try runTestsFromYail(path: "control.yail")
+  }
+
+  func testLogicBlocks() throws {
+    try runTestsFromYail(path: "logic.yail")
+  }
+
+  func testMathBlocks() throws {
+//    try runTestsFromYail(path: "math.yail")
+  }
+
+  func testTextBlocks() throws {
+//    try runTestsFromYail(path: "text.yail")
+  }
+
+  func testListBlocks() throws {
+//    try runTestsFromYail(path: "lists.yail")
+  }
+
+  func testColorBlocks() throws {
+    try runTestsFromYail(path: "colors.yail")
+  }
+}
---
+++ appinventor/components-ios/AIComponentKitTests/CsvUtilTests.swift
@@ -0,0 +1,9 @@
+//
+//  CsvUtilTests.swift
+//  AIComponentKitTests
+//
+//  Created by Evan Patton on 2/21/18.
+//  Copyright © 2018 MIT Center for Mobile Learning. All rights reserved.
+//
+
+import Foundation
