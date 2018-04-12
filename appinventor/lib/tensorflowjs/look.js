95013b2928da6af51b6f58995277267f998e34d4
Kevin <kevin-vr@users.noreply.github.com>
2018-04-12 16:40:42 -0400

    update tfjs and change quotes

--- appinventor/lib/tensorflowjs/look.js
+++ appinventor/lib/tensorflowjs/look.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-console.log('Look: Using Tensorflow.js version ' + tf.version.tfjs);
+console.log("Look: Using Tensorflow.js version " + tf.version.tfjs);
 
-// const MOBILENET_MODEL_PATH = 'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json';
-const MOBILENET_MODEL_PATH = 'model.json';
+// const MOBILENET_MODEL_PATH = "https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json";
+const MOBILENET_MODEL_PATH = "model.json";
 
 const IMAGE_SIZE = 224;
 const TOPK_PREDICTIONS = 10;
@@ -14,7 +14,7 @@ const mobilenetDemo = async () => {
   const zeros = tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
   mobilenet.predict(zeros).dispose();
   zeros.dispose();
-  console.log('Look: Mobilenet ready');
+  console.log("Look: Mobilenet ready");
   Look.ready();
 };
 
@@ -32,7 +32,7 @@ async function predict(imgElement) {
   for (let i = 0; i < classes.length; i++) {
     result.push([classes[i].className, classes[i].probability.toFixed(5)]);
   }
-  console.log('Look: prediction is ' + JSON.stringify(result));
+  console.log("Look: prediction is " + JSON.stringify(result));
   Look.reportResult(JSON.stringify(result));
 }
 
@@ -61,11 +61,11 @@ async function getTopKClasses(logits, topK) {
   return topClassesAndProbs;
 }
 
-var video = document.createElement('video');
-video.setAttribute('autoplay', '');
-video.setAttribute('playsinline', '');
+var video = document.createElement("video");
+video.setAttribute("autoplay", "");
+video.setAttribute("playsinline", "");
 video.width = 500;
-video.style.display = 'none';
+video.style.display = "none";
 
 var frontFacing = true;
 var isPlaying = false;
@@ -75,22 +75,22 @@ var img = new Image();
 img.width = 500;
 
 var isImageShowing = true;
-img.style.display = 'block';
+img.style.display = "block";
 
 document.body.appendChild(video);
 document.body.appendChild(img);
 
-video.addEventListener('loadedmetadata', function () {
+video.addEventListener("loadedmetadata", function () {
     video.height = this.videoHeight * video.width / this.videoWidth;
 }, false);
 
 function startVideo() {
   if (!isPlaying && isVideoMode) {
-    navigator.mediaDevices.getUserMedia({video: {facingMode: frontFacing ? 'user' : 'environment'}, audio: false})
+    navigator.mediaDevices.getUserMedia({video: {facingMode: frontFacing ? "user" : "environment"}, audio: false})
     .then(stream => (video.srcObject = stream))
     .catch(e => log(e));
     isPlaying = true;
-    video.style.display = 'block';
+    video.style.display = "block";
   }
 }
 
@@ -98,7 +98,7 @@ function stopVideo() {
   if (isPlaying && isVideoMode && video.srcObject) {
     video.srcObject.getTracks().forEach(t => t.stop());
     isPlaying = false;
-    video.style.display = 'none';
+    video.style.display = "none";
   }
 }
 
@@ -119,30 +119,30 @@ function classifyImageData(imageData) {
     img.onload = function() {
       predict(img);
     }
-    img.src = 'data:image/png;base64,' + imageData;
+    img.src = "data:image/png;base64," + imageData;
   }
 }
 
 function showImage() {
   if (!isImageShowing && !isVideoMode) {
-    img.style.display = 'block';
+    img.style.display = "block";
     isImageShowing = true;
   }
 }
 
 function hideImage() {
   if (isImageShowing) {
-    img.style.display = 'none';
+    img.style.display = "none";
     isImageShowing = false;
   }
 }
 
 function setInputMode(inputMode) {
-  if (inputMode == 'image' && isVideoMode) {
+  if (inputMode == "image" && isVideoMode) {
     stopVideo();
     isVideoMode = false;
     showImage();
-  } else if (inputMode == 'video' && !isVideoMode) {
+  } else if (inputMode == "video" && !isVideoMode) {
     hideImage();
     isVideoMode = true;
     startVideo();
