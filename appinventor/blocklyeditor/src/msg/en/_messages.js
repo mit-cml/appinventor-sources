e577bcb6d0099df42520894f7471b2a4a83c250f
Conor Shipp <conorshipp22@gmail.com>
2018-05-05 19:59:00 +0100

    Change "OK" button to "Reset" in Hard Reset dialog
    Resolves #1211

--- appinventor/blocklyeditor/src/msg/en/_messages.js
+++ appinventor/blocklyeditor/src/msg/en/_messages.js
@@ -1392,6 +1392,7 @@ Blockly.Msg.en.switch_language_to_english = {
     Blockly.Msg.REPL_OK_LOWER = "Ok";
     Blockly.Msg.REPL_GOT_IT = "Got It";
     Blockly.Msg.REPL_UPDATE_INFO = 'The update is now being installed on your device. Watch your device (or emulator) screen and approve the software installation when prompted.<br /><br />IMPORTANT: When the update finishes, choose "DONE" (don\'t click "open"). Then go to App Inventor in your web browser, click the "Connect" menu and choose "Reset Connection".  Then reconnect the device.';
+    Blockly.Msg.REPL_RESET = "Reset";
 
     Blockly.Msg.REPL_UPDATE_NO_UPDATE = "No Update is Available";
     Blockly.Msg.REPL_UPDATE_NO_CONNECTION = "You must be connected to a Companion to update it";
