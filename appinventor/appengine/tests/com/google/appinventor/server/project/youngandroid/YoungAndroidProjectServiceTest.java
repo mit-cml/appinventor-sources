bf20dbf1dbdd0aec533ac0df6138a256d5ab439a
Farzeen Harunani <farzeen@mit.edu>
2018-06-26 13:04:00 -0400

    Changes requested from Evan

--- appinventor/appengine/tests/com/google/appinventor/server/project/youngandroid/YoungAndroidProjectServiceTest.java
+++ appinventor/appengine/tests/com/google/appinventor/server/project/youngandroid/YoungAndroidProjectServiceTest.java
@@ -1,6 +1,5 @@
 // -*- mode: java; c-basic-offset: 2; -*-
-// Copyright 2009-2011 Google, All Rights reserved
-// Copyright 2011-2012 MIT, All rights reserved
+// Copyright 2011-2018 MIT, All rights reserved
 // Released under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 
@@ -9,8 +8,9 @@ package com.google.appinventor.server.project.youngandroid;
 import junit.framework.TestCase;
 import org.junit.Test;
 
-/* Testing the case of a null build URL */
 public class YoungAndroidProjectServiceTest extends TestCase {
+	
+/* Testing the case of a null build URL */
   @Test
   public void testBuildErrorMsgDoesntThrowNPE() {
   	YoungAndroidProjectService obj = new YoungAndroidProjectService(null); 
