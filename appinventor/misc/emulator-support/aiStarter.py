8ffb8779f525125ae7ddaddb231dc8579ceb4a39
Pavitra <pavitragolchha@gmail.com>
2018-07-05 19:54:13 +0530

    Update

--- appinventor/misc/emulator-support/aiStarter.py
+++ appinventor/misc/emulator-support/aiStarter.py
@@ -7,20 +7,24 @@ import sys
 
 from bottle import run, route, response
 
-VERSION = "2.2"
+VERSION = '2.2'
 
 OS = platform.system()
 
-if OS == 'Linux':       # Linux
+if OS == 'Linux':     # Linux
     PLATDIR = '/usr/google/appinventor/commands-for-Appinventor'
-elif OS == 'Darwin':    # MacOS
+elif OS == 'Darwin':  # MacOS
     PLATDIR = '/Applications/AppInventor/commands-for-Appinventor'
-elif OS == 'Windows':   # Windows
+elif OS == 'Windows': # Windows
     PLATDIR = os.path.join(os.environ["ProgramFiles"], 'AppInventor', 'commands-for-Appinventor')
-else:                   # Unknown OS
+else:                 # Unknown OS
     sys.exit(1)
 
-PLATDIR += os.path.sep  # Append path separator at the end
+# Path to executables
+ADB = PLATDIR + os.path.sep + 'adb'
+RUN_EMULATOR = PLATDIR + os.path.sep + 'run-emulator'
+RESET_EMULATOR = PLATDIR + os.path.sep + 'reset-emulator'
+KILL_EMULATOR = PLATDIR + os.path.sep + 'kill-emulator'
 
 
 @route('/ping/')
@@ -55,7 +59,7 @@ def utest():
 
 @route('/start/')
 def start():
-    subprocess.call(f'"{PLATDIR}run-emulator"', shell=True)
+    subprocess.call(f'"{RUN_EMULATOR}"', shell=True)
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] = 'origin, content-type'
     return ''
@@ -63,7 +67,7 @@ def start():
 
 @route('/emulatorreset/')
 def emulatorreset():
-    subprocess.call(f'"{PLATDIR}reset-emulator"', shell=True)
+    subprocess.call(f'"{RESET_EMULATOR}"', shell=True)
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] = 'origin, content-type'
     return ''
@@ -97,7 +101,8 @@ def ucheck():
     if device:
         return {
             "status": "OK",
-            "device": device, "version": VERSION
+            "device": device,
+            "version": VERSION
         }
     else:
         return {
@@ -120,73 +125,74 @@ def reset():
 
 @route('/replstart/:device')
 def replstart(device=None):
-    print(f"Device = {device}")
+    print('Device =', device)
     try:
-        subprocess.check_output(f'"{PLATDIR}adb" -s {device} forward tcp:8001 tcp:8001', shell=True)
-        if re.match('.*emulat.*', device):  # Only fake the menu key for the emulator
-            subprocess.check_output(f'"{PLATDIR}adb" -s {device} shell input keyevent 82', shell=True,)
+        subprocess.check_output(f'"{ADB}" -s {device} forward tcp:8001 tcp:8001', shell=True)
+        if re.match('emulator.*', device):  # Only fake the menu key for the emulator
+            subprocess.check_output(f'"{PLATDIR}adb" -s {device} shell input keyevent 82', shell=True)
         subprocess.check_output(
-            f'"{PLATDIR}adb" -s {device} shell am start -a android.intent.action.VIEW -n edu.mit.appinventor.aicompanion3/.Screen1 --ez rundirect true',
+            f'"{ADB}" -s {device} shell am start -a android.intent.action.VIEW -n edu.mit.appinventor.aicompanion3/.Screen1 --ez rundirect true',
             shell=True)
         response.headers['Access-Control-Allow-Origin'] = '*'
         response.headers['Access-Control-Allow-Headers'] = 'origin, content-type'
         return ''
     except subprocess.CalledProcessError as e:
-        print(f"Problem starting companion app : status {e.returncode}\n")
+        print('Problem starting companion app : status', e.returncode)
         return ''
 
 
 def checkrunning(emulator):
     try:
-        result = subprocess.check_output(f'"{PLATDIR}adb" devices', shell=True)
+        match = ''
+        result = subprocess.check_output(f'"{ADB}" devices', shell=True)
         lines = result.splitlines()
         for line in lines[1:]:
+            line = str(line, 'utf-8')
             if line:
-                l = str(line, 'utf-8')  # convert byte to string
                 if emulator:
-                    m = re.search(r'^(emulator-\d+)\s+device.*', l)
+                    match = re.search(r'^(emulator-\d+)\s+device$', line)
                 else:
-                    if re.search(r'^(emulator-\d+)\s+device.*', l):  # We are an emulator
-                        continue  # Skip it
-                    m = re.search(r'^([\w\d]+)\s+device.*', l)
-                if m:
+                    if re.search(r'^(emulator-\d+)\s+device$', line): # We are emulator
+                        continue                                      # Skip it
+                    match = re.search(r'^([\w\d]+)\s+device$', line)
+                if match:
                     break
-        if m:
-            return m.group(1)
+        if match:
+            return match.group(1)
         return False
     except subprocess.CalledProcessError as e:
-        print(f"Problem checking for devices : status {e.returncode}\n")
+        print('Problem checking for devices : status', e.returncode)
         return False
 
 
 def killadb():
-    """Time to nuke adb!"""
     try:
-        subprocess.check_output(f'"{PLATDIR}adb" kill-server', shell=True)
-        print("Killed adb\n")
+        subprocess.check_output(f'"{ADB}" kill-server', shell=True)
+        print('Killed adb')
     except subprocess.CalledProcessError as e:
-        print(f"Problem stopping adb : status {e.returncode}\n")
-        return ''
+        print('Problem stopping adb : status', e.returncode)
 
 
 def killemulator():
     try:
-        subprocess.check_output(f'"{PLATDIR}kill-emulator"', shell=True)
-        print("Killed emulator\n")
+        subprocess.check_output(f'"{KILL_EMULATOR}"', shell=True)
+        print('Killed emulator')
     except subprocess.CalledProcessError as e:
-        print(f"Problem stopping emulator : status {e.returncode}\n")
-        return ''
+        print('Problem stopping emulator : status', e.returncode)
 
 
 def shutdown():
-    killadb()
-    killemulator()
+    try:
+        killemulator()
+        killadb()
+    except:
+        pass
 
 
 if __name__ == '__main__':
-    print(f'AppInventor tools located here: "{PLATDIR}"')
+    print('AppInventor tools located here:', PLATDIR, '\n')
 
     import atexit
-
     atexit.register(shutdown)
+
     run(host='127.0.0.1', port=8004)
