027ee8a41b8097781cae22f4269a3f655238cd5d
SusanRatiLane <43011595+SusanRatiLane@users.noreply.github.com>
2018-09-10 10:31:54 -0400

    Check for java reserved words in component and project names (#1348)
    Issue #1336 - Projects with reserved words do no compile correctly.
    YAIL reserved words are also a bad idea, but they were only
    checked for component names.
    
    Now component and project names check for both YAIL and java words
    
    Closes #1336

--- appinventor/appengine/src/com/google/appinventor/client/OdeMessages.java
+++ appinventor/appengine/src/com/google/appinventor/client/OdeMessages.java
@@ -985,12 +985,15 @@ public interface OdeMessages extends Messages {
   @Description("Error shown when a new component type would be the same as a component instance name")
   String sameAsComponentInstanceNameError();
 
-  @DefaultMessage("Component name cannot be any of the following: CsvUtil, Double, Float, " +
+  @DefaultMessage("Name cannot be any of the following: CsvUtil, Double, Float, " +
       "Integer, JavaCollection, JavaIterator, KawaEnvironment, Long, Short, SimpleForm, String, " +
-      "Pattern, YailList, YailNumberToString, YailRuntimeError")
-  @Description("Error shown when a new component name is a variable name already used in the" +
-      "Yail code")
-  String badComponentNameError();
+      "Pattern, YailList, YailNumberToString, YailRuntimeError, abstract, continue, for, new, " +
+      "switch, assert, default, goto, package, synchronized, boolean, do, if, private, this, break, " +
+      "double, implements, protected, throw, byte, else, import, public, throws, case, enum, instanceof, " +
+      "return, transient, catch, extends, int, short, try, char, final, interface, static, void, class, " +
+      "finally, long, strictfp, volatile, const, float, native, super, while")
+  @Description("Error shown when a new name is a reserved name in Yail or Java code")
+  String reservedNameError();
 
   @DefaultMessage("Deleting this component will delete all blocks associated with it in the " +
       "Blocks Editor. Are you sure you want to delete?")
