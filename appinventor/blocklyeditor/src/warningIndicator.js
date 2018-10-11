ab62ca1d44d439106fcd5ca635b036906aa52f58
Susan Rati Lane <srlane@mit.edu>
2018-10-11 13:29:03 -0400

    Change warning navigation button labels
    The UI needs review overall, but this makes the variable names more informative.

--- appinventor/blocklyeditor/src/warningIndicator.js
+++ appinventor/blocklyeditor/src/warningIndicator.js
@@ -123,21 +123,21 @@ Blockly.WarningIndicator.prototype.createDom = function() {
       this.svgGroup_);
   this.warningToggleText_.textContent = Blockly.Msg.SHOW_WARNINGS;
 
-  this.navLeft_ = Blockly.utils.createSvgElement('rect',
+  this.warningNavLeft_ = Blockly.utils.createSvgElement('rect',
       {'fill': "#eeeeee",'width':"40", 'height':"20", 'x':"5",'y':"50",'style':"stroke:black;stroke-width:1;cursor:pointer;"},
       this.svgGroup_);
-  this.navLeftText_ = Blockly.utils.createSvgElement('text',
+  this.warningNavLeftText_ = Blockly.utils.createSvgElement('text',
       {'fill': "black", 'transform':"translate(23,65)",'text-anchor':"middle",'style':"font-size:10pt;cursor:pointer;"},
       this.svgGroup_);
-  this.navLeftText_.textContent = "<<";
+  this.warningNavLeftText_.textContent = "<<";
 
-  this.navRight_ = Blockly.utils.createSvgElement('rect',
+  this.warningNavRight_ = Blockly.utils.createSvgElement('rect',
       {'fill': "#eeeeee",'width':"40", 'height':"20", 'x':"50",'y':"50",'style':"stroke:black;stroke-width:1;cursor:pointer;"},
       this.svgGroup_);
-  this.navRightText_ = Blockly.utils.createSvgElement('text',
+  this.warningNavRightText_ = Blockly.utils.createSvgElement('text',
       {'fill': "black", 'transform':"translate(70,65)",'text-anchor':"middle",'style':"font-size:10pt;cursor:pointer;"},
       this.svgGroup_);
-  this.navRightText_.textContent = ">>";
+  this.warningNavRightText_.textContent = ">>";
 
   return this.svgGroup_;
 };
@@ -151,10 +151,10 @@ Blockly.WarningIndicator.prototype.init = function() {
   Blockly.bindEvent_(window, 'resize', this, this.position_);
   Blockly.bindEvent_(this.warningToggle_, 'mouseup', this, Blockly.WarningIndicator.prototype.onclickWarningToggle);
   Blockly.bindEvent_(this.warningToggleText_, 'mouseup', this, Blockly.WarningIndicator.prototype.onclickWarningToggle);
-  Blockly.bindEvent_(this.navLeft_, 'mouseup', this, Blockly.WarningIndicator.prototype.onclickWarningNavLeft);
-  Blockly.bindEvent_(this.navLeftText_, 'mouseup', this, Blockly.WarningIndicator.prototype.onclickWarningNavLeft);
-  Blockly.bindEvent_(this.navRight_, 'mouseup', this, Blockly.WarningIndicator.prototype.onclickWarningNavRight);
-  Blockly.bindEvent_(this.navRightText_, 'mouseup', this, Blockly.WarningIndicator.prototype.onclickWarningNavRight);
+  Blockly.bindEvent_(this.warningNavLeft_, 'mouseup', this, Blockly.WarningIndicator.prototype.onclickWarningNavLeft);
+  Blockly.bindEvent_(this.warningNavLeftText_, 'mouseup', this, Blockly.WarningIndicator.prototype.onclickWarningNavLeft);
+  Blockly.bindEvent_(this.warningNavRight_, 'mouseup', this, Blockly.WarningIndicator.prototype.onclickWarningNavRight);
+  Blockly.bindEvent_(this.warningNavRightText_, 'mouseup', this, Blockly.WarningIndicator.prototype.onclickWarningNavRight);
 };
 
 /**
@@ -179,10 +179,10 @@ Blockly.WarningIndicator.prototype.dispose = function() {
 
   this.warningToggle_ = null;
   this.warningToggleText_ = null;
-  this.navLeft_ = null;
-  this.navLeftText_ = null;
-  this.navRight_ = null;
-  this.navRightText_ = null;
+  this.warningNavLeft_ = null;
+  this.warningNavLeftText_ = null;
+  this.warningNavRight_ = null;
+  this.warningNavRightText_ = null;
 
 };
 
