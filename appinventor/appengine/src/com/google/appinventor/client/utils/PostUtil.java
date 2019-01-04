ec95a9718d418ad860f911982e744fca7da86e65
Kelsey Chan <kelseykchan@gmail.com>
2019-01-04 15:44:00 -0500

    Publish project to new gallery
    - Adds a "Publish to New Gallery" button to toolbar
    - Creates new user in new gallery if one does not already exist
    - Publishes selected project to new gallery and opens new gallery project page in new window

--- appinventor/appengine/src/com/google/appinventor/client/utils/PostUtil.java
+++ appinventor/appengine/src/com/google/appinventor/client/utils/PostUtil.java
@@ -1,50 +1,78 @@
 package com.google.appinventor.client.utils;
 
+import com.google.appinventor.shared.rpc.ServerLayout;
+import com.google.appinventor.shared.rpc.user.User;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.json.client.JSONException;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONString;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.xhr.client.ReadyStateChangeHandler;
 import com.google.gwt.xhr.client.XMLHttpRequest;
 
+import java.util.Objects;
+
 public class PostUtil {
 
-    static {
-        installCode();
+    public static void addAppToGallery(User user, long projectId, String projectName) {
+        new PostUtil().postAddNewUserAndPublishApp(user, "ai2", projectId, projectName);
     }
 
-    public void doPost(String stuff) {
+    public void postAddNewUserAndPublishApp(final User user, String appInventorInstance, final long projectId, final String projectName) {
         XMLHttpRequest xhr = XMLHttpRequest.create();
         xhr.setOnReadyStateChange(new ReadyStateChangeHandler() {
             @Override
             public void onReadyStateChange(XMLHttpRequest xhr) {
                 if (xhr.getReadyState() == XMLHttpRequest.DONE) {
-                    Window.alert("DONE!");
+                    new PostUtil().postAddAppToGallery(user.getUserId(), projectId, projectName);
                 }
             }
         });
         xhr.open("POST", "http://localhost:8090/api/user/create");
         xhr.setRequestHeader("Content-Type", "application/json");
-//        xhr.setRequestHeader("Content-Type", "application/zip+appinventor;base64");
         JSONObject json = new JSONObject();
         try {
-            json.put("authorId", new JSONString("4"));
-            json.put("name", new JSONString("Olaf"));
-            json.put("username", new JSONString("snow_master"));
-            json.put("appInventorInstance", new JSONString("ai2"));
+            json.put("authorId", new JSONString(user.getUserId()));
+            json.put("name", new JSONString(user.getUserName()));
+            json.put("username", new JSONString(user.getUserName() + "_" + user.getUserId()));
+            json.put("appInventorInstance", new JSONString(appInventorInstance));
             xhr.send(json.toString());
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    public static void test() {
-        new PostUtil().doPost("hello world");
+    public void postAddAppToGallery(String userId, long projectId, String projectName) {
+        String aiaUrl = GWT.getModuleBaseURL() + ServerLayout.DOWNLOAD_SERVLET_BASE + ServerLayout.DOWNLOAD_PROJECT_SOURCE + "/" + Objects.toString(projectId);
+        addAppToGalleryPostHelper(aiaUrl, projectName, userId, Objects.toString(projectId));
     }
 
-    public static native void installCode()/*-{
-    top.testPost = $entry(@com.google.appinventor.client.utils.PostUtil::test());
-  }-*/;
+    public static native void addAppToGalleryPostHelper(String aiaUrl, String title, String authorId, String projectId)/*-{
+    var aiaRequest = new XMLHttpRequest();
+    aiaRequest.responseType = "blob";
+    aiaRequest.onreadystatechange = function() {
+        if (aiaRequest.readyState === 4) {
+            var formData = new FormData();
 
-    public static native void foobar()/*-{}-*/;
+            formData.append("title", title);
+            formData.append("authorId", authorId);
+            formData.append("projectId", projectId);
+            formData.append("appInventorInstance", "ai2");
+            formData.append("aia", aiaRequest.response);
+
+            var request = new XMLHttpRequest();
+            request.responseType = "json";
+            request.onreadystatechange = function() {
+                if (request.readyState === 4) {
+                    if (request.response.project && request.response.project.id) {
+                        window.open("http://localhost:3000/#/project/" + request.response.project.id, "_blank");
+                    }
+                }
+            }
+            request.open("POST", "http://localhost:8090/api/project/create");
+            request.send(formData);
+        }
+    }
+    aiaRequest.open("GET", aiaUrl);
+    aiaRequest.send();
+    }-*/;
 }
