7969c9c8e77f7b9af922d7130b2b9c4e4cc06bbb
Zheng Ge <zg242@cornell.edu>
2019-02-16 13:00:52 -0500

    Store shared constants

---
+++ appinventor/components/src/com/google/appinventor/components/common/ComponentDescriptorConstants.java
@@ -0,0 +1,27 @@
+package com.google.appinventor.components.common;
+
+public class ComponentDescriptorConstants {
+  private ComponentDescriptorConstants() {
+    // nothing
+  }
+  // shared constants between ComponentListGenerator.java and Compiler.java
+  public static final String ARMEABI_V7A_SUFFIX = "-v7a";
+  public static final String ARM64_V8A_SUFFIX = "-v8a";
+  public static final String X86_64_SUFFIX = "-x8a";
+  public static final String ASSET_DIRECTORY = "component";
+  public static final String ASSETS_TARGET = "assets";
+  public static final String ACTIVITIES_TARGET = "activities";
+  public static final String LIBRARIES_TARGET = "libraries";
+  public static final String NATIVE_TARGET = "native";
+  public static final String PERMISSIONS_TARGET = "permissions";
+  public static final String BROADCAST_RECEIVERS_TARGET = "broadcastReceivers";
+  public static final String ANDROIDMINSDK_TARGET = "androidMinSdk";
+  public static final String CONDITIONALS_TARGET = "conditionals";
+  // public static final String CONDITONAL_TARGET = "conditional";
+
+  // TODO(Will): Remove the following target once the deprecated
+  //             @SimpleBroadcastReceiver annotation is removed. It should
+  //             should remain for the time being because otherwise we'll break
+  //             extensions currently using @SimpleBroadcastReceiver.
+  public static final String BROADCAST_RECEIVER_TARGET = "broadcastReceiver";
+}
