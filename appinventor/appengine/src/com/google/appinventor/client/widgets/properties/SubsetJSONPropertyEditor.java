ba8e04875817553817dce955a0dc4c63a7088606
Susan Rati Lane <srlane@mit.edu>
2019-02-21 10:20:47 -0500

    Work in progress. Creating JSON property editor.
    Change-Id: I7975ab0ae861ae7f3ea0f7ae325ec345af115f4c

--- appinventor/appengine/src/com/google/appinventor/client/widgets/properties/SubsetJSONPropertyEditor.java
+++ appinventor/appengine/src/com/google/appinventor/client/widgets/properties/SubsetJSONPropertyEditor.java
@@ -1,26 +1,53 @@
 // -*- mode: java; c-basic-offset: 2; -*-
-// Copyright 2009-2011 Google, All Rights reserved
-// Copyright 2011-2018 MIT, All rights reserved
+// Copyright 2019 MIT, All rights reserved
 // Released under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 
 package com.google.appinventor.client.widgets.properties;
 
-import com.google.gwt.user.client.ui.TextArea;
+import static com.google.appinventor.client.Ode.MESSAGES;
 
-/**
- * Property editor for long text.  Appears as a scrollable, resizable area.
- */
-public class SubsetJSONPropertyEditor extends PropertyEditor {
+import com.google.appinventor.client.explorer.project.Project;
+import com.google.appinventor.client.explorer.project.ProjectChangeListener;
+import com.google.appinventor.shared.rpc.project.ProjectNode;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.*;
+import sun.java2d.pipe.SpanShapeRenderer;
 
+import java.util.Iterator;
+
+public class SubsetJSONPropertyEditor  extends AdditionalChoicePropertyEditor
+        implements ProjectChangeListener {
 
   public SubsetJSONPropertyEditor() {
-   super();
+    super();
+    Frame subsetSelector = new Frame();
+    SimplePanel framePanel = new SimplePanel();
 
+    subsetSelector.setUrl("http://fred.com");
+    framePanel.add(subsetSelector);
+    initAdditionalChoicePanel(framePanel);
 
   }
 
-  public void updateValue(){
+  // AdditionalChoicePropertyEditor implementation
+  @Override
+  protected boolean okAction() {
+    return true;
+  }
 
+
+  // ProjectChangeListener implementation
+  @Override
+  public void onProjectLoaded(Project project) {
+  }
+
+  public void onProjectNodeAdded(Project project, ProjectNode node) {
+  }
+
+  @Override
+  public void onProjectNodeRemoved(Project project, ProjectNode node) {
   }
 }
