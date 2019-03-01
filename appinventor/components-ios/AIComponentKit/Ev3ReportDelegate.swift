cb651d6edbf0da68c006781399b50ca388a3cb95
Joy Yu <joysyu@mit.edu>
2019-03-01 12:09:01 -0800

    Implement Ev3Motors
    Change-Id: I58af5a7fa2aed935d7b1231919afc4e005e2ac59

---
+++ appinventor/components-ios/AIComponentKit/Ev3ReportDelegate.swift
@@ -0,0 +1,11 @@
+//  https://github.com/andiikaa/ev3ios
+//
+//  Created by Andre on 22.04.16.
+//  Copyright © 2016 Andre. All rights reserved.
+//
+import Foundation
+
+protocol Ev3ReportDelegate {
+  
+  func reportReceived(report: [UInt8])
+}
