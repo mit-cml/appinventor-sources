48a4db78f939576d9e8512b939249d6d583b2dc0
Helen <hs667@cornell.edu>
2019-03-10 21:17:37 -0400

    Add context menu item to hide and show workspace SVG elements

--- appinventor/blocklyeditor/src/workspace_svg.js
+++ appinventor/blocklyeditor/src/workspace_svg.js
@@ -568,6 +568,26 @@ Blockly.WorkspaceSvg.prototype.customContextMenu = function(menuOptions) {
   };
   menuOptions.splice(3, 0, exportOption);
 
+  //Hide workspace SVG elements backpack, zoom, and trashcan
+  var hideWorkspace = {enabled: true};
+  hideWorkspace.text = Blockly.Msg.HIDE;
+  hideWorkspace.callback= function() {
+    self.backpack_.svgGroup_.style.display='none';
+    self.trashcan.svgGroup_.style.display='none';
+    self.zoomControls_.svgGroup_.style.display='none';
+  };
+  menuOptions.push(hideWorkspace);
+
+  //Show workspace SVG elements backpack, zoom, and trashcan
+  var showWorkspace = {enabled: true};
+  showWorkspace.text = Blockly.Msg.SHOW;
+  showWorkspace.callback= function() {
+    self.backpack_.svgGroup_.style.display='block';
+    self.trashcan.svgGroup_.style.display='block';
+    self.zoomControls_.svgGroup_.style.display='block';
+  };
+  menuOptions.push(showWorkspace);
+
   // Arrange blocks in row order.
   var arrangeOptionH = {enabled: (Blockly.workspace_arranged_position !== Blockly.BLKS_HORIZONTAL)};
   arrangeOptionH.text = Blockly.Msg.ARRANGE_H;
