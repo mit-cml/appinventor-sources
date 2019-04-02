4248e19f9d4d5a15037484dce4bb5a2c1963673b
Γκάγκας Αναστάσιος <tasosggps@gmail.com>
2019-04-03 02:18:56 +0300

    Fixed indentation

--- appinventor/components/src/com/google/appinventor/components/runtime/Ev3Mailbox.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/Ev3Mailbox.java
@@ -21,53 +21,53 @@ import com.google.appinventor.components.common.YaVersion;
  */
 
 @DesignerComponent(version = YaVersion.EV3_MAILBOX_COMPONENT_VERSION,
-        description = "A component that provides high level interfaces to a LEGO MINDSTORMS EV3 " +
-                "robot, with functions that can send messages to the Ev3 mailbox.",
-        category = ComponentCategory.LEGOMINDSTORMS,
-        nonVisible = true,
-        iconName = "images/legoMindstormsEv3.png")
+                   description = "A component that provides high level interfaces to a LEGO MINDSTORMS EV3 " +
+                   "robot, with functions that can send messages to the Ev3 mailbox.",
+                   category = ComponentCategory.LEGOMINDSTORMS,
+                   nonVisible = true,
+                   iconName = "images/legoMindstormsEv3.png")
 @SimpleObject
 public class Ev3Mailbox extends LegoMindstormsEv3Base {
 
-    public Ev3Mailbox(ComponentContainer container) {
-        super(container, "Ev3Mailbox");
-    }
+  public Ev3Mailbox(ComponentContainer container) {
+    super(container, "Ev3Mailbox");
+  }
 
-    @SimpleFunction(description = "Send a text message")
-    public void SendMailboxText(String name, String message) {
-        String functionName = "SendMailboxText";
-        byte[] command = Ev3BinaryParser.encodeSystemCommand(Ev3Constants.SystemCommand.WRITEMAILBOX,
-                                                            false,
-                                                            (byte) (name.length()+1),
-                                                            name,
-                                                            (short) (message.length()+1),
-                                                            message);
-        sendCommand(functionName, command, false);
-    }
+  @SimpleFunction(description = "Send a text message")
+  public void SendMailboxText(String name, String message) {
+    String functionName = "SendMailboxText";
+    byte[] command = Ev3BinaryParser.encodeSystemCommand(Ev3Constants.SystemCommand.WRITEMAILBOX,
+                                                         false,
+                                                         (byte) (name.length()+1),
+                                                         name,
+                                                         (short) (message.length()+1),
+                                                         message);
+    sendCommand(functionName, command, false);
+  }
 
-    @SimpleFunction(description = "Send a numeric message")
-    public void SendMailboxNumeric(String name, float message) {
-        String functionName = "SendMailboxNumeric";
-        int hexMessage = Float.floatToIntBits(message);
-        byte[] command = Ev3BinaryParser.encodeSystemCommand(Ev3Constants.SystemCommand.WRITEMAILBOX,
-                                                            false,
-                                                            (byte) (name.length()+1),
-                                                            name,
-                                                            (short) 4,
-                                                            hexMessage);
-        sendCommand(functionName, command, false);
-    }
+  @SimpleFunction(description = "Send a numeric message")
+  public void SendMailboxNumeric(String name, float message) {
+    String functionName = "SendMailboxNumeric";
+    int hexMessage = Float.floatToIntBits(message);
+    byte[] command = Ev3BinaryParser.encodeSystemCommand(Ev3Constants.SystemCommand.WRITEMAILBOX,
+                                                         false,
+                                                         (byte) (name.length()+1),
+                                                         name,
+                                                         (short) 4,
+                                                         hexMessage);
+    sendCommand(functionName, command, false);
+  }
 
-    @SimpleFunction(description = "Send a boolean message")
-    public void SendMailboxBoolean(String name, boolean message) {
-        String functionName = "SendMailboxBoolean";
-        int hexMessage = message ? 1:0;
-        byte[] command = Ev3BinaryParser.encodeSystemCommand(Ev3Constants.SystemCommand.WRITEMAILBOX,
-                                                                false,
-                                                                (byte) (name.length()+1),
-                                                                name,
-                                                                (short) 1,
-                                                                (byte) hexMessage);
-        sendCommand(functionName, command, false);
-    }
+  @SimpleFunction(description = "Send a boolean message")
+  public void SendMailboxBoolean(String name, boolean message) {
+    String functionName = "SendMailboxBoolean";
+    int hexMessage = message ? 1:0;
+    byte[] command = Ev3BinaryParser.encodeSystemCommand(Ev3Constants.SystemCommand.WRITEMAILBOX,
+                                                         false,
+                                                         (byte) (name.length()+1),
+                                                         name,
+                                                         (short) 1,
+                                                         (byte) hexMessage);
+    sendCommand(functionName, command, false);
+  }
 }
