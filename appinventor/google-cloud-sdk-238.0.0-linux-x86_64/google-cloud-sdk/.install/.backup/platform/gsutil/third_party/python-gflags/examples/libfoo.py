0baa66dd6c004e15d5dae425b6524dd1da948bc7
vagish dilawari <dilawari2008@gmail.com>
2019-04-02 23:59:07 +0530

    correction

--- appinventor/google-cloud-sdk-238.0.0-linux-x86_64/google-cloud-sdk/.install/.backup/platform/gsutil/third_party/python-gflags/examples/libfoo.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-"""Defines some flags."""
-
-import gflags
-
-gflags.DEFINE_integer('num_replicas', 3, 'Number of replicas to start')
-gflags.DEFINE_boolean('rpc2', True, 'Turn on the usage of RPC2.')
