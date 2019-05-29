05dd117530e1943a3ec4bd34ca2946b1bcff44ce
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-05-29 22:29:07 +0200

    Add minimal ChartLine (Line Chart) component class

---
+++ appinventor/components/src/com/google/appinventor/components/runtime/ChartLine.java
@@ -0,0 +1,28 @@
+package com.google.appinventor.components.runtime;
+
+import android.view.View;
+import com.github.mikephil.charting.charts.LineChart;
+import com.google.appinventor.components.annotations.DesignerComponent;
+import com.google.appinventor.components.annotations.SimpleObject;
+import com.google.appinventor.components.common.ComponentCategory;
+
+@DesignerComponent(version = 1,
+    category = ComponentCategory.LAYOUT,
+    description = "A component that allows visualizing data using lines")
+@SimpleObject
+public final class ChartLine extends ChartBase<LineChart> {
+
+    /**
+     * Creates a new Line Chart component.
+     *
+     * @param container container, component will be placed in
+     */
+    public ChartLine(ComponentContainer container) {
+        super(container);
+    }
+
+    @Override
+    public View getView() {
+        return view;
+    }
+}
