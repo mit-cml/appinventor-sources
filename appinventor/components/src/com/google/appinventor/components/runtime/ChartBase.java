dabd4cbb2ad10472c122923743e8e2947ad7e42e
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-05-29 23:44:17 +0200

    Add Description & Background Color properties to ChartBase component

--- appinventor/components/src/com/google/appinventor/components/runtime/ChartBase.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/ChartBase.java
@@ -1,14 +1,18 @@
 package com.google.appinventor.components.runtime;
 
-import com.google.appinventor.components.annotations.SimpleObject;
-import com.google.appinventor.components.annotations.UsesLibraries;
+import com.google.appinventor.components.annotations.*;
 import com.github.mikephil.charting.charts.Chart;
+import com.google.appinventor.components.common.PropertyTypeConstants;
 
 @SimpleObject
 @UsesLibraries(libraries = "mpandroidchart.jar")
 public abstract class ChartBase<T extends Chart> extends AndroidViewComponent {
 
     protected T view;
+
+    private String description;
+    private int backgroundColor;
+
 
     /**
      * Creates a new ChartBase component.
@@ -25,5 +29,60 @@ public abstract class ChartBase<T extends Chart> extends AndroidViewComponent {
     protected void initChart() {
         // Adds the view to the designated container
         container.$add(this);
+
+        // Set default values
+        BackgroundColor(Component.COLOR_DEFAULT);
+        Description("");
+    }
+
+    /**
+     * Returns the description label text of the Chart.
+     *
+     * @return  description label
+     */
+    @SimpleProperty(
+            category = PropertyCategory.APPEARANCE)
+    public String Description() {
+        return description;
+    }
+
+    /**
+     * Specifies the text displayed by the description label.
+     *
+     * @param text  description
+     */
+    @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_STRING,
+        defaultValue = "")
+    @SimpleProperty
+    public void Description(String text) {
+        this.description = text;
+        view.getDescription().setText(description);
+    }
+
+    /**
+     * Returns the chart's background color as an alpha-red-green-blue
+     * integer.
+     *
+     * @return  background RGB color with alpha
+     */
+    @SimpleProperty(
+            category = PropertyCategory.APPEARANCE)
+    public int BackgroundColor() {
+        return backgroundColor;
+    }
+
+    /**
+     * Specifies the chart's background color as an alpha-red-green-blue
+     * integer.
+     *
+     * @param argb  background RGB color with alpha
+     */
+    @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_COLOR,
+            defaultValue = Component.DEFAULT_VALUE_COLOR_NONE)
+    @SimpleProperty
+    public void BackgroundColor(int argb) {
+        backgroundColor = argb;
+
+        view.setBackgroundColor(argb);
     }
 }
