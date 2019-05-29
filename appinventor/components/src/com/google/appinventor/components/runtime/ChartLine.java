8f5040ab240337cce30fcbf015053fc62ae8a481
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-05-29 23:45:52 +0200

    Initialize view to LineChart in ChartLine component

--- appinventor/components/src/com/google/appinventor/components/runtime/ChartLine.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/ChartLine.java
@@ -19,6 +19,8 @@ public final class ChartLine extends ChartBase<LineChart> {
      */
     public ChartLine(ComponentContainer container) {
         super(container);
+
+        view = new LineChart(container.$context());
     }
 
     @Override
