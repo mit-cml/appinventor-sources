8fcd625e98fd7397aefffe10e32c08ae2dbe6a87
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-05-31 19:32:21 +0200

    MockLineChart styling changes & test data loading

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockLineChart.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockLineChart.java
@@ -1,10 +1,16 @@
 package com.google.appinventor.client.editor.simple.components;
 
 import com.google.appinventor.client.editor.simple.SimpleEditor;
+import com.google.appinventor.components.common.ComponentConstants;
 import org.pepstock.charba.client.LineChart;
+import org.pepstock.charba.client.data.DataPoint;
 import org.pepstock.charba.client.data.LineDataset;
+import org.pepstock.charba.client.enums.CubicInterpolationMode;
+import org.pepstock.charba.client.enums.SteppedLine;
 import org.pepstock.charba.client.resources.EmbeddedResources;
 import org.pepstock.charba.client.resources.ResourcesType;
+
+import java.util.ArrayList;
 
 public final class MockLineChart extends MockVisibleComponent {
     public static final String TYPE = "LineChart";
@@ -23,11 +29,34 @@ public final class MockLineChart extends MockVisibleComponent {
     public MockLineChart(SimpleEditor editor) {
         super(editor, TYPE, images.image());
 
-        // Initialize mock label UI
+        // Initialize Line Chart mock Widget
         lineChartWidget = new LineChart();
+
+        // Initialize data set
         LineDataset dataset = lineChartWidget.newDataset();
-        dataset.setData(1, 2, 3, 4, 6);
+
+        // Construct test data
+        ArrayList<DataPoint> dataPoints = new ArrayList<>();
+
+        for (int i = 0; i < 10; ++i) {
+            DataPoint point = new DataPoint();
+            point.setX(i);
+            point.setY(2*i);
+            dataPoints.add(point);
+        }
+
+        dataset.setDataPoints(dataPoints);
+
+        // Style settings
+        dataset.setFill(false);
+        dataset.setPointBackgroundColor("black");
+        dataset.setBorderColor("black");
+        dataset.setCubicInterpolationMode(CubicInterpolationMode.MONOTONE);
+
+
+        // Chart widget setup
         lineChartWidget.getData().setDatasets(dataset);
+        lineChartWidget.getOptions().setMaintainAspectRatio(false);
 
         lineChartWidget.setStylePrimaryName("ode-SimpleMockComponent");
         initComponent(lineChartWidget);
@@ -38,5 +67,15 @@ public final class MockLineChart extends MockVisibleComponent {
         super.onPropertyChange(propertyName, newValue);
 
         // TBD
+    }
+
+    @Override
+    public int getPreferredWidth() {
+        return ComponentConstants.VIDEOPLAYER_PREFERRED_WIDTH;
+    }
+
+    @Override
+    public int getPreferredHeight() {
+        return ComponentConstants.VIDEOPLAYER_PREFERRED_HEIGHT;
     }
 }
