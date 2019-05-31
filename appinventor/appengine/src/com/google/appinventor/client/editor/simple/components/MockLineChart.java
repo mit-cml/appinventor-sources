6319d675fb1943c025fca87a01dd720586e2f715
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-05-31 15:30:39 +0200

    Add and integrate Charba library for MockLineChart (experimental)

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockLineChart.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockLineChart.java
@@ -1,9 +1,13 @@
 package com.google.appinventor.client.editor.simple.components;
 
 import com.google.appinventor.client.editor.simple.SimpleEditor;
+import org.pepstock.charba.client.LineChart;
+import org.pepstock.charba.client.data.LineDataset;
 
-public final class MockLineChart extends MockImageBase {
+public final class MockLineChart extends MockVisibleComponent {
     public static final String TYPE = "LineChart";
+
+    private LineChart lineChartWidget;
 
     /**
      * Creates a new instance of a Mock Line Chart component.
@@ -12,5 +16,21 @@ public final class MockLineChart extends MockImageBase {
      */
     public MockLineChart(SimpleEditor editor) {
         super(editor, TYPE, images.image());
+
+        // Initialize mock label UI
+        lineChartWidget = new LineChart();
+        LineDataset dataset = lineChartWidget.newDataset();
+        dataset.setData(1, 2, 3, 4, 6);
+        lineChartWidget.getData().setDatasets(dataset);
+
+        lineChartWidget.setStylePrimaryName("ode-SimpleMockComponent");
+        initComponent(lineChartWidget);
+    }
+
+    @Override
+    public void onPropertyChange(String propertyName, String newValue) {
+        super.onPropertyChange(propertyName, newValue);
+
+        // TBD
     }
 }
