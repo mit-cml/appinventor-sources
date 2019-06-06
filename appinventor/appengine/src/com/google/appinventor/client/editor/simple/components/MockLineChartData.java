8e747db3005a22a3d15ee597cca239e17e9f1cb6
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-06-06 23:30:11 +0200

    Minor styling changes for the code (imports & Javadoc)

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockLineChartData.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockLineChartData.java
@@ -4,23 +4,19 @@ import com.google.appinventor.client.editor.simple.SimpleEditor;
 import com.google.gwt.user.client.ui.InlineHTML;
 
 public class MockLineChartData extends MockVisibleComponent {
-    /**
-     * Component type name.
-     */
     public static final String TYPE = "LineChartData";
 
-    // GWT label widget used to mock a Simple Label
+    // Temporary placeholder for the Chart Data image
     private InlineHTML labelWidget;
 
     /**
-     * Creates a new MockLabel component.
+     * Creates a new MockLineChartData component.
      *
      * @param editor  editor of source file the component belongs to
      */
     public MockLineChartData(SimpleEditor editor) {
         super(editor, TYPE, images.label());
 
-        // Initialize mock label UI
         labelWidget = new InlineHTML();
         labelWidget.setStylePrimaryName("ode-SimpleMockComponent");
         labelWidget.setText("LINE CHART DATA");
