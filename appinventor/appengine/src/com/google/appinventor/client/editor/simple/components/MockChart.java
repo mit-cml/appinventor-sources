7bc870d04db48677545105c42e1012ac0733772a
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-06-09 19:11:46 +0200

    addDataSeries method comments in MockChart class

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockChart.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockChart.java
@@ -112,12 +112,20 @@ abstract class MockChart<C extends AbstractChart> extends MockContainer {
      * @param dataSeries  Data Series object to add.
      */
     public void addDataSeries(Dataset dataSeries) {
+        // When adding the first Data Series, it should be set
+        // to the Chart Data object itself rather then appended,
+        // to register the first (new) DataSet List to the Chart data.
+        // Subsequent adding of Data Series objects can simply be added
+        // to the end of the List.
         if (chartWidget.getData().getDatasets().size() == 0) {
             chartWidget.getData().setDatasets(dataSeries);
         } else {
             chartWidget.getData().getDatasets().add(dataSeries);
         }
 
+        // Refresh the Chart. Although the Controller is typically responsible
+        // for refreshing the Chart, since this method should be called on the Mock
+        // Chart object, the refreshing can be done here.
         chartWidget.update();
     }
 
