55fcef9ab4c5282dededdec4f42832dbca57935e
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-06-13 23:53:27 +0200

    Add SetElements tests for LineChartModel

--- appinventor/components/tests/com/google/appinventor/components/runtime/util/LineChartModelTest.java
+++ appinventor/components/tests/com/google/appinventor/components/runtime/util/LineChartModelTest.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Unit teests for the LineChartModel class.
+ * Unit tests for the LineChartModel class.
  */
 public class LineChartModelTest extends RobolectricTestBase {
     /*
@@ -80,5 +80,69 @@ public class LineChartModelTest extends RobolectricTestBase {
         Entry entry = model.getDataset().getEntryForIndex(0);
         Assert.assertEquals(x, entry.getX());
         Assert.assertEquals(y, entry.getY());
+    }
+
+    /**
+     * Tests to ensure that Data Series entries are not changed
+     * when passing in invalid input via setEelements.
+     */
+    @Test
+    public void testSetElementsInvalid() {
+        String elements = "1, 2, 3, 4, A, B";
+
+        model.setElements(elements);
+
+        // Make sure that the method was abruptly cut because
+        // of invalid entries detected.
+        Assert.assertEquals(0, model.getDataset().getEntryCount());
+    }
+
+    /**
+     * Tests to ensure that the entries are properly parsed
+     * and ordered when using the setElements method.
+     */
+    @Test
+    public void testSetElementsEven() {
+        // We are adding (3, 1), (2, 4), (1, 2)
+        // End result should be ordered by X value.
+        String elements = "3, 1, 2, 4, 1, 2";
+        model.setElements(elements);
+
+        // Make sure that 3 elements have been added
+        Assert.assertEquals(3, model.getDataset().getEntryCount());
+
+        // Verify the 3 entries
+        Entry entry1 = model.getDataset().getEntryForIndex(0);
+        Assert.assertEquals(1f, entry1.getX());
+        Assert.assertEquals(2f, entry1.getY());
+
+        Entry entry2 = model.getDataset().getEntryForIndex(1);
+        Assert.assertEquals(2f, entry2.getX());
+        Assert.assertEquals(4f, entry2.getY());
+
+        Entry entry3 = model.getDataset().getEntryForIndex(2);
+        Assert.assertEquals(3f, entry3.getX());
+        Assert.assertEquals(1f, entry3.getY());
+    }
+
+    /**
+     * Tests to ensure that the last entry is cut
+     * off to make the list even, and the proper
+     * entries are added.
+     */
+    @Test
+    public void testSetElementsOdd() {
+        // List length is odd
+        String elements = "1, 3, 5";
+        model.setElements(elements);
+
+        // Only the first entry should be added (cut off last value
+        // since pairs are accepted)
+        Assert.assertEquals(1, model.getDataset().getEntryCount());
+
+        // Make sure entry is correct
+        Entry entry = model.getDataset().getEntryForIndex(0);
+        Assert.assertEquals(1f, entry.getX());
+        Assert.assertEquals(3f, entry.getY());
     }
 }
