bd766e0e82e1ec2545c28629a28031d12b96eeab
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-06-13 23:23:37 +0200

    Create LineChartModelTest class with some property tests + addEntry test

---
+++ appinventor/components/tests/com/google/appinventor/components/runtime/util/LineChartModelTest.java
@@ -0,0 +1,76 @@
+package com.google.appinventor.components.runtime.util;
+
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.google.appinventor.components.runtime.RobolectricTestBase;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LineChartModelTest extends RobolectricTestBase {
+    private LineChartModel model;
+    private LineData data;
+
+    @Before
+    public void setup() {
+        data = new LineData();
+        model = new LineChartModel(data);
+    }
+
+    /**
+     * Test to ensure that the constructor properly instantiates an
+     * empty data set, and that the reference of the passed in data
+     * object instance is not broken.
+     */
+    @Test
+    public void testConstructor() {
+        Assert.assertEquals(data, model.getData());
+        Assert.assertEquals(1, data.getDataSetCount());
+        Assert.assertEquals(0, data.getDataSetByIndex(0).getEntryCount());
+    }
+
+    /**
+     * Tests whether the setLabel method correctly changes the label
+     * of the Data Series.
+     */
+    @Test
+    public void testSetLabel() {
+        String label = "Test Label Text";
+        model.setLabel(label);
+        Assert.assertEquals(label, model.getDataset().getLabel());
+    }
+
+    /**
+     * Tests whether the setColor method correctly changes the color
+     * of the Data Series.
+     */
+    @Test
+    public void testSetColor() {
+        int argb = 0xFFEEDDCC;
+        model.setColor(argb);
+        Assert.assertEquals(argb, model.getDataset().getColor());
+    }
+
+    /**
+     * Tests whether an entry is correctly added to the Data Set
+     * upon calling the add entry method with x and y coordinates.
+     */
+    @Test
+    public void testAddEntry() {
+        // Pre-condition: make sure there are no entries initially
+        Assert.assertEquals(0, model.getDataset().getEntryCount());
+
+        // Add an entry
+        float x = 4;
+        float y = 5;
+        model.addEntry(x, y);
+
+        // Ensure that the entry has been added
+        Assert.assertEquals(1, model.getDataset().getEntryCount());
+
+        // Make sure that a correct entry has been added
+        Entry entry = model.getDataset().getEntryForIndex(0);
+        Assert.assertEquals(x, entry.getX());
+        Assert.assertEquals(y, entry.getY());
+    }
+}
