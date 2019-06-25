28fc10bce04389bb138ac4a316aa0b1f05b7132f
Diego Barreiro <diego.barreiro.perez@gmail.com>
2019-06-25 15:45:14 +0200

    UX: Init Component

---
+++ appinventor/components/src/com/google/appinventor/components/runtime/Arduino.java
@@ -0,0 +1,105 @@
+/**
+ * ©Kodular 2017 - 2019 | FOR MAKEROID ONLY AND NOT PRIVATE USE!
+ * @author diego@kodular.io (Diego Barreiro)
+ * @author pavitra@kodular.io (Pavitra Golchha)
+ */
+ 
+package com.google.appinventor.components.runtime;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.physicaloid.lib.*;
+
+import com.google.appinventor.components.annotations.*;
+import com.google.appinventor.components.runtime.*;
+import com.google.appinventor.components.common.*;
+
+import java.io.UnsupportedEncodingException;
+
+@DesignerComponent(version = YaVersion.ARDUINO_COMPONENT_VERSION,
+    description = "Arduino USB Serial Component",
+    category = ComponentCategory.CONNECTIVITY,
+    nonVisible = true,
+    iconName = "images/arduino.png")
+
+@SimpleObject
+@UsesLibraries(libraries = "physicaloid.jar")
+public class MakeroidArduino extends AndroidNonvisibleComponent implements Component {
+    private static final String LOG_TAG = "Makeroid Arduino USB Serial Component";
+
+    private Context context;
+
+    Physicaloid mPhysicaloid;
+
+    private int baudRate = 9600;
+
+    public MakeroidArduino(ComponentContainer container) {
+        super(container.$form());
+        context = container.$context();
+        Log.d(LOG_TAG, "Created");
+    }
+
+    @SimpleFunction(description = "Initializes Arduino Connection")
+    public void InitializeArduino() {
+        mPhysicaloid = new Physicaloid(context);
+        Log.d(LOG_TAG, "Initialized");
+    }
+
+    @SimpleFunction(description = "Opens Arduino Connection")
+    public boolean OpenArduino() {
+        Log.d(LOG_TAG, "Opening connection");
+        return mPhysicaloid.open();
+    }
+
+    @SimpleFunction(description = "Closes Arduino Connection")
+    public boolean CloseArduino() {
+        Log.d(LOG_TAG, "Closing connection");
+        return mPhysicaloid.close();
+    }
+
+    @SimpleFunction(description = "Default baud rate is 9600 bps")
+    public void BaudRate(int baudRate) {
+        this.baudRate = baudRate;
+        mPhysicaloid.setBaudrate(baudRate);
+        Log.d(LOG_TAG, "Baud Rate: " + baudRate);
+    }
+
+    @SimpleFunction(description = "Read from Serial")
+    public void ReadArduino() {
+        byte[] buf = new byte[256];
+        boolean success = true;
+        String data = "";
+
+        if (mPhysicaloid.read(buf) > 0) {
+            try {
+                data = new String(buf, "UTF-8");
+            } catch (UnsupportedEncodingException mEr) {
+                success = false;
+                Log.e(LOG_TAG, mEr.getMessage());
+            }
+        } else {
+            success = false;
+        }
+
+        AfterReadArduino(success, data);
+    }
+
+    @SimpleFunction(description = "Write Data to Serial")
+    public void WriteArduino(String writeDataArduino) {
+        if (!writeDataArduino.isEmpty()) {
+            byte[] buf = writeDataArduino.getBytes();
+            mPhysicaloid.write(buf);
+        }
+    }
+
+    @SimpleFunction(description = "Returns true when the Arduino connection is open")
+    public boolean IsOpenedArduino() {
+        return mPhysicaloid.isOpened();
+    }
+
+    @SimpleEvent(description = "Triggered after Read function")
+    public void AfterReadArduino(boolean success, String data) {
+        EventDispatcher.dispatchEvent(this, "AfterRead", success, data);
+    }
+}
