c0ef17d9b66304ade2b8d8253cb210eeb4f748f3
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-07-05 18:10:19 +0200

    Add comments for setElements method in ChartModel

--- appinventor/components/src/com/google/appinventor/components/runtime/ChartModel.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/ChartModel.java
@@ -56,18 +56,28 @@ public abstract class ChartModel<T extends DataSet, D extends ChartData> {
      * @param elements String in CSV format
      */
     public void setElements(String elements) {
+        // Get the expected number of tuples
         int tupleSize = getTupleSize();
 
+        // Split all the CSV entries by comma
         String[] entries = elements.split(",");
 
+        // Iterate over every tuple (by grouping them)
+        // We start from tupleSize - 1 since the (tupleSize - 1)-th
+        // entry will be the last entry of the tuple.
+        // The index is incremented by the tupleSize to move to the next
+        // group of entries for a tuple.
         for (int i = tupleSize - 1; i < entries.length; i += tupleSize) {
             List<String> tupleEntries = new ArrayList<String>();
 
+            // Iterate over all the tuple entries
+            // First entry is in (i - tupleSize + 1)
             for (int j = tupleSize - 1; j >= 0; --j) {
                 int index = i - j;
                 tupleEntries.add(entries[index]);
             }
 
+            // Add entry from the parsed tuple
             addEntryFromTuple(YailList.makeList(tupleEntries));
         }
     }
