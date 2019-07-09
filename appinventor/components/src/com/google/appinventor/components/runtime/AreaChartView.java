0dd3af136979c6f6296d4c733bb5090afd621f24
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-07-09 18:41:01 +0200

    Fix Area Chart fill not working (disable hardware acceleration for Chart)

--- appinventor/components/src/com/google/appinventor/components/runtime/AreaChartView.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/AreaChartView.java
@@ -5,6 +5,10 @@ import android.app.Activity;
 public class AreaChartView extends LineChartView {
     public AreaChartView(Activity context) {
         super(context);
+
+        // In order for the fill under the Chart to work on SDK < 18,
+        // hardware acceleration has to be disabled.
+        chart.setHardwareAccelerationEnabled(false);
     }
 
     @Override
