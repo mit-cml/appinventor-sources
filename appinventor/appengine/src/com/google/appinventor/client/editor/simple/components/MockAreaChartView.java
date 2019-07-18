1b47d3b04cccb6a345a3c681f56dc8624a508e15
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-07-18 19:37:08 +0200

    Add comment with regards to Legend reversing for the Mock Area Chart

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockAreaChartView.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockAreaChartView.java
@@ -6,6 +6,18 @@ public class MockAreaChartView extends MockLineChartViewBase {
     }
 
     @Override
+    protected void initializeDefaultSettings() {
+        super.initializeDefaultSettings();
+
+        // Due to differing drawing orders of the Android and the
+        // Mock Chart implementations, the Legend has to be reversed
+        // in the Mock Area Chart and the Data Series have to be added
+        // in reverse order as well to represent the Android data series
+        // accurately.
+        chartWidget.getOptions().getLegend().setReverse(true);
+    }
+
+    @Override
     public MockChartDataModel createDataModel() {
         return new MockAreaChartDataModel(chartWidget.getData());
     }
