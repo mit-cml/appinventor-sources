0df617be010fc09d5e2970648b6f074c327d0171
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-07-22 18:14:08 +0200

    Create PointChartView abstraction; Create ScatterChartView subclass

---
+++ appinventor/components/src/com/google/appinventor/components/runtime/ScatterChartView.java
@@ -0,0 +1,26 @@
+package com.google.appinventor.components.runtime;
+
+import android.app.Activity;
+import com.github.mikephil.charting.charts.ScatterChart;
+import com.github.mikephil.charting.data.ScatterData;
+
+public class ScatterChartView extends PointChartView<ScatterChart, ScatterData> {
+  /**
+   * Instantiate a new ScatterChartView in the given context.
+   *
+   * @param context Context to instantiate view in
+   */
+  public ScatterChartView(Activity context) {
+    chart = new ScatterChart(context);
+
+    data = new ScatterData();
+    chart.setData(data);
+
+    initializeDefaultSettings();
+  }
+
+  @Override
+  public ChartDataModel createChartModel() {
+    return null;
+  }
+}
