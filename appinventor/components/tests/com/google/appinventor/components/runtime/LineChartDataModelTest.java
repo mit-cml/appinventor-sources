619831cee5c17068b0e6ab0806fe4daa6e06b544
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-07-30 00:47:05 +0200

    Add extra test cases for addEntry for LineChartDataModel test class

--- appinventor/components/tests/com/google/appinventor/components/runtime/LineChartDataModelTest.java
+++ appinventor/components/tests/com/google/appinventor/components/runtime/LineChartDataModelTest.java
@@ -133,4 +133,74 @@ public class LineChartDataModelTest
     model.importFromList(pairs);
     assertExpectedEntriesHelper(expectedEntries);
   }
+
+  /**
+   * Test to ensure that adding an entry to a Data Series inserts it
+   * in the appropriate position.
+   * This method tests the case where the element is inserted at the
+   * end of the Data Series entries.
+   */
+  @Test
+  public void testAddEntryMatchLast() {
+    ArrayList<YailList> tuples = new ArrayList<YailList>() {{
+      add(createTuple(0f, 0f));
+      add(createTuple(1f, 2f));
+      add(createTuple(2f, 3f));
+      add(createTuple(5f, 11f));
+      add(createTuple(9f, 15f));
+      add(createTuple(11f, 16f));
+    }};
+
+    YailList pairs = YailList.makeList(tuples);
+
+    ArrayList<Entry> expectedEntries = new ArrayList<Entry>() {{
+      add(new Entry(0f, 0f));
+      add(new Entry(1f, 2f));
+      add(new Entry(2f, 3f));
+      add(new Entry(5f, 11f));
+      add(new Entry(9f, 15f));
+      add(new Entry(11f, 16f));
+      add(new Entry(15f, 1f));
+    }};
+
+    // Import the data and assert all the entries
+    model.importFromList(pairs);
+    model.addEntryFromTuple(createTuple(15f, 1f)); // Should be inserted after all the entries that have x value 4
+    assertExpectedEntriesHelper(expectedEntries);
+  }
+
+  /**
+   * Test to ensure that adding an entry to a Data Series inserts it
+   * in the appropriate position.
+   * This method tests the case where the element is inserted at the
+   * start of the Data Series entries.
+   */
+  @Test
+  public void testAddEntryMatchFirst() {
+    ArrayList<YailList> tuples = new ArrayList<YailList>() {{
+      add(createTuple(-4f, 5f));
+      add(createTuple(-1f, 2f));
+      add(createTuple(3f, 1f));
+      add(createTuple(7f, 11f));
+      add(createTuple(15f, 15f));
+      add(createTuple(21f, 16f));
+    }};
+
+    YailList pairs = YailList.makeList(tuples);
+
+    ArrayList<Entry> expectedEntries = new ArrayList<Entry>() {{
+      add(new Entry(-7f, 1f));
+      add(new Entry(-4f, 5f));
+      add(new Entry(-1f, 2f));
+      add(new Entry(3f, 1f));
+      add(new Entry(7f, 11f));
+      add(new Entry(15f, 15f));
+      add(new Entry(21f, 16f));
+    }};
+
+    // Import the data and assert all the entries
+    model.importFromList(pairs);
+    model.addEntryFromTuple(createTuple(-7f, 1f)); // Should be inserted after all the entries that have x value 4
+    assertExpectedEntriesHelper(expectedEntries);
+  }
 }
