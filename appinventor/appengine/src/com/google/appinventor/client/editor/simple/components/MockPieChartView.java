bd3f91c69babd91150f79f8ef40c7801f4997785
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-08-01 00:05:47 +0200

    Create MockPieChartView (WIP)

---
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockPieChartView.java
@@ -0,0 +1,15 @@
+package com.google.appinventor.client.editor.simple.components;
+
+import org.pepstock.charba.client.PieChart;
+
+public class MockPieChartView extends MockChartView<PieChart> {
+  public MockPieChartView() {
+    chartWidget = new PieChart();
+    initializeDefaultSettings();
+  }
+
+  @Override
+  public MockChartDataModel createDataModel() {
+    return new MockLineChartDataModel(chartWidget.getData());
+  }
+}
