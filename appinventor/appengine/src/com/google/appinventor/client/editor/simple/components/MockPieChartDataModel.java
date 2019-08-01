062a80522b9a9aca86a81ead6b2d4e5d5c47b65b
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-08-01 14:54:05 +0200

    MockPieChart JavaDoc & commenting

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockPieChartDataModel.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockPieChartDataModel.java
@@ -10,10 +10,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MockPieChartDataModel extends MockChartDataModel<PieDataset> {
+  // Local references of the colors and labels properties have to be
+  // kept in the Data Model class to be able to add to them and set them.
+  // This is due to the API for changing colors and labels being quite limited (in v2.5)
   private List<String> colors = new ArrayList<String>();
   private List<String> labels = new ArrayList<String>();
   private String color = "";
 
+  // Since labels of the Legend are global per entry, the
+  // view has to be kept in the Data Model. It is
+  // required to notify the view of the deletion of the
+  // Pie Chart Data Model to handle the Legend creation.
   private MockPieChartView view;
 
   /**
@@ -21,10 +28,12 @@ public class MockPieChartDataModel extends MockChartDataModel<PieDataset> {
    * the Data object of a specific Chart.
    *
    * @param chartData Chart Data object to link to
+   * @Param view  Mock Pie Chart View to link Data Model to
    */
   public MockPieChartDataModel(Data chartData, MockPieChartView view) {
     super(chartData);
 
+    // Set a local reference to the parent Mock Pie Chart View
     this.view = view;
 
     // Create the Data Series object
@@ -42,12 +51,17 @@ public class MockPieChartDataModel extends MockChartDataModel<PieDataset> {
     this.color = color;
     color = getHexColor(color);
 
+    // Clear the old colors list
     colors.clear();
 
+    // Add the same color for every entry
+    // TODO: Possibly make a List of colors as a Designer property?
     for (int i = 0; i < dataSeries.getData().size(); ++i) {
       colors.add(color);
     }
 
+    // Since the setBackgroundColor method accepts varargs as parameters,
+    // we have to cast our result to a String array.
     dataSeries.setBackgroundColor(colors.toArray(new String[0]));
   }
 
@@ -79,7 +93,10 @@ public class MockPieChartDataModel extends MockChartDataModel<PieDataset> {
   @Override
   public void addEntryFromTuple(String... tuple) {
     try {
+      // First entry is a String
       String x = tuple[0];
+
+      // Second entry is expected to be a double; attempt parsing
       Double y = Double.parseDouble(tuple[1]);
 
       // Add data entry
@@ -89,7 +106,7 @@ public class MockPieChartDataModel extends MockChartDataModel<PieDataset> {
         dataSeries.getData().add(y);
       }
 
-      // Add entry label (legend entry)
+      // Add entry label (x value corresponds to the label)
       labels.add(x);
     } catch (NumberFormatException e) {
       // Wrong input. Do nothing.
@@ -114,6 +131,9 @@ public class MockPieChartDataModel extends MockChartDataModel<PieDataset> {
       setDefaultElements();
     }
 
+    // After importing the data, the colors have to be changed again
+    // (since the new entry count might be different)
+    // TODO: possible to optimize by tracking differences
     changeColor(this.color);
   }
 
@@ -122,13 +142,20 @@ public class MockPieChartDataModel extends MockChartDataModel<PieDataset> {
     return 2;
   }
 
+  /**
+   * Returns a List of entry labels in order
+   * @return  List of entry labels (Strings_
+   */
   public List<String> getLabels() {
     return labels;
   }
 
   @Override
   public void removeDataSeriesFromChart() {
+    // The view has to be notified of the removal of the Data Model
     view.removeDataModel(this);
+
+    // Proceed with general removal from the Chart
     super.removeDataSeriesFromChart();
   }
 }
--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockPieChartView.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockPieChartView.java
@@ -1,21 +1,24 @@
 package com.google.appinventor.client.editor.simple.components;
 
-import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import org.pepstock.charba.client.IsChart;
 import org.pepstock.charba.client.PieChart;
-import org.pepstock.charba.client.callbacks.LegendCallback;
 import org.pepstock.charba.client.callbacks.LegendLabelsCallback;
-import org.pepstock.charba.client.data.Dataset;
+import org.pepstock.charba.client.callbacks.TooltipCustomCallback;
 import org.pepstock.charba.client.data.PieDataset;
-import org.pepstock.charba.client.items.DatasetMetaItem;
 import org.pepstock.charba.client.items.LegendLabelItem;
+import org.pepstock.charba.client.items.TooltipModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MockPieChartView extends MockChartView<PieChart> {
+  // A local List of attached Data Models has to be kept to
+  // construct Legend entries
   private List<MockPieChartDataModel> dataModels = new ArrayList<MockPieChartDataModel>();
 
+  /**
+   * Creates a new Mock Pie Chart View instance.
+   */
   public MockPieChartView() {
     chartWidget = new PieChart();
     initializeDefaultSettings();
@@ -25,32 +28,46 @@ public class MockPieChartView extends MockChartView<PieChart> {
   protected void initializeDefaultSettings() {
     super.initializeDefaultSettings();
 
-    chartWidget.getOptions().getLegend().getLabels().setLabelsCallback(new LegendLabelsCallback() {
+    // Create a custom Legend generator for the Pie Chart
+    chartWidget.getOptions().getLegend()
+        .getLabels().setLabelsCallback(new LegendLabelsCallback() {
       @Override
       public LegendLabelItem[] generateLegendLabels(IsChart chart) {
+        // Create a List to store Legend Items to add to the Char
         List<LegendLabelItem> legendItems = new ArrayList<LegendLabelItem>();
+
+        // Create a List to store all the labels across all the Data Series
         List<String> dataLabels = new ArrayList<String>();
 
+        // Iterate over all attached Data Models
         for (int i = 0; i < dataModels.size(); ++i) {
           MockPieChartDataModel model = dataModels.get(i);
+          PieDataset dataset = model.dataSeries; // TODO: Use getter instead
+
+          // Get the labels of the model and add them to the
+          // data labels list storing all the labels
           List<String> labels = model.getLabels();
           dataLabels.addAll(labels);
 
-          PieDataset dataset = model.dataSeries;
-
+          // Iterate over all the labels
           for (int j = 0; j < labels.size(); ++j) {
+            // For each label, create a new legend item
             LegendLabelItem legendItem = new LegendLabelItem();
-            legendItem.setDatasetIndex(i);
-            legendItem.setIndex(j);
-            legendItem.setText(labels.get(j));
-            legendItem.setFillStyle(dataset.getBackgroundColor().get(j));
+            legendItem.setDatasetIndex(i); // Set the dataset reference index
+            legendItem.setIndex(j); // Set the entry index reference (deos not work as expected)
+            legendItem.setText(labels.get(j)); // Set the text of the required label
+            legendItem.setFillStyle(dataset.getBackgroundColor().get(j)); // Set the representative fill style
 
+            // Add the legend item to the resulting Legend Items list
             legendItems.add(legendItem);
           }
         }
 
+        // setLabels accepts varargs as a parameter. Data Labels result must
+        // be cast to an array.
         chartWidget.getData().setLabels(dataLabels.toArray(new String[0]));
 
+        // Cast result to array
         return legendItems.toArray(new LegendLabelItem[0]);
       }
     });
@@ -58,11 +75,19 @@ public class MockPieChartView extends MockChartView<PieChart> {
 
   @Override
   public MockChartDataModel createDataModel() {
+    // Create a new MockPieChartDataModel
     MockPieChartDataModel model = new MockPieChartDataModel(chartWidget.getData(), this);
+
+    // Add the Data Model to the local List of Data Models
     dataModels.add(model);
+
     return model;
   }
 
+  /**
+   * Unlinks the specified Data Model from the Mock Pie Chart View.
+   * @param model  model to unlink
+   */
   public void removeDataModel(MockPieChartDataModel model) {
     dataModels.remove(model);
   }
