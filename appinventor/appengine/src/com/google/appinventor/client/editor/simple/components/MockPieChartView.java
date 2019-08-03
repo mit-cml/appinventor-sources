e9939f4feab980809d5ba1761c4dade1d6a2fe71
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-08-03 15:37:43 +0200

    Add Tooltip callback to properly display both x and y values when hovered

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockPieChartView.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockPieChartView.java
@@ -4,10 +4,12 @@ import com.google.appinventor.client.output.OdeLog;
 import org.pepstock.charba.client.IsChart;
 import org.pepstock.charba.client.PieChart;
 import org.pepstock.charba.client.callbacks.LegendLabelsCallback;
+import org.pepstock.charba.client.callbacks.TooltipBodyCallback;
 import org.pepstock.charba.client.callbacks.TooltipCustomCallback;
 import org.pepstock.charba.client.callbacks.TooltipFilterCallback;
 import org.pepstock.charba.client.callbacks.TooltipItemSortCallback;
 import org.pepstock.charba.client.callbacks.TooltipLabelCallback;
+import org.pepstock.charba.client.callbacks.TooltipTitleCallback;
 import org.pepstock.charba.client.colors.ColorBuilder;
 import org.pepstock.charba.client.colors.IsColor;
 import org.pepstock.charba.client.data.PieDataset;
@@ -43,6 +45,60 @@ public class MockPieChartView extends MockChartView<PieChart> {
     // TODO: Set tooltips to display X value as well (needs additional logic with labels)
     // Can be accomplished by:
     // chartWidget.getOptions().getTooltips().getCallbacks().setLabelCallback();
+    chartWidget.getOptions().getTooltips().getCallbacks().setLabelCallback(new TooltipLabelCallback() {
+      @Override
+      public String onBeforeLabel(IsChart chart, TooltipItem item) {
+        return "";
+      }
+
+      @Override
+      public String onLabel(IsChart chart, TooltipItem item) {
+        // Get the Data Series & entry indexes
+        int seriesIndex = item.getDatasetIndex();
+        int entryIndex = item.getIndex();
+
+        // Get the corresponding MockPieChartDataModel
+        MockPieChartDataModel model = dataModels.get(seriesIndex);
+
+        // The x value can be found in the labels List, while the
+        // y value can be found in the Data List of the Data Series
+        String xValue = model.getLabels().get(entryIndex);
+        double yValue = model.dataSeries.getData().get(entryIndex);
+
+        // Format the label String which displays the x and the y values
+        return xValue + ": " + yValue;
+      }
+
+      @Override
+      public TooltipLabelColor onLabelColor(IsChart chart, TooltipItem item) {0
+        TooltipLabelColor color = new TooltipLabelColor();
+
+        // Get the Data Series & entry indexes
+        int seriesIndex = item.getDatasetIndex();
+        int entryIndex = item.getIndex();
+
+        // Get the corresponding Data Series & retrieve the color of
+        // the corresponding entry
+        PieDataset dataSeries = dataModels.get(seriesIndex).dataSeries;
+        IsColor colorValue = dataSeries.getBackgroundColor().get(entryIndex);
+
+        // Set the color value of the constructed TooltipLabelColor object and
+        // return it
+        color.setBackgroundColor(colorValue);
+        return color;
+      }
+
+      @Override
+      public IsColor onLabelTextColor(IsChart chart, TooltipItem item) {
+        // Label text should always be white
+        return ColorBuilder.build(255, 255, 255);
+      }
+
+      @Override
+      public String onAfterLabel(IsChart chart, TooltipItem item) {
+        return "";
+      }
+    });
 
     // Disable the Legend Click event which hides a data series (by setting
     // an empty event handler). The reason this is done is because the
