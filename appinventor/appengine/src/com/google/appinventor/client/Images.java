6517c5b807316d83c55b041ce9501f84278dce44
Bart Mathijssen <bart@bartmathijssen.com>
2019-08-24 19:23:41 +0200

    Move NavigationBar to right side in landscape mode

--- appinventor/appengine/src/com/google/appinventor/client/Images.java
+++ appinventor/appengine/src/com/google/appinventor/client/Images.java
@@ -69,12 +69,6 @@ public interface Images extends Resources {
   ImageResource phonebar();
 
   /**
-   * Navigation bar shown below the form in the visual designer
-   */
-  @Source("com/google/appinventor/images/navigationbar.png")
-  ImageResource navigationbar();
-
-  /**
    * Spinning/wait graphic to indicate long-running operations.
    */
   @Source("com/google/appinventor/images/spin_16.gif")
--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockForm.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockForm.java
@@ -28,6 +28,8 @@ import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style;
 
+import com.google.gwt.resources.client.ImageResource;
+
 import com.google.gwt.user.client.Timer;
 
 import com.google.gwt.user.client.ui.AbsolutePanel;
@@ -147,25 +149,24 @@ public final class MockForm extends MockContainer {
    * Widget for a mock phone navigation bar; Shows at the bottom of the viewer
    */
   private class NavigationBar extends Composite {
-    private static final int HEIGHT = 35;
+    private static final int HEIGHT = 44;
 
     // UI elements
-    private DockPanel bar;
-    private Image navigationBarImage;
+    private AbsolutePanel bar;
 
     /*
      * Creates a new phone navigation bar; Shows at the bottom of the viewer.
      */
 
     NavigationBar() {
-      navigationBarImage = new Image(images.navigationbar());
-      bar = new DockPanel();
-      bar.setHorizontalAlignment(HorizontalPanel.ALIGN_CENTER);
-      bar.add(navigationBarImage, DockPanel.CENTER);
+      bar = new AbsolutePanel();
       initWidget(bar);
 
-      setStylePrimaryName("ode-SimpleMockFormPhoneBar"); //reuse the css for the phone
-      setSize("100%", HEIGHT + "px");
+      setStylePrimaryName("ode-SimpleMockFormNavigationBarPortrait");
+    }
+
+    public int getHeight() {
+      return HEIGHT;
     }
   }
 
@@ -224,14 +225,17 @@ public final class MockForm extends MockContainer {
   // Form UI components
   AbsolutePanel formWidget;
   AbsolutePanel phoneWidget;
+  AbsolutePanel responsivePanel;
 
   ScrollPanel scrollPanel;
   private TitleBar titleBar;
+  private NavigationBar navigationBar;
   private MockComponent selectedComponent;
 
   int screenWidth;              // TEMP: Make package visible so we can use it MockHVLayoutBase
   private int screenHeight;
   int usableScreenHeight;       // TEMP: Make package visible so we can use it MockHVLayoutBase
+  int usableScreenWidth;
 
   // Set of listeners for any changes of the form
   final HashSet<FormChangeListener> formChangeListeners = new HashSet<FormChangeListener>();
@@ -271,18 +275,22 @@ public final class MockForm extends MockContainer {
     phoneWidget.setStylePrimaryName("ode-SimpleMockFormPhonePortrait");
     formWidget = new AbsolutePanel();
     formWidget.setStylePrimaryName("ode-SimpleMockForm");
+    responsivePanel = new AbsolutePanel();
 
     // Initialize mock form UI by adding the phone bar and title bar.
-    formWidget.add(new PhoneBar());
+    responsivePanel.add(new PhoneBar());
     titleBar = new TitleBar();
-    formWidget.add(titleBar);
+    responsivePanel.add(titleBar);
 
     // Put a ScrollPanel around the rootPanel.
     scrollPanel = new ScrollPanel(rootPanel);
-    formWidget.add(scrollPanel);
+    responsivePanel.add(scrollPanel);
+
+    formWidget.add(responsivePanel);
 
     //Add navigation bar at the bottom of the viewer.
-    formWidget.add(new NavigationBar());
+    navigationBar = new NavigationBar();
+    formWidget.add(navigationBar);
 
     phoneWidget.add(formWidget);
     initComponent(phoneWidget);
@@ -322,27 +330,34 @@ public final class MockForm extends MockContainer {
       if (idxPhoneSize == 0) phoneWidget.setStylePrimaryName("ode-SimpleMockFormPhoneLandscape");
       else if (idxPhoneSize == 1) phoneWidget.setStylePrimaryName("ode-SimpleMockFormPhoneLandscapeTablet");
       else if (idxPhoneSize == 2) phoneWidget.setStylePrimaryName("ode-SimpleMockFormPhoneLandscapeMonitor");
+      navigationBar.setStylePrimaryName("ode-SimpleMockFormNavigationBarLandscape");
     }
     else {
       if (idxPhoneSize == 0) phoneWidget.setStylePrimaryName("ode-SimpleMockFormPhonePortrait");
       else if (idxPhoneSize == 1) phoneWidget.setStylePrimaryName("ode-SimpleMockFormPhonePortraitTablet");
       else if (idxPhoneSize == 2) phoneWidget.setStylePrimaryName("ode-SimpleMockFormPhonePortraitMonitor");
+      navigationBar.setStylePrimaryName("ode-SimpleMockFormNavigationBarPortrait");
     }
   }
   /*
-   * Resizes the scrollPanel and formWidget based on the screen size.
+   * Resizes the scrollPanel, responsivePanel, and formWidget based on the screen size.
    */
   private void resizePanel(int newWidth, int newHeight){
     screenWidth = newWidth;
     screenHeight = newHeight;
-    usableScreenHeight = screenHeight - PhoneBar.HEIGHT - titleBar.getHeight() - NavigationBar.HEIGHT;
+    if (landscape) {
+      usableScreenWidth = screenWidth - navigationBar.getHeight();
+      usableScreenHeight = screenHeight - PhoneBar.HEIGHT - titleBar.getHeight();
+    } else {
+      usableScreenWidth = screenWidth;
+      usableScreenHeight = screenHeight - PhoneBar.HEIGHT - titleBar.getHeight() - navigationBar.getHeight();
+    }
 
-
-    rootPanel.setPixelSize(screenWidth, usableScreenHeight);
-    scrollPanel.setPixelSize(screenWidth + getVerticalScrollbarWidth(), usableScreenHeight);
+    rootPanel.setPixelSize(usableScreenWidth, usableScreenHeight);
+    scrollPanel.setPixelSize(usableScreenWidth + getVerticalScrollbarWidth(), usableScreenHeight);
     formWidget.setPixelSize(screenWidth + getVerticalScrollbarWidth(), screenHeight);
     // Store properties
-    changeProperty(PROPERTY_NAME_WIDTH, "" + screenWidth);
+    changeProperty(PROPERTY_NAME_WIDTH, "" + usableScreenWidth);
     boolean scrollable = Boolean.parseBoolean(getPropertyValue(PROPERTY_NAME_SCROLLABLE));
     if (!scrollable) {
       changeProperty(PROPERTY_NAME_HEIGHT, "" + usableScreenHeight);
@@ -523,10 +538,16 @@ public final class MockForm extends MockContainer {
         landscape = false;
       }
       setPhoneStyle();
-      usableScreenHeight = screenHeight - PhoneBar.HEIGHT - titleBar.getHeight() - NavigationBar.HEIGHT;
+      if (landscape) {
+        usableScreenWidth = screenWidth - navigationBar.getHeight();
+        usableScreenHeight = screenHeight - PhoneBar.HEIGHT - titleBar.getHeight();
+      } else {
+        usableScreenWidth = screenWidth;
+        usableScreenHeight = screenHeight - PhoneBar.HEIGHT - titleBar.getHeight() - navigationBar.getHeight();
+      }
       resizePanel(screenWidth, screenHeight);
 
-      changeProperty(PROPERTY_NAME_WIDTH, "" + screenWidth);
+      changeProperty(PROPERTY_NAME_WIDTH, "" + usableScreenWidth);
       boolean scrollable = Boolean.parseBoolean(getPropertyValue(PROPERTY_NAME_SCROLLABLE));
       if (!scrollable) {
         changeProperty(PROPERTY_NAME_HEIGHT, "" + usableScreenHeight);
