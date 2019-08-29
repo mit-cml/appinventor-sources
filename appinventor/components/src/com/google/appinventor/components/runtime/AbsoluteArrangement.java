c2986614f942cc9c75668f00e0f44308ebbfde52
ObadaAL <obadaalkhatib2@gmail.com>
2019-08-29 16:07:39 -0400

    fixed indentation and added copyright header

--- appinventor/components/src/com/google/appinventor/components/runtime/AbsoluteArrangement.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/AbsoluteArrangement.java
@@ -1,3 +1,8 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2019 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 package com.google.appinventor.components.runtime;
 
 import android.app.Activity;
@@ -31,33 +36,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Container for components that arranges them absolutely. 
+ * Container for components that arranges them absolutely.
  */
 
-@DesignerComponent(version = YaVersion.ABSOLUTEARRANGEMENT_COMPONENT_VERSION,
-	description = "<p>A formatting element in which to place components " +
-	"that should be displayed at any coordinates.</p>",
-	category = ComponentCategory.LAYOUT)
+@DesignerComponent(version = YaVersion.ABSOLUTEARRANGEMENT_COMPONENT_VERSION, 
+									 description = "<p>A formatting element in which to place components "
+											 				+ "that should be displayed at any coordinates.</p>", 
+									 category = ComponentCategory.LAYOUT)
 @SimpleObject
 public class AbsoluteArrangement extends AndroidViewComponent implements Component, ComponentContainer {
 	private final Activity context;
-	
+
 	// Layout
 	private final RelativeLayout viewLayout;
 	// Backing for background color
 	private int backgroundColor;
 	// This is the Drawable corresponding to the Image property.
-	// If an Image has never been set or if the most recent Image could not be loaded, this is null.
+	// If an Image has never been set or if the most recent Image could not be
+	// loaded, this is null.
 	private Drawable backgroundImageDrawable;
 	// Image path
 	private String imagePath = "";
-	
+
 	private Drawable defaultButtonDrawable;
-	
+
 	private final Handler androidUIHandler = new Handler();
-	
+
 	private static final String LOG_TAG = "AArrangement";
-	
+
 	/**
 	 * Creates a new AbsoluteArrangement component.
 	 * 
@@ -66,27 +72,26 @@ public class AbsoluteArrangement extends AndroidViewComponent implements Compone
 	public AbsoluteArrangement(ComponentContainer container) {
 		super(container);
 		context = container.$context();
-		
-		Log.d(LOG_TAG, "Setting up layoutManager");
-		
-		viewLayout = new RelativeLayout(context, 
-				ComponentConstants.EMPTY_A_ARRANGEMENT_WIDTH, 
-				ComponentConstants.EMPTY_A_ARRANGEMENT_HEIGHT);
-	    
-	    // Save the default values in case the user wants them back later.
-	    defaultButtonDrawable = getView().getBackground();
 
-	    container.$add(this);
-	    BackgroundColor(Component.COLOR_DEFAULT);
+		Log.d(LOG_TAG, "Setting up layoutManager");
+
+		viewLayout = new RelativeLayout(context, ComponentConstants.EMPTY_A_ARRANGEMENT_WIDTH,
+				ComponentConstants.EMPTY_A_ARRANGEMENT_HEIGHT);
+
+		// Save the default values in case the user wants them back later.
+		defaultButtonDrawable = getView().getBackground();
+
+		container.$add(this);
+		BackgroundColor(Component.COLOR_DEFAULT);
 	}
-	
+
 	// ComponentContainer implementation
-	
+
 	@Override
 	public Activity $context() {
 		return context;
 	}
-	  
+
 	@Override
 	public Form $form() {
 		return container.$form();
@@ -96,152 +101,151 @@ public class AbsoluteArrangement extends AndroidViewComponent implements Compone
 	public void $add(AndroidViewComponent component) {
 		viewLayout.add(component);
 	}
-	
+
 	@Override
 	public void setChildWidth(AndroidViewComponent component, int width) {
 
-	    System.err.println("TableArrangment.setChildWidth: width = " + width + " component = " + component);
-	    if (width <= LENGTH_PERCENT_TAG) {
+		System.err.println("TableArrangment.setChildWidth: width = " + width + " component = " + component);
+		if (width <= LENGTH_PERCENT_TAG) {
 
-	        int cWidth = container.$form().Width();
+			int cWidth = container.$form().Width();
 
-	        if ((cWidth > LENGTH_PERCENT_TAG) && (cWidth <= 0)) {
-	        	// FILL_PARENT OR LENGTH_PREFERRED
-	        	width = LENGTH_PREFERRED;
-	        } else {
-	        	System.err.println("%%TableArrangement.setChildWidth(): width = " + width + " parent Width = " + cWidth + " child = " + component);
-	        	width = cWidth * (- (width - LENGTH_PERCENT_TAG)) / 100;
-	        }
-	    }
+			if ((cWidth > LENGTH_PERCENT_TAG) && (cWidth <= 0)) {
+				// FILL_PARENT OR LENGTH_PREFERRED
+				width = LENGTH_PREFERRED;
+			} else {
+				System.err.println("%%TableArrangement.setChildWidth(): width = " + width + " parent Width = " + cWidth
+						+ " child = " + component);
+				width = cWidth * (-(width - LENGTH_PERCENT_TAG)) / 100;
+			}
+		}
 
-	    component.setLastWidth(width);
+		component.setLastWidth(width);
 
-	    ViewUtil.setChildWidthForRelativeLayout(component.getView(), width);
+		ViewUtil.setChildWidthForRelativeLayout(component.getView(), width);
 	}
 
 	@Override
 	public void setChildHeight(AndroidViewComponent component, int height) {
-	    if (height <= LENGTH_PERCENT_TAG) {
-	    	int cHeight = container.$form().Height();
+		if (height <= LENGTH_PERCENT_TAG) {
+			int cHeight = container.$form().Height();
 
-	    	if ((cHeight > LENGTH_PERCENT_TAG) && (cHeight <= 0)) {
-	    		// FILL_PARENT OR LENGTH_PREFERRED
-	    		height = LENGTH_PREFERRED;
-	    	} else {
-	    		height = cHeight * (- (height - LENGTH_PERCENT_TAG)) / 100;
-	    	}
-	    }
+			if ((cHeight > LENGTH_PERCENT_TAG) && (cHeight <= 0)) {
+				// FILL_PARENT OR LENGTH_PREFERRED
+				height = LENGTH_PREFERRED;
+			} else {
+				height = cHeight * (-(height - LENGTH_PERCENT_TAG)) / 100;
+			}
+		}
 
-	    component.setLastHeight(height);
+		component.setLastHeight(height);
 
-	    ViewUtil.setChildHeightForRelativeLayout(component.getView(), height);
+		ViewUtil.setChildHeightForRelativeLayout(component.getView(), height);
 
 	}
-	
+
 	// AndroidViewComponent implementation
-	
+
 	@Override
 	public View getView() {
-	    return viewLayout.getLayoutManager();
+		return viewLayout.getLayoutManager();
 	}
 
 	/**
-     * Returns the component's background color as an alpha-red-green-blue
-     * integer.
-     *
-     * @return  background RGB color with alpha
-     */
-    @SimpleProperty(category = PropertyCategory.APPEARANCE,
-            description = "Returns the component's background color")
-    public int BackgroundColor() {
-        return backgroundColor;
-    }
+	 * Returns the component's background color as an alpha-red-green-blue integer.
+	 *
+	 * @return background RGB color with alpha
+	 */
+	@SimpleProperty(category = PropertyCategory.APPEARANCE, description = "Returns the component's background color")
+	public int BackgroundColor() {
+		return backgroundColor;
+	}
 
-    /**
-     * Specifies the button's background color as an alpha-red-green-blue
-     * integer.  If the parameter is {@link Component#COLOR_DEFAULT}, the
-     * original beveling is restored.  If an Image has been set, the color
-     * change will not be visible until the Image is removed.
-     *
-     * @param argb background RGB color with alpha
-     */
-    @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_COLOR,
-            defaultValue = Component.DEFAULT_VALUE_COLOR_DEFAULT)
-    @SimpleProperty(description = "Specifies the component's background color. " +
-            "The background color will not be visible if an Image is being displayed.")
-    public void BackgroundColor(int argb) {
-        backgroundColor = argb;
-        updateAppearance();
+	/**
+	 * Specifies the button's background color as an alpha-red-green-blue integer.
+	 * If the parameter is {@link Component#COLOR_DEFAULT}, the original beveling is
+	 * restored. If an Image has been set, the color change will not be visible
+	 * until the Image is removed.
+	 *
+	 * @param argb background RGB color with alpha
+	 */
+	@DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_COLOR, defaultValue = Component.DEFAULT_VALUE_COLOR_DEFAULT)
+	@SimpleProperty(description = "Specifies the component's background color. "
+			+ "The background color will not be visible if an Image is being displayed.")
+	public void BackgroundColor(int argb) {
+		backgroundColor = argb;
+		updateAppearance();
 
-    }
+	}
 
-    /**
-     * Returns the path of the button's image.
-     *
-     * @return  the path of the button's image
-     */
-    @SimpleProperty(
-            category = PropertyCategory.APPEARANCE)
-    public String Image() {
-        return imagePath;
-    }
+	/**
+	 * Returns the path of the button's image.
+	 *
+	 * @return the path of the button's image
+	 */
+	@SimpleProperty(category = PropertyCategory.APPEARANCE)
+	public String Image() {
+		return imagePath;
+	}
 
-    /**
-     * Specifies the path of the button's image.
-     *
-     * <p/>See {@link com.google.appinventor.components.runtime.util.MediaUtil#determineMediaSource} for information about what
-     * a path can be.
-     *
-     * @param path  the path of the button's image
-     */
-    @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_ASSET, defaultValue = "")
-    @SimpleProperty(description = "Specifies the path of the component's image.  " +
-            "If there is both an Image and a BackgroundColor, only the Image will be visible.")
-    public void Image(String path) {
-        // If it's the same as on the prior call and the prior load was successful,
-        // do nothing.
-        if (path.equals(imagePath) && backgroundImageDrawable != null) {
-            return;
-        }
+	/**
+	 * Specifies the path of the button's image.
+	 *
+	 * <p/>
+	 * See
+	 * {@link com.google.appinventor.components.runtime.util.MediaUtil#determineMediaSource}
+	 * for information about what a path can be.
+	 *
+	 * @param path the path of the button's image
+	 */
+	@DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_ASSET, defaultValue = "")
+	@SimpleProperty(description = "Specifies the path of the component's image.  "
+			+ "If there is both an Image and a BackgroundColor, only the Image will be visible.")
+	public void Image(String path) {
+		// If it's the same as on the prior call and the prior load was successful,
+		// do nothing.
+		if (path.equals(imagePath) && backgroundImageDrawable != null) {
+			return;
+		}
 
-        imagePath = (path == null) ? "" : path;
+		imagePath = (path == null) ? "" : path;
 
-        // Clear the prior background image.
-        backgroundImageDrawable = null;
+		// Clear the prior background image.
+		backgroundImageDrawable = null;
 
-        // Load image from file.
-        if (imagePath.length() > 0) {
-            try {
-                backgroundImageDrawable = MediaUtil.getBitmapDrawable(container.$form(), imagePath);
-            } catch (IOException ioe) {
-                // Fall through with a value of null for backgroundImageDrawable.
-            }
-        }
+		// Load image from file.
+		if (imagePath.length() > 0) {
+			try {
+				backgroundImageDrawable = MediaUtil.getBitmapDrawable(container.$form(), imagePath);
+			} catch (IOException ioe) {
+				// Fall through with a value of null for backgroundImageDrawable.
+			}
+		}
 
-        // Update the appearance based on the new value of backgroundImageDrawable.
-        updateAppearance();
-    }
+		// Update the appearance based on the new value of backgroundImageDrawable.
+		updateAppearance();
+	}
 
-
-  // Update appearance based on values of backgroundImageDrawable, backgroundColor and shape.
-  // Images take precedence over background colors.
-  private void updateAppearance() {
-    // If there is no background image,
-    // the appearance depends solely on the background color and shape.
-    if (backgroundImageDrawable == null) {
-      if (backgroundColor == Component.COLOR_DEFAULT) {
-        // If there is no background image and color is default,
-        // restore original 3D bevel appearance.
-        ViewUtil.setBackgroundDrawable(viewLayout.getLayoutManager(), defaultButtonDrawable);
-      } else {
-        // Clear the background image.
-        ViewUtil.setBackgroundDrawable(viewLayout.getLayoutManager(), null);
-        viewLayout.getLayoutManager().setBackgroundColor(backgroundColor);
-      }
-    } else {
-      // If there is a background image
-      ViewUtil.setBackgroundImage(viewLayout.getLayoutManager(), backgroundImageDrawable);
-    }
-  }
+	// Update appearance based on values of backgroundImageDrawable, backgroundColor
+	// and shape.
+	// Images take precedence over background colors.
+	private void updateAppearance() {
+		// If there is no background image,
+		// the appearance depends solely on the background color and shape.
+		if (backgroundImageDrawable == null) {
+			if (backgroundColor == Component.COLOR_DEFAULT) {
+				// If there is no background image and color is default,
+				// restore original 3D bevel appearance.
+				ViewUtil.setBackgroundDrawable(viewLayout.getLayoutManager(), defaultButtonDrawable);
+			} else {
+				// Clear the background image.
+				ViewUtil.setBackgroundDrawable(viewLayout.getLayoutManager(), null);
+				viewLayout.getLayoutManager().setBackgroundColor(backgroundColor);
+			}
+		} else {
+			// If there is a background image
+			ViewUtil.setBackgroundImage(viewLayout.getLayoutManager(), backgroundImageDrawable);
+		}
+	}
 
 }
--- appinventor/components/src/com/google/appinventor/components/runtime/RelativeLayout.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/RelativeLayout.java
@@ -1,3 +1,8 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2019 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 package com.google.appinventor.components.runtime;
 
 import com.google.appinventor.components.annotations.SimpleObject;
@@ -11,16 +16,16 @@ import android.view.View;
 import android.util.Log;
 
 /**
- * Relative Layout for placing components at a specified position inside the 
- * layout. Closely follows the implementation of LinearLayout with certain 
+ * Relative Layout for placing components at a specified position inside the
+ * layout. Closely follows the implementation of LinearLayout with certain
  * changes.
  */
 @SimpleObject
 public class RelativeLayout implements Layout {
-	
+
 	private final android.widget.RelativeLayout layoutManager;
 	private final Handler handler;
-	
+
 	/**
 	 * Creates a new relative layout.
 	 * 
@@ -29,68 +34,69 @@ public class RelativeLayout implements Layout {
 	RelativeLayout(Context context) {
 		this(context, null, null);
 	}
-	
+
 	/**
 	 * Creates a new relative layout with a preferred empty width/height.
 	 * 
-	 * @param context view context
-	 * @param preferredEmptyWidth the preferred width of an empty layout
+	 * @param context              view context
+	 * @param preferredEmptyWidth  the preferred width of an empty layout
 	 * @param preferredEmptyHeight the preferred height of an empty layout
 	 */
-	RelativeLayout(Context context, final Integer preferredEmptyWidth, 
-			final Integer preferredEmptyHeight) {
-		if (preferredEmptyWidth == null && preferredEmptyHeight != null ||
-		    preferredEmptyWidth != null && preferredEmptyHeight == null) {
-		    throw new IllegalArgumentException("RelativeLayout - preferredEmptyWidth and " +
-		    		"preferredEmptyHeight must be either both null or both not null");
+	RelativeLayout(Context context, final Integer preferredEmptyWidth, final Integer preferredEmptyHeight) {
+		if (preferredEmptyWidth == null && preferredEmptyHeight != null
+				|| preferredEmptyWidth != null && preferredEmptyHeight == null) {
+			throw new IllegalArgumentException("RelativeLayout - preferredEmptyWidth and "
+					+ "preferredEmptyHeight must be either both null or both not null");
 		}
-		
+
 		handler = new Handler();
-		
-		// Create an Android RelativeLayout, but override onMeasure so that we can use our preferred
-	    // empty width/height.
-	    layoutManager = new android.widget.RelativeLayout(context) {
-	        @Override
-	        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-	        	// If there was no preferred empty width/height specified (see constructors above), just
-	        	// call super. (This is the case for the Form component.)
-	        	if (preferredEmptyWidth == null || preferredEmptyHeight == null) {
-	        		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-	        		return;
-	        	}
 
-	        	// If the layout has any children, just call super.
-	        	if (getChildCount() != 0) {
-	        		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-	        		return;
-	        	}
+		// Create an Android RelativeLayout, but override onMeasure so that we can use
+		// our preferred
+		// empty width/height.
+		layoutManager = new android.widget.RelativeLayout(context) {
+			@Override
+			protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+				// If there was no preferred empty width/height specified (see constructors
+				// above), just
+				// call super. (This is the case for the Form component.)
+				if (preferredEmptyWidth == null || preferredEmptyHeight == null) {
+					super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+					return;
+				}
 
-	        	setMeasuredDimension(getSize(widthMeasureSpec, preferredEmptyWidth),
-	        						getSize(heightMeasureSpec, preferredEmptyHeight));
-	        }
+				// If the layout has any children, just call super.
+				if (getChildCount() != 0) {
+					super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+					return;
+				}
 
-	        private int getSize(int measureSpec, int preferredSize) {
-	        	int result;
-	        	int specMode = MeasureSpec.getMode(measureSpec);
-	        	int specSize = MeasureSpec.getSize(measureSpec);
+				setMeasuredDimension(getSize(widthMeasureSpec, preferredEmptyWidth),
+						getSize(heightMeasureSpec, preferredEmptyHeight));
+			}
 
-	        	if (specMode == MeasureSpec.EXACTLY) {
-	        		// We were told how big to be
-	        		result = specSize;
-	        	} else {
-	        		// Use the preferred size.
-	        		result = preferredSize;
-	        		if (specMode == MeasureSpec.AT_MOST) {
-	        			// Respect AT_MOST value if that was what is called for by measureSpec
-	        			result = Math.min(result, specSize);
-	        		}
-	        	}
+			private int getSize(int measureSpec, int preferredSize) {
+				int result;
+				int specMode = MeasureSpec.getMode(measureSpec);
+				int specSize = MeasureSpec.getSize(measureSpec);
 
-	        	return result;
-	        }
-	    };
+				if (specMode == MeasureSpec.EXACTLY) {
+					// We were told how big to be
+					result = specSize;
+				} else {
+					// Use the preferred size.
+					result = preferredSize;
+					if (specMode == MeasureSpec.AT_MOST) {
+						// Respect AT_MOST value if that was what is called for by measureSpec
+						result = Math.min(result, specSize);
+					}
+				}
+
+				return result;
+			}
+		};
 	}
-	
+
 	/**
 	 * Returns the width.
 	 * 
@@ -99,7 +105,7 @@ public class RelativeLayout implements Layout {
 	public int getWidth() {
 		return layoutManager.getWidth();
 	}
-	
+
 	/**
 	 * Returns the height.
 	 * 
@@ -108,7 +114,7 @@ public class RelativeLayout implements Layout {
 	public int getHeight() {
 		return layoutManager.getHeight();
 	}
-	
+
 	@Override
 	public ViewGroup getLayoutManager() {
 		return layoutManager;
@@ -116,14 +122,13 @@ public class RelativeLayout implements Layout {
 
 	@Override
 	public void add(AndroidViewComponent component) {
-		// We cannot add component to layoutManager just yet because component 
+		// We cannot add component to layoutManager just yet because component
 		// does not have its own x and y coordinates yet.
-		component.getView().setLayoutParams(
-				new android.widget.RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 
-															   ViewGroup.LayoutParams.WRAP_CONTENT));
+		component.getView().setLayoutParams(new android.widget.RelativeLayout.LayoutParams(
+				ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
 		addComponentLater(component);
 	}
-	
+
 	/**
 	 * Causes addComponent to be called later.
 	 * 
@@ -131,23 +136,22 @@ public class RelativeLayout implements Layout {
 	 */
 	private void addComponentLater(final AndroidViewComponent component) {
 		handler.post(new Runnable() {
-		    public void run() {
-		        addComponent(component);
-		    }
+			public void run() {
+				addComponent(component);
+			}
 		});
 	}
-	
+
 	private void addComponent(final AndroidViewComponent component) {
 		int x = component.XCoord();
 		int y = component.YCoord();
-		if (x == ComponentConstants.DEFAULT_X_Y || 
-			y == ComponentConstants.DEFAULT_X_Y) {
+		if (x == ComponentConstants.DEFAULT_X_Y || y == ComponentConstants.DEFAULT_X_Y) {
 			addComponentLater(component);
 		} else {
 			if (x >= 0 && x < getWidth() && y >= 0 && y < getHeight()) {
 				ViewGroup.LayoutParams params = component.getView().getLayoutParams();
-				android.widget.RelativeLayout.LayoutParams newParams = 
-						new android.widget.RelativeLayout.LayoutParams(params.width, params.height);
+				android.widget.RelativeLayout.LayoutParams newParams = new android.widget.RelativeLayout.LayoutParams(
+						params.width, params.height);
 				newParams.topMargin = ViewUtil.calculatePixels(component.getView(), y);
 				newParams.leftMargin = ViewUtil.calculatePixels(component.getView(), x);
 				layoutManager.addView(component.getView(), newParams);
@@ -156,5 +160,5 @@ public class RelativeLayout implements Layout {
 			}
 		}
 	}
-	
+
 }
