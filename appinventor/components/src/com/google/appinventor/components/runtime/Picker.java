5e24802935e74a4f88182589adc52dbf42ccf789
Evan W. Patton <ewpatton@mit.edu>
2019-09-09 08:45:49 -0400

    Add %type% as a token in component descriptions
    App Inventor doesn't present a type hierarchy even though internally
    it makes sense for objects to have shared methods defined in super
    classes. However, the documentation for the properties, methods, and
    events of an App Inventor component shared in this way must typically
    be written generic enough so that they make sense when shown on
    concrete classes. This can lead to either awkward/generic sounding
    documentation or requires that the component author override all of
    the relevant features on the component to provide component-specific
    descriptions.
    
    This change makes it so that descriptions of components, properties,
    methods, and events can contain one or more %type% tokens that can be
    substituted with the name of the concrete representation shown to the
    App Inventor user. This allows for documentation to be shared at the
    super class level while still making it sound specific to the
    individual component type.
    
    Change-Id: Ida4eb22bec12b2e33ac73de2bb5706d1683b227a

--- appinventor/components/src/com/google/appinventor/components/runtime/Picker.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/Picker.java
@@ -48,7 +48,7 @@ public abstract class Picker extends ButtonBase implements ActivityResultListene
   // Functions
 
   /**
-   * Opens the picker, as though the user clicked on it.
+   * Opens the %type%, as though the user clicked on it.
    */
   @SimpleFunction(description = "Opens the picker, as though the user clicked on it.")
   public void Open() {
@@ -58,7 +58,7 @@ public abstract class Picker extends ButtonBase implements ActivityResultListene
   // Events
 
   /**
-   * Event to raise when the button of the component is clicked or the list is shown
+   * Event to raise when the %type% is clicked or the list is shown
    * using the Open block.  This event occurs before the list of items is displayed, and 
    * can be used to prepare the list before it is shown.
    */
