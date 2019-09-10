52bbec0e0af1e516b79a6be5dfbb2109df42490e
Evan W. Patton <ewpatton@mit.edu>
2019-09-10 14:01:46 -0400

    Internationalize new 'no project dialog'
    Change-Id: I9802f22cbe9fba8ed768de995d40b315ec3483ed

--- appinventor/appengine/src/com/google/appinventor/client/explorer/dialogs/NoProjectDialogBox.java
+++ appinventor/appengine/src/com/google/appinventor/client/explorer/dialogs/NoProjectDialogBox.java
@@ -1,27 +1,23 @@
 // -*- mode: java; c-basic-offset: 2; -*-
-// Copyright 2009-2011 Google, All Rights reserved
-// Copyright 2011-2017 MIT, All rights reserved
+// Copyright 2019 MIT, All rights reserved
 // Released under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 
 package com.google.appinventor.client.explorer.dialogs;
 
+import com.google.appinventor.client.Ode;
+import com.google.appinventor.client.explorer.project.Project;
+import com.google.appinventor.client.wizards.NewProjectWizard.NewProjectCommand;
+import com.google.appinventor.client.wizards.TemplateUploadWizard;
+import com.google.appinventor.client.wizards.youngandroid.NewYoungAndroidProjectWizard;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.appinventor.client.Images;
-import com.google.appinventor.client.Ode;
-import com.google.appinventor.client.explorer.project.Project;
-import com.google.appinventor.client.wizards.TemplateUploadWizard;
-import com.google.appinventor.client.wizards.youngandroid.NewYoungAndroidProjectWizard;
-import com.google.appinventor.client.wizards.NewProjectWizard.NewProjectCommand;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 
 /**
  * A dialog containing options to begin 3 different tutorials or being a new
@@ -30,66 +26,72 @@ import com.google.gwt.event.dom.client.ClickHandler;
  */
 public class NoProjectDialogBox extends DialogBox {
 
-    private static NoProjectDialogBoxUiBinder uiBinder =
-    GWT.create(NoProjectDialogBoxUiBinder.class);
+  private static NoProjectDialogBoxUiBinder uiBinder =
+      GWT.create(NoProjectDialogBoxUiBinder.class);
 
-    interface NoProjectDialogBoxUiBinder extends UiBinder<Widget, NoProjectDialogBox> {};
+  interface NoProjectDialogBoxUiBinder extends UiBinder<Widget, NoProjectDialogBox> {
+  }
 
-    /**
-    * Class to open a new project with the tutorial's contents when the user
-    * clicks on the "Go to Tutorial" button.
-    */
-    private class NewTutorialProject implements NewProjectCommand {
-        public void execute(Project project) {
-            Ode.getInstance().openYoungAndroidProjectInDesigner(project);
-        }
+  /**
+   * Class to open a new project with the tutorial's contents when the user
+   * clicks on the "Go to Tutorial" button.
+   */
+  private class NewTutorialProject implements NewProjectCommand {
+    public void execute(Project project) {
+      Ode.getInstance().openYoungAndroidProjectInDesigner(project);
     }
+  }
 
-    @UiField Button closeDialogBox;
-    @UiField Button goToPurr;
-    @UiField Button goToTalk;
-    @UiField Button goToYR;
-    @UiField Button noDialogNewProject;
+  @UiField
+  Button closeDialogBox;
+  @UiField
+  Button goToPurr;
+  @UiField
+  Button goToTalk;
+  @UiField
+  Button goToYR;
+  @UiField
+  Button noDialogNewProject;
 
-    /**
-    * Creates a new dialog box when the user has no current projects in their
-    * projects list. This will give them an option to open a tutorial project or
-    * create their own project.
-    */
-    public NoProjectDialogBox() {
-        this.setStylePrimaryName("ode-noDialogDiv");
-        add(uiBinder.createAndBindUi(this));
-        this.center();
-        this.setAnimationEnabled(true);
-        this.setAutoHideEnabled(true);
-    }
+  /**
+   * Creates a new dialog box when the user has no current projects in their
+   * projects list. This will give them an option to open a tutorial project or
+   * create their own project.
+   */
+  public NoProjectDialogBox() {
+    this.setStylePrimaryName("ode-noDialogDiv");
+    add(uiBinder.createAndBindUi(this));
+    this.center();
+    this.setAnimationEnabled(true);
+    this.setAutoHideEnabled(true);
+  }
 
-    @UiHandler("closeDialogBox")
-    void handleClose(ClickEvent e) {
-        this.hide();
-    }
+  @UiHandler("closeDialogBox")
+  void handleClose(ClickEvent e) {
+    this.hide();
+  }
 
-    @UiHandler("goToPurr")
-    void handleGoToPurr(ClickEvent e) {
-        this.hide();
-        new TemplateUploadWizard().createProjectFromExistingZip("HelloPurr", new NewTutorialProject());
-    }
+  @UiHandler("goToPurr")
+  void handleGoToPurr(ClickEvent e) {
+    this.hide();
+    new TemplateUploadWizard().createProjectFromExistingZip("HelloPurr", new NewTutorialProject());
+  }
 
-    @UiHandler("goToTalk")
-    void handleGoToTalk(ClickEvent e) {
-        this.hide();
-        TemplateUploadWizard.openProjectFromTemplate("http://appinventor.mit.edu/yrtoolkit/yr/aiaFiles/talk_to_me/TalkToMe.asc", new NewTutorialProject());
-    }
+  @UiHandler("goToTalk")
+  void handleGoToTalk(ClickEvent e) {
+    this.hide();
+    TemplateUploadWizard.openProjectFromTemplate("http://appinventor.mit.edu/yrtoolkit/yr/aiaFiles/talk_to_me/TalkToMe.asc", new NewTutorialProject());
+  }
 
-    @UiHandler("goToYR")
-    void handleGoToYR(ClickEvent e) {
-        this.hide();
-        TemplateUploadWizard.openProjectFromTemplate("http://appinventor.mit.edu/yrtoolkit/yr/aiaFiles/hello_bonjour/translate_tutorial.asc", new NewTutorialProject());
-    }
+  @UiHandler("goToYR")
+  void handleGoToYR(ClickEvent e) {
+    this.hide();
+    TemplateUploadWizard.openProjectFromTemplate("http://appinventor.mit.edu/yrtoolkit/yr/aiaFiles/hello_bonjour/translate_tutorial.asc", new NewTutorialProject());
+  }
 
-    @UiHandler("noDialogNewProject")
-    void handleNewProject(ClickEvent e) {
-        this.hide();
-        new NewYoungAndroidProjectWizard(null).show();
-    }
+  @UiHandler("noDialogNewProject")
+  void handleNewProject(ClickEvent e) {
+    this.hide();
+    new NewYoungAndroidProjectWizard(null).show();
+  }
 }
