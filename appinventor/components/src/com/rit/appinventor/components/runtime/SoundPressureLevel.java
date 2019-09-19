964776ab3a2cda11445a36188bff4a589440472c
Ryan Connors <connorsryanj@gmail.com>
2019-09-19 17:20:05 -0400

    Initial Commit

---
+++ appinventor/components/src/com/rit/appinventor/components/runtime/SoundPressureLevel.java
@@ -0,0 +1,155 @@
+package com.rit.appinventor.components.runtime;
+
+import android.content.Context;
+import android.util.Log;
+import com.google.appinventor.components.annotations.*;
+import com.google.appinventor.components.common.ComponentCategory;
+import com.google.appinventor.components.common.PropertyTypeConstants;
+import com.google.appinventor.components.common.YaVersion;
+import com.google.appinventor.components.runtime.*;
+import android.Manifest;
+import android.media.MediaRecorder;
+import android.media.MediaRecorder.OnErrorListener;
+import android.media.MediaRecorder.OnInfoListener;
+import android.app.Activity;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.os.Environment;
+
+@DesignerComponent(version = YaVersion.SOUNDPRESSURELEVEL_COMPONENT_VERION,
+        description = "Non-visible component that can collect sound pressure level data",
+        category = ComponentCategory.SENSORS,
+        nonVisible = true,
+        iconName = "images/extension.png")
+@SimpleObject(external = true)
+public class SoundPressureLevel extends AndroidNonvisibleComponent
+        implements OnStopListener, OnResumeListener, Deleteable {
+
+    private final static String LOG_TAG = "SoundPressureLevel";
+    private boolean isEnabled;
+    //private String RecordingBuffer = [];
+    private static final int audioSource = MIC;
+    private static final int sampleRateInHz = 441000;
+    private static final int channelConfig = CHANNEL_IN_MONO;
+    private static final int audioFormat = ENCODING_PCM_16BIT;
+    private static final int minBufferSize = AudioRecord.getMinBufferSize(sampleRateInHz,channelConfig,audioFormat);
+
+    public BarometerSensor(ComponentContainer container) {
+        super(container.$form());
+
+        recorder = new AudioRecord(MIC, sampleRateInHz, channelConfig, audioFormat, minBufferSize);
+        form.registerForOnResume(this);
+        form.registerForOnStop(this);
+        Enabled(true);
+        Log.d(LOG_TAG, "spl created");
+    }
+
+    @Override
+    public void onDelete() {
+        if (isEnabled) {
+            stopListening();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        if (isEnabled) {
+            startListening();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        if (isEnabled) {
+            stopListening();
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (isEnabled) {
+            BarometerChanged(sensorEvent.values[0]);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        // TODO(markf): Figure out if we actually need to do something here.
+    }
+
+    /**
+     * Assumes that audioRecord has been initialized, which happens in constructor
+     */
+    private void startListening() {
+        recorder.startRecording();
+    }
+
+    /**
+     * Assumes that audioRecorder has been initialized, which happens in constructor
+     */
+    private void stopListening() {
+        recorder.stop();
+    }
+
+    /**
+     * Specifies whether the sensor should generate events.  If true,
+     * the sensor will generate events.  Otherwise, no events are
+     * generated even if the device is accelerated or shaken.
+     *
+     * @param enabled {@code true} enables sensor event generation,
+     *                {@code false} disables it
+     */
+    @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_BOOLEAN,
+            defaultValue = "True")
+    @SimpleProperty
+    public void Enabled(boolean enabled) {
+        if (this.isEnabled != enabled) {
+            this.isEnabled = enabled;
+            if (enabled) {
+                startListening();
+            } else {
+                stopListening();
+            }
+        }
+    }
+
+    /**
+     * Available property getter method (read-only property).
+     *
+     * @return {@code true} indicates that an accelerometer sensor is available,
+     * {@code false} that it isn't
+     */
+    @SimpleProperty(
+            category = PropertyCategory.BEHAVIOR)
+    public boolean Available() {
+        return (sensorManager.getSensorList(Sensor.TYPE_PRESSURE).size() > 0);
+    }
+
+    /**
+     * If true, the sensor will generate events.  Otherwise, no events
+     * are generated
+     *
+     * @return {@code true} indicates that the sensor generates events,
+     * {@code false} that it doesn't
+     */
+    @SimpleProperty(
+            category = PropertyCategory.BEHAVIOR)
+    public boolean Enabled() {
+        return isEnabled;
+    }
+
+    @SimpleProperty(
+            category = PropertyCategory.BEHAVIOR)
+    public float Millibar() {
+        return currentMillibar;
+    }
+
+    /**
+     * Indicates the barometer changed.
+     */
+    @SimpleEvent
+    public void BarometerChanged(float mbar) {
+        this.currentMillibar = mbar;
+        EventDispatcher.dispatchEvent(this, "BarometerChanged", this.currentMillibar);
+    }
+}
