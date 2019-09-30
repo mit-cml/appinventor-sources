c86b911a1796d40dc4a0d31c52574ab14e86c480
rgangela99 <rgangela99@gmail.com>
2019-09-29 21:47:54 -0400

    Make delta even bigger

--- appinventor/components/tests/com/google/appinventor/components/runtime/util/GeoJSONUtilTest.java
+++ appinventor/components/tests/com/google/appinventor/components/runtime/util/GeoJSONUtilTest.java
@@ -403,14 +403,14 @@ public class GeoJSONUtilTest extends MapTestBase {
 
   private static void assertTestProperties(MapFeatureBaseWithFill feature) {
     assertEquals(COLOR_BLUE, feature.FillColor() | 0xFF000000);
-    assertEquals(0.4, feature.FillOpacity(), 1e-8);
+    assertEquals(0.4, feature.FillOpacity(), 1e-6);
     assertEquals(Math.round(0.4 * 255), feature.FillColor() >>> 24);
     assertTestProperties((MapFeatureBase) feature);
   }
 
   private static void assertTestProperties(MapFeatureBase feature) {
     assertEquals(COLOR_GREEN, feature.StrokeColor() | 0xFF000000);
-    assertEquals(0.7, feature.StrokeOpacity(), 1e-8);
+    assertEquals(0.7, feature.StrokeOpacity(), 1e-6);
     assertEquals(Math.round(0.7 * 255), feature.StrokeColor() >>> 24);
     assertEquals(3, feature.StrokeWidth());
     assertTestProperties((MapFeature) feature);
