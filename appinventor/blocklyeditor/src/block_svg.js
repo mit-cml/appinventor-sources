0917f35c7ab5ff987494393ed327bec3f6af192f
Evan W. Patton <ewpatton@mit.edu>
2019-10-30 17:49:37 -0400

    Implement extra null checks on flydown
    Change-Id: I9fc9e5516c329ac8b286fc075ad0b863018164bb

--- appinventor/blocklyeditor/src/block_svg.js
+++ appinventor/blocklyeditor/src/block_svg.js
@@ -66,9 +66,14 @@ Blockly.BlockSvg.prototype.onMouseDown_ = (function(func) {
         workspace.getParentSvg().parentNode.focus();
       }
       if (Blockly.FieldFlydown.openFieldFlydown_) {
-        if (goog.dom.contains(Blockly.getMainWorkspace().flydown_.svgGroup_, this.svgGroup_)) {
-          //prevent hiding the flyout if a child block is the target
-          Blockly.getMainWorkspace().flydown_.shouldHide = false;
+        var flydown = Blockly.getMainWorkspace().getFlydown();
+        if (flydown) {
+          if (goog.dom.contains(flydown.svgGroup_, this.svgGroup_)) {
+            //prevent hiding the flyout if a child block is the target
+            flydown.shouldHide = false;
+          }
+        } else {
+          console.warn('openFieldFlydown_ was set but flydown_ was undefined!');
         }
       }
       var retval = func.call(this, e);
