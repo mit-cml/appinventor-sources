74bd7e556b98708efb1624bca0c82059ab7a9880
rgangela99 <rgangela99@gmail.com>
2019-12-10 14:06:34 -0500

    Add transportation methods to OdeMessages

--- appinventor/appengine/src/com/google/appinventor/client/editor/youngandroid/properties/YoungAndroidNavigationMethodChoicePropertyEditor.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/youngandroid/properties/YoungAndroidNavigationMethodChoicePropertyEditor.java
@@ -1,6 +1,5 @@
 // -*- mode: java; c-basic-offset: 2; -*-
-// Copyright 2009-2011 Google, All Rights reserved
-// Copyright 2011-2012 MIT, All rights reserved
+// Copyright 2019 MIT, All rights reserved
 // Released under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 package com.google.appinventor.client.editor.youngandroid.properties;
@@ -8,19 +7,18 @@ package com.google.appinventor.client.editor.youngandroid.properties;
 import com.google.appinventor.client.widgets.properties.ChoicePropertyEditor;
 import com.google.appinventor.components.common.ComponentConstants;
 
-// cludge for now.  mimic how this is done for button shape
-
+import static com.google.appinventor.client.Ode.MESSAGES;
 
 /**
- * Property editor for vertical alignment choice.
+ * Property editor for navigation method choice.
  */
 public class YoungAndroidNavigationMethodChoicePropertyEditor extends ChoicePropertyEditor {
 
   private static final Choice[] methods = new Choice[] {
-    new Choice("foot-walking", "foot-walking"),
-    new Choice("driving-car", "driving-car"),
-    new Choice("cycling-regular", "cycling-regular"),
-    new Choice ("wheelchair", "wheelchair")
+    new Choice(MESSAGES.Walking(), MESSAGES.Walking()),
+    new Choice(MESSAGES.Driving(), MESSAGES.Driving()),
+    new Choice(MESSAGES.Cycling(), MESSAGES.Cycling()),
+    new Choice (MESSAGES.Wheelchair(), MESSAGES.Wheelchair())
   };
 
   public YoungAndroidNavigationMethodChoicePropertyEditor() {
