992ed80b0cf9935a76601494d640cf9f8c40d525
Pavitra <pavitragolchha@gmail.com>
2019-12-30 22:57:59 +0530

    Created a test visible extension

---
+++ appinventor/components/src/test/SimpleLabel.java
@@ -0,0 +1,36 @@
+package test;
+
+import android.view.View;
+import android.widget.TextView;
+import com.google.appinventor.components.annotations.DesignerComponent;
+import com.google.appinventor.components.annotations.SimpleFunction;
+import com.google.appinventor.components.annotations.SimpleObject;
+import com.google.appinventor.components.common.ComponentCategory;
+import com.google.appinventor.components.runtime.AndroidViewComponent;
+import com.google.appinventor.components.runtime.ComponentContainer;
+
+@DesignerComponent(version = 1,
+        description = "Simple Label extension",
+        category = ComponentCategory.EXTENSION,
+        iconName = "images/extension.png")
+@SimpleObject(external = true)
+public class SimpleLabel extends AndroidViewComponent {
+
+    private TextView tv;
+
+    public SimpleLabel(ComponentContainer container) {
+        super(container.$form());
+
+        tv = new TextView(container.$context());
+    }
+
+    @Override
+    public View getView() {
+        return tv;
+    }
+
+    @SimpleFunction()
+    public void Text(String text) {
+        tv.setText(text);
+    }
+}
