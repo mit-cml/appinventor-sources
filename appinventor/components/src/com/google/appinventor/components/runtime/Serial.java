7717d118715ba7846c7eb54f495cab30b84d8c90
Diego Barreiro <diego.barreiro.perez@gmail.com>
2020-02-15 00:39:06 +0100

    FIX: Not needed import

--- appinventor/components/src/com/google/appinventor/components/runtime/Serial.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/Serial.java
@@ -15,7 +15,6 @@ import com.google.appinventor.components.common.ComponentCategory;
 import com.google.appinventor.components.annotations.DesignerComponent;
 import com.google.appinventor.components.annotations.DesignerProperty;
 import com.google.appinventor.components.annotations.PropertyCategory;
-import com.google.appinventor.components.annotations.SimpleEvent;
 import com.google.appinventor.components.annotations.SimpleFunction;
 import com.google.appinventor.components.annotations.SimpleObject;
 import com.google.appinventor.components.annotations.SimpleProperty;
