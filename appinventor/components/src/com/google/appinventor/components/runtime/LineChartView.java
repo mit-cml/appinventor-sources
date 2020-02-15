ce560b1d576dfca31c71e336d0df2de3f900a161
Evaldas Latoskinas <evaldasl61@gmail.com>
2020-02-15 23:25:24 +0100

    Add (missing) docs to LineChartView

--- appinventor/components/src/com/google/appinventor/components/runtime/LineChartView.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/LineChartView.java
@@ -1,23 +1,26 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2019-2020 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 package com.google.appinventor.components.runtime;
 
-import android.app.Activity;
-import android.view.ViewGroup;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.LineData;
-
+/**
+ * Class for handling the UI (view) of the Line Chart for the Chart component.
+ * @see com.google.appinventor.components.runtime.ChartView
+ */
 public class LineChartView extends LineChartViewBase {
-    /**
-     * Instantiate a new LineChartView in the given context.
-     *
-     * @param context Context to instantiate view in
-     */
-    public LineChartView(Form context) {
-        super(context);
-    }
+  /**
+   * Instantiate a new LineChartView in the given context.
+   *
+   * @param context Context to instantiate view in
+   */
+  public LineChartView(Form context) {
+    super(context);
+  }
 
-    @Override
-    public ChartDataModel createChartModel() {
-        return new LineChartDataModel(data);
-    }
+  @Override
+  public ChartDataModel createChartModel() {
+    return new LineChartDataModel(data);
+  }
 }
