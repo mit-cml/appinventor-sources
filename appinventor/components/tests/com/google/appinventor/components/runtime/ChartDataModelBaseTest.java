299aec95a9603e6c4527a70a0f01e76d508bad6e
Evaldas Latoskinas <evaldasl61@gmail.com>
2020-02-16 13:36:23 +0100

    Add copyright preambles to Chart test files

--- appinventor/components/tests/com/google/appinventor/components/runtime/ChartDataModelBaseTest.java
+++ appinventor/components/tests/com/google/appinventor/components/runtime/ChartDataModelBaseTest.java
@@ -1,3 +1,8 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2019-2020 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 package com.google.appinventor.components.runtime;
 
 import com.github.mikephil.charting.data.ChartData;
