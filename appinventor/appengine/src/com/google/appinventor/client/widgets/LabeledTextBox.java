9048ac1d2a360606982e6f5c18be35de3c7e3ec9
Evan W. Patton <ewpatton@mit.edu>
2020-04-13 10:44:57 -0400

    Fix delete assets not working in nb183a
    Change-Id: I7aef0856d098b490d36d36d410dc9763ccb5a034

--- appinventor/appengine/src/com/google/appinventor/client/widgets/LabeledTextBox.java
+++ appinventor/appengine/src/com/google/appinventor/client/widgets/LabeledTextBox.java
@@ -1,6 +1,6 @@
 // -*- mode: java; c-basic-offset: 2; -*-
 // Copyright 2009-2011 Google, All Rights reserved
-// Copyright 2011-2018 MIT, All rights reserved
+// Copyright 2011-2020 MIT, All rights reserved
 // Released under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 
@@ -155,9 +155,9 @@ public class LabeledTextBox extends Composite {
   public void setErrorMessage(String errorMessage) {
     this.errorMessage = errorMessage;
   }
-  
+
   public void setColor(String color) {
-	textbox.getElement().getStyle().setBorderColor(color);
+    textbox.getElement().getStyle().setBorderColor(color);
   }
 
   /**
