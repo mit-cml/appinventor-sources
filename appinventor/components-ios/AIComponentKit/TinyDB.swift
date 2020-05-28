181c880241e26b68aa66c150292ac56d6ce83b76
Evan W. Patton <ewpatton@mit.edu>
2020-05-28 09:50:15 -0400

    Implement Namespace property on TinyDB (mit-cml/appinventor-sources-ios#464)
    * Implement Namespace property on TinyDB
    
    Change-Id: If512d0a05303b3020384fb3240bf13347a296a27
    
    * Switch to older style XCTest configuration
    
    Change-Id: Iec9d61106daa33070f3444f30043657a566c24a7

--- appinventor/components-ios/AIComponentKit/TinyDB.swift
+++ appinventor/components-ios/AIComponentKit/TinyDB.swift
@@ -1,10 +1,5 @@
-//
-//  TinyDB.swift
-//  AIComponentKit
-//
-//  Created by Evan Patton on 10/20/16.
-//  Copyright © 2016 MIT Center for Mobile Learning. All rights reserved.
-//
+// -*- mode: swift; swift-mode:basic-offset: 2; -*-
+// Copyright © 2016-2020 Massachusetts Institute of Technology, All rights reserved.
 
 import Foundation
 import CoreData
@@ -13,28 +8,46 @@ import SQLite
 open class TinyDB: NonvisibleComponent {
 
   fileprivate var _database: Connection!
-  fileprivate let _table = Table("tinydb")
+  fileprivate var _table = Table("TinyDB1")
   fileprivate let _key = Expression<String>("_key")
   fileprivate let _value = Expression<String>("_value")
+  private var _namespace = "TinyDB1"
 
   public override init(_ parent: ComponentContainer) {
     let assetmgr = parent.form.application?.assetManager
     let sqlitedb = (assetmgr?.pathForPrivateAsset("TinyDb1.sqlite"))!
     do {
-      _database = try Connection(sqlitedb)
-    } catch {
-      _database = nil
+      _database = try? Connection(sqlitedb)
     }
     super.init(parent)
     do {
-      try _database.run(_table.create(ifNotExists: true) { t in
+      _ = try? _database.run(_table.create(ifNotExists: true) { t in
         t.column(_key, primaryKey: true)
         t.column(_value)
       })
-    } catch {
-      NSLog("Unexpected error creating TinyDB")
     }
   }
+
+  /// MARK: TinyDB Properties
+
+  @objc open var Namespace: String {
+    get {
+      return _namespace
+    }
+    set (namespace) {
+      do {
+        let new_table = Table(namespace)
+        _ = try? _database.run(new_table.create(ifNotExists: true) { t in
+          t.column(_key, primaryKey: true)
+          t.column(_value)
+        })
+        _table = new_table
+        _namespace = namespace
+      }
+    }
+  }
+
+  /// MARK: TinyDB Methods
 
   @objc open func StoreValue(_ tag: String, _ valueToStore: AnyObject) {
     do {
@@ -47,10 +60,9 @@ open class TinyDB: NonvisibleComponent {
 
   @objc open func GetValue(_ tag: String, _ valueIfTagNotThere: AnyObject) -> AnyObject {
     do {
-      if let value = try _database.pluck(_table.select(_value).filter(_key == tag)) {
-        if let result = try getObjectFromJson(value[_value]) {
+      if let value = try _database.pluck(_table.select(_value).filter(_key == tag)),
+        let result = try getObjectFromJson(value[_value]) {
           return result
-        }
       }
     } catch {
       NSLog("Unable to read value from TinyDB")
