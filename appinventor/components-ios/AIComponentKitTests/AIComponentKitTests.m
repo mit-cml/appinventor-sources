85e17ceb8b47bfc7b5e4f840ad1bac372461ada9
Evan W. Patton <ewpatton@mit.edu>
2020-12-02 12:49:12 -0500

    Implement tests for garbage collection
    Change-Id: I060cd293ace7bdb3680b2219b7a05100700e0116

--- appinventor/components-ios/AIComponentKitTests/AIComponentKitTests.m
+++ appinventor/components-ios/AIComponentKitTests/AIComponentKitTests.m
@@ -54,6 +54,7 @@
 - (void)testFontSize {
   SCMInterpreter *interpreter = [[SCMInterpreter alloc] init];
   Form *form = [[Form alloc] init];
+  form.formName = @"Screen1";
   NSURL *runtimeUrl = [[NSBundle bundleForClass:[ReplForm class]] URLForResource:@"runtime" withExtension:@"scm"];
   if (runtimeUrl != nil) {
     if (interpreter.exception != nil) {
@@ -69,10 +70,111 @@
       return;
     }
   }
+  [interpreter evalForm:@"(reset-current-form-environment)"];
   [interpreter evalForm:@"(add-component Screen1 com.google.appinventor.components.runtime.Label Label1)"];
   [interpreter evalForm:@"(set-and-coerce-property! 'Label1 'FontSize 30 'number)"];
   Label *label = (Label *) form.components[0];
   XCTAssertEqual(30.0, ((UILabel *) label.view).font.pointSize);
+}
+
+- (void)testDeinit {
+  SCMInterpreter *interpreter = SCMInterpreter.shared;
+  __weak YailDictionary *environment = nil;
+  __weak Form *weakForm = nil;
+  @autoreleasepool {
+    NSURL *runtimeUrl = [[NSBundle bundleForClass:[ReplForm class]] URLForResource:@"runtime" withExtension:@"scm"];
+    if (runtimeUrl != nil) {
+      if (interpreter.exception != nil) {
+        // FIXME: Happens due to SCMInterpreter not finding runtime.scm when testing
+        [interpreter clearException];
+      }
+      NSString *text = [NSString stringWithContentsOfURL:runtimeUrl encoding:NSUTF8StringEncoding error:nil];
+      [interpreter evalForm:text];
+      if (interpreter.exception != nil) {
+        NSLog(@"Exception loading runtime: %@ (%@)", interpreter.exception.name, interpreter.exception);
+        XCTFail(@"Exception occurred loading runtime");
+        return;
+      }
+    }
+    Form *form = [[Form alloc] init];
+    form.formName = @"Screen1";
+    weakForm = form;
+    [interpreter setCurrentForm:form];
+    [interpreter evalForm:@"(reset-current-form-environment)"];
+    [interpreter evalForm:@"(add-component Screen1 com.google.appinventor.components.runtime.Label Label1)"];
+    environment = form.environment;
+    form = nil;
+  }
+  NSLog(@"Clearing *this-form*");
+  [interpreter evalForm:@"(set! *this-form* #!null)"];
+  NSLog(@"Running GC");
+  [interpreter runGC];
+  NSLog(@"Testing environment");
+  id strongForm = weakForm;
+  if (strongForm != nil) {
+    [interpreter printGCRoots:strongForm];
+  }
+  XCTAssertNil(environment);
+}
+
+- (void)testDeinitMap {
+  SCMInterpreter *interpreter = SCMInterpreter.shared;
+  __weak Form *weakForm = nil;
+  __weak Map *weakMap = nil;
+  @autoreleasepool {
+    NSURL *runtimeUrl = [[NSBundle bundleForClass:[ReplForm class]] URLForResource:@"runtime" withExtension:@"scm"];
+    if (runtimeUrl != nil) {
+      if (interpreter.exception != nil) {
+        // FIXME: Happens due to SCMInterpreter not finding runtime.scm when testing
+        [interpreter clearException];
+      }
+      NSString *text = [NSString stringWithContentsOfURL:runtimeUrl encoding:NSUTF8StringEncoding error:nil];
+      [interpreter evalForm:text];
+      if (interpreter.exception != nil) {
+        NSLog(@"Exception loading runtime: %@ (%@)", interpreter.exception.name, interpreter.exception);
+        XCTFail(@"Exception occurred loading runtime");
+        return;
+      }
+    }
+    Form *form = [[Form alloc] init];
+    form.formName = @"Screen1";
+    weakForm = form;
+    [interpreter setCurrentForm:form];
+    [interpreter evalForm:@"(reset-current-form-environment)"];
+    [interpreter evalForm:@"(add-component Screen1 com.google.appinventor.components.runtime.Map Map1)"];
+    [interpreter evalForm:@"(add-component Map1 com.google.appinventor.components.runtime.Marker Marker1)"];
+    [interpreter evalForm:@"(add-component Map1 com.google.appinventor.components.runtime.LineString LineString1)"];
+    [interpreter evalForm:@"(add-component Map1 com.google.appinventor.components.runtime.Polygon Polygon1)"];
+    [interpreter evalForm:@"(add-component Map1 com.google.appinventor.components.runtime.Rectangle Rectangle1)"];
+    [interpreter evalForm:@"(add-component Map1 com.google.appinventor.components.runtime.Circle Circle1)"];
+    weakMap = (Map *) form.environment[@"Map1"];
+    form = nil;
+  }
+  [interpreter evalForm:@"(set! *this-form* #!null)"];
+  [interpreter runGC];
+  id strongForm = weakForm;
+  if (strongForm) {
+    [interpreter printGCRoots:strongForm];
+  }
+  XCTAssertNil(weakForm);
+  id strongMap = weakMap;
+  if (strongMap) {
+    [interpreter printGCRoots:strongMap];
+  }
+  XCTAssertNil(weakMap);
+}
+
+- (void)testDeinitNoScheme {
+  __weak Form *weakForm = nil;
+  @autoreleasepool {
+    Form *form = [[Form alloc] init];
+    form.formName = @"Screen1";
+    Map *map = [[Map alloc] init:form];
+    weakForm = form;
+    form = nil;
+  }
+  Form *strongForm = weakForm;
+  XCTAssertNil(strongForm);
 }
 
 @end
