c38e25283a518649988c8d71da3bcf286f1f883d
Rupesh Kumar <46577873+rupesh-kumar-lpu@users.noreply.github.com>
2021-01-04 09:08:27 +0530

    Method to print stream and record in buffer

--- appinventor/buildserver/src/com/google/appinventor/buildserver/Execution.java
+++ appinventor/buildserver/src/com/google/appinventor/buildserver/Execution.java
@@ -30,6 +30,38 @@ public final class Execution {
   private static final Joiner joiner = Joiner.on(" ");
 
   /*
+   * Input stream handler and string buffer recorder used for stdout/stderr redirection and to return results in StringBuffer
+   */
+  private static class RedirectStreamHandleAndgetBuffer extends Thread {
+    // Streams to redirect from and to
+    private final InputStream input;
+    private final PrintWriter output;
+    private final StringBuffer outputbuffer;
+
+    RedirectStreamHandleAndgetBuffer(PrintWriter output, InputStream input, StringBuffer outputbuffer) {
+      this.input = Preconditions.checkNotNull(input);
+      this.output = Preconditions.checkNotNull(output);
+      this.outputbuffer = Preconditions.checkNotNull(outputbuffer);
+      start();
+    }
+
+    @Override
+    public void run() {
+      try {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        String line;
+        while ((line = reader.readLine()) != null) {
+          output.println(line);
+          outputbuffer.append(line).append("\n");
+        }
+      } catch (IOException ioe) {
+        // OK to ignore...
+        LOG.log(Level.WARNING, "____I/O Redirection failure: ", ioe);
+      }
+    }
+  }
+
+  /*
    * Input stream handler used for stdout and stderr redirection.
    */
   private static class RedirectStreamHandler extends Thread {
@@ -115,6 +147,36 @@ public final class Execution {
     } catch (Exception e) {
       LOG.log(Level.WARNING, "____Execution failure: ", e);
       return false;
+    }
+  }
+
+  /**
+   * Executes a command in a command shell.
+   *
+   * @param workingDir  working directory for the command
+   * @param command  command to execute and its arguments
+   * @param out  standard output stream to redirect to
+   * @param err  standard error stream to redirect to
+   * @return  String Array with three arguments [true/false-String,Out-String,Err-String]
+   */
+  public static String[] executeandget(File workingDir, String[] command, PrintStream out,
+                                       PrintStream err) {
+    LOG.log(Level.INFO, "____Executing " + joiner.join(command));
+    if (System.getProperty("os.name").startsWith("Windows")){
+      for(int i =0; i < command.length; i++){
+        command[i] = command[i].replace("\"", "\\\"");
+      }
+    }
+    try {
+      Process process = Runtime.getRuntime().exec(command, null, workingDir);
+      StringBuffer strBufferOut=new StringBuffer();
+      StringBuffer strBufferErr=new StringBuffer();
+      new RedirectStreamHandleAndgetBuffer(new PrintWriter(out, true), process.getInputStream(),strBufferOut);
+      new RedirectStreamHandleAndgetBuffer(new PrintWriter(err, true), process.getErrorStream(),strBufferErr);
+      return new String[]{Boolean.toString(process.waitFor() == 0),strBufferOut.toString(),strBufferErr.toString()};
+    } catch (Exception e) {
+      LOG.log(Level.WARNING, "____Execution failure: ", e);
+      return new String[]{"false","",""}; //Must have size 3
     }
   }
 
