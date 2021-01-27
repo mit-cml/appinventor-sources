a506beb260dc444640093c1358252b73d1e1500d
Diego Barreiro <diego.barreiro.perez@gmail.com>
2021-01-27 18:45:34 +0100

    [WIP] Merge both versions
    Missing AabZipper, AabCompiler, Compiler and ProjectBuilder

--- appinventor/buildserver/src/com/google/appinventor/buildserver/YoungAndroidConstants.java
+++ appinventor/buildserver/src/com/google/appinventor/buildserver/YoungAndroidConstants.java
@@ -1,5 +1,16 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2009-2011 Google, All Rights reserved
+// Copyright 2011-2021 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 package com.google.appinventor.buildserver;
 
+/**
+ * Constants related to Young Android projects and files.
+ *
+ * @author lizlooney@google.com (Liz Looney)
+ */
 public class YoungAndroidConstants {
   private YoungAndroidConstants() {}
 
