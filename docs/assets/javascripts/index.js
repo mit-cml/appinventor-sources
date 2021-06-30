6d918051bdaad3e7b20dbebdd727c6c201f24e91
Preet Vadaliya <preetvadaliya.ict18@gmail.com>
2021-06-30 15:45:26 +0530

    index.js updated

--- docs/assets/javascripts/index.js
+++ docs/assets/javascripts/index.js
@@ -1,55 +1,78 @@
-let options = {
-  scrollbars: false,
-  readOnly: true,
-};
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright © 2013-2021 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 
-function renderBlock(blockType, workspaceId, options, componentInfo, workspaceElement) {
-  let workspace = Blockly.inject(workspaceId, options);
-  if (componentInfo != null) {
-    Blockly.Blocks[blockType].initComponent(componentInfo);
-    if (componentInfo[1] == "get") {
-      workspaceElement.style.marginBottom = "8px";
-    }
-  }
-  let block = workspace.newBlock(blockType);
+/**
+* Main Index file for rendering process.
+* @author preetvadaliya.ict18@gmail.com (Preet Vadaliya)
+*/
+
+function renderBlock(block, workspaceElement) {
   block.initSvg();
   block.render();
-  block.moveBy(8, 8);
+  if (block.outputConnection != null) {
+    block.moveBy(16, 8);
+  } else {
+    block.moveBy(8, 8);
+  }
   workspaceElement.style.height = block.getHeightWidth()["height"] + 16 + "px";
-  Blockly.svgResize(workspace);
 }
 
-Array.from(document.querySelectorAll("[block-type = component_method]")).forEach(element => {
-  let componentInfo = [];
-  componentInfo.push(element.getAttribute("component-selector"));
-  componentInfo.push(element.getAttribute("method-selector"));
-  componentInfo.push(element.getAttribute("method-params").split("-"));
-  componentInfo.push(element.getAttribute("return-type"));
-  renderBlock("component_method", element.id, {scrollbars: false,readOnly: true}, componentInfo, element);
-});
-
-Array.from(document.querySelectorAll("[block-type = component_set_get]")).forEach(element => {
-  let componentInfo = [];
-  componentInfo.push(element.getAttribute("component-selector"));
-  componentInfo.push(element.getAttribute("property-type"));
-  componentInfo.push(element.getAttribute("property-selector"));
-  renderBlock("component_set_get", element.id, {scrollbars: false,readOnly: true}, componentInfo, element);
-});
-
-Array.from(document.querySelectorAll("[block-type = component_component_block]")).forEach(element => {
-  let componentInfo = [];
-  componentInfo.push(element.getAttribute("component-selector"));
-  renderBlock("component_component_block", element.id, {scrollbars: false,readOnly: true}, componentInfo, element);
-});
-
+// Render event blocks.
 Array.from(document.querySelectorAll("[block-type = component_event]")).forEach(element => {
-  let componentInfo = [];
-  componentInfo.push(element.getAttribute("component-selector"));
-  componentInfo.push(element.getAttribute("event-selector"));
-  componentInfo.push(element.getAttribute("event-params").split("-"));
-  renderBlock("component_event", element.id, {scrollbars: false,readOnly: true}, componentInfo, element);
+  let componentType = element.getAttribute("component-selector");
+  let eventName = element.getAttribute("event-selector");
+  let workspace = Blockly.inject(element.id, { scrollbars: false, readOnly: true });
+  let block = workspace.newBlock('component_event');
+  block.initComponent(componentType, eventName, false, true);
+  renderBlock(block, element);
 });
 
+// Render method blocks.
+Array.from(document.querySelectorAll("[block-type = component_method]")).forEach(element => {
+  let componentType = element.getAttribute("component-selector");
+  let methodName = element.getAttribute("method-selector");
+  let workspace = Blockly.inject(element.id, { scrollbars: false, readOnly: true });
+  let block = workspace.newBlock('component_method');
+  block.initComponent(componentType, methodName, false);
+  renderBlock(block, element);
+});
+
+// Render property set or get blocks.
+Array.from(document.querySelectorAll("[block-type = component_set_get]")).forEach(element => {
+  let componentType = element.getAttribute("component-selector");
+  let propertyName = element.getAttribute("property-selector");
+  let setOrGet = element.getAttribute("property-type");
+  let workspace = Blockly.inject(element.id, { scrollbars: false, readOnly: true });
+  let block = workspace.newBlock('component_set_get');
+  block.initComponent(componentType, setOrGet, false, propertyName);
+  if (setOrGet == "get") {
+    element.style.marginBottom = "8px";
+  }
+  renderBlock(block, element);
+});
+
+// Render component blocks.
+Array.from(document.querySelectorAll("[block-type = component_component_block]")).forEach(element => {
+  let componentType = element.getAttribute("component-selector");
+  let workspace = Blockly.inject(element.id, { scrollbars: false, readOnly: true });
+  let block = workspace.newBlock('component_component_block');
+  block.initComponent(componentType);
+  renderBlock(block, element);
+});
+
+// Render default blocks.
 Array.from(document.querySelectorAll("[type = ai-2-block]")).forEach(element => {
-  renderBlock(element.id, element.id, {scrollbars: false,readOnly: true}, null, element);
+  let workspace = Blockly.inject(element.id, { scrollbars: false, readOnly: true });
+  let block = workspace.newBlock(element.id);
+  renderBlock(block, element);
+});
+
+Array.from(document.querySelectorAll("[type = ai-2-default-block]")).forEach(element => {
+  let workspace = Blockly.inject(element.id, { scrollbars: false, readOnly: true });
+  let blockXML = Blockly.Xml.textToDom(DEFAULT_BLOCKS[element.id]);
+  blockXML = blockXML.firstElementChild;
+  let block = Blockly.Xml.domToBlock(blockXML, workspace);
+  renderBlock(block, element);
 });
