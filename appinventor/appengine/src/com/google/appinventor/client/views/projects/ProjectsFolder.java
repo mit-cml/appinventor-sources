ff06a51e457c6d62267074be115db6630fa6a8a9
Vishwas-Adiga <vishwasadiga@gmail.com>
2021-07-08 01:37:53 +0530

    Groundwork on implementing folders

---
+++ appinventor/appengine/src/com/google/appinventor/client/views/projects/ProjectsFolder.java
@@ -0,0 +1,13 @@
+package com.google.appinventor.client.views.projects;
+
+import com.google.appinventor.client.explorer.project.Project;
+
+import java.util.List;
+
+public interface ProjectsFolder {
+  public List<Project> getSelectedProjects();
+
+  public void refresh();
+
+  public void setSelectionOfAll(boolean selectionState);
+}
