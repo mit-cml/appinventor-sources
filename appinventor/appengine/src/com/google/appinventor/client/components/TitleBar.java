2cc89e0353dddcc6c6e3a504d92a5c048dd697be
Vishwas-Adiga <vishwasadiga@gmail.com>
2021-07-08 01:36:16 +0530

    Add placeholder top bar (temp)

--- appinventor/appengine/src/com/google/appinventor/client/components/TitleBar.java
+++ appinventor/appengine/src/com/google/appinventor/client/components/TitleBar.java
@@ -3,9 +3,10 @@ package com.google.appinventor.client.components;
 import com.google.appinventor.client.Ode;
 
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
 
 public class TitleBar extends Composite {
   public TitleBar() {
-    initWidget(Ode.getInstance().getTopPanel());
+    initWidget(new FlowPanel());
   }
 }
