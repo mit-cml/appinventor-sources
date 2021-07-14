783cf0f00dff9f1b409253e68c21db88cc3269e0
Evan W. Patton <ewpatton@mit.edu>
2021-07-14 12:57:06 -0400

    Update to SDK 30
    Change-Id: If7ab18f23f6bbc40929df4b4ab36601c7a610dcc

--- appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
+++ appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
@@ -642,9 +642,9 @@ public class YaVersion {
 
   // ................................. Target SDK Version Number ..................................
 
-  public static final int TARGET_SDK_VERSION = 29;
+  public static final int TARGET_SDK_VERSION = 30;
 
-  public static final String TARGET_ANDROID_VERSION = "Android 10.0";
+  public static final String TARGET_ANDROID_VERSION = "Android 11.0";
 
   // ................................. Component Version Numbers ..................................
 
