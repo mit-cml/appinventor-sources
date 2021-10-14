ed163a20ff9fe315bab09a6c21cc035b38e7354b
Evan W. Patton <ewpatton@mit.edu>
2021-10-14 15:51:28 -0400

    Add link for language tag documentation
    Change-Id: I4ef1b13567b541f00cec7921c6b97dc5cf439071

--- appinventor/components/src/com/google/appinventor/components/runtime/SpeechRecognizer.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/SpeechRecognizer.java
@@ -78,7 +78,9 @@ public class SpeechRecognizer extends AndroidNonvisibleComponent
    * Suggests the language to use for recognizing speech. An empty string (the default) will
    * use the system's default language.
    *
-   *     Language is specified using an IETF language tag, such as en-US or es-MX.
+   *     Language is specified using a [language tag](https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes)
+   *     with an optional region suffix, such as en or es-MX. The set of supported languages will
+   *     vary by device.
    *
    * @return the target language for recognition
    */
