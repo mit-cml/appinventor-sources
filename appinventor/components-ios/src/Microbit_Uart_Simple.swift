d42f871b80cf6c10dd6d83cf91b04c92f9138304
Evan W. Patton <ewpatton@mit.edu>
2022-12-01 09:27:23 -0500

    Start implementing Micro:bit UART component
    Change-Id: Ic4c61bac111217c0916ae5806c3fe87147559ad0

---
+++ appinventor/components-ios/src/Microbit_Uart_Simple.swift
@@ -0,0 +1,90 @@
+// -*- mode: swift; swift-mode:basic-offset: 2; -*-
+// Copyright © 2022 Massachusetts Institute of Technology. All rights reserved.
+
+import Foundation
+
+fileprivate let kMicrobitUartService = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+fileprivate let kMicrobitUartCharacteristic = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+@objc public class Microbit_Uart_Simple : NonvisibleComponent, BluetoothConnectionListener {
+
+  private var bleConnection: BluetoothLE? = nil
+
+  @objc public override init(_ container: ComponentContainer) {
+    super.init(container)
+  }
+
+  // MARK: Properties
+
+  @objc public var BluetoothDevice: BluetoothLE? {
+    get {
+      return self.bleConnection
+    }
+    set {
+      if let oldConn = self.bleConnection {
+        oldConn.removeConnectionListener(self)
+      }
+      self.bleConnection = newValue
+      if let conn = self.bleConnection {
+        conn.addConnectionListener(self)
+      }
+    }
+  }
+
+  // MARK: Methods
+
+  @objc public func SendMessage(_ message: String) {
+    guard let conn = self.bleConnection else {
+      reportNullConnection("SendMessage")
+      return
+    }
+    conn.ExWriteStringValuesWithResponse(kMicrobitUartService, kMicrobitUartCharacteristic,
+        false, message, self.rxCharacteristicWriteHandler)
+  }
+
+  // MARK: Events
+
+  @objc public func MessageReceived(_ message: String) {
+    EventDispatcher.dispatchEvent(of: self, called: "MessageReceived",
+        arguments: message as AnyObject)
+  }
+
+  @objc public func MessageSent(_ message: String) {
+    EventDispatcher.dispatchEvent(of: self, called: "MessageSent", arguments: message as AnyObject)
+  }
+
+  // MARK: BluetoothConnectionListener implementation
+
+  @objc public func onConnected(_ conn: BluetoothLE) {
+    conn.ExRegisterForStringValues(kMicrobitUartService, kMicrobitUartCharacteristic, false,
+        self.txCharacteristicHandler)
+  }
+
+  @objc public func onDisconnected(_ conn: BluetoothLE) {
+    conn.ExUnregisterForValues(kMicrobitUartService, kMicrobitUartCharacteristic,
+        self.txCharacteristicHandler)
+  }
+
+  // MARK: Private implementation
+
+  private func rxCharacteristicWriteHandler(_ service: String, _ characteristic: String,
+      _ values: [String]) {
+    guard !values.isEmpty else {
+      return
+    }
+    MessageReceived(values.first ?? "")
+  }
+
+  private func txCharacteristicHandler(_ service: String, _ characteristic: String,
+      _ values: [String]) {
+    guard !values.isEmpty else {
+      return
+    }
+    MessageSent(values.first ?? "")
+  }
+
+  private func reportNullConnection(_ method: String) {
+    self._form?.dispatchErrorOccurredEvent(self, method, ErrorMessage.ERROR_EXTENSION_ERROR,
+        1 as AnyObject, "Microbit_Uart_Simple", "BluetoothDevice is not set!")
+  }
+}
