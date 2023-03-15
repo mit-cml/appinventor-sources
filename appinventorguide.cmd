1ec114bcebdda11cfa8afa31f56e1d181f8cb2d6
Bran <123478828+anbran223@users.noreply.github.com>
2023-03-15 23:16:14 +0800

    Create appinventorguide.cmd

---
+++ appinventorguide.cmd
@@ -0,0 +1,6 @@
+echo Let's Create An App
+echo 1. Build One App
+echo 2. Build Two Apps
+
+echo Use MIT App Inventor it's A Free Web-Application that creates Apps using languages like Blocks
+echo Go to the Link "https://appinventor.mit.edu"
