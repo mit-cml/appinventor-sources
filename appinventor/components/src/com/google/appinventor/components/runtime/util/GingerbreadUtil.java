3966415a311faf5b9cb7697d77f02ef44a3ce00e
Diego Barreiro <diego.barreiro.perez@gmail.com>
2023-03-17 00:52:36 +0100

    [FIX] NFC Tag ID is only available on API 10 and higher

--- appinventor/components/src/com/google/appinventor/components/runtime/util/GingerbreadUtil.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/util/GingerbreadUtil.java
@@ -15,6 +15,7 @@ import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.Ndef;
 import android.nfc.tech.NdefFormatable;
+import android.os.Build;
 import android.os.Parcelable;
 import android.util.Log;
 
@@ -128,8 +129,15 @@ public class GingerbreadUtil {
         byte[] payload = msgs[0].getRecords()[0].getPayload();
         //the substring chops off the two language encoding bits at the beginning
         String message = new String(payload).substring(3);
-        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-        nfc.TagRead(StringUtils.toHexString(tag.getId()), message);
+
+        String tagId = "";
+        // TODO: After minSDK gets upgraded to 14, remove this check
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD_MR1) {
+          Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+          tagId = StringUtils.toHexString(tag.getId());
+        }
+
+        nfc.TagRead(tagId, message);
       } else {
         Tag detectedTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
         NdefMessage msg = null;
