14f4be2bc220bad636920be8740e86f83da8269b
SusanRatiLane <srlane@mit.edu>
2023-03-21 16:19:53 -0400

    Change checkbox margins for folder items to be more readable

--- appinventor/appengine/src/com/google/appinventor/client/explorer/youngandroid/ProjectListItem.java
+++ appinventor/appengine/src/com/google/appinventor/client/explorer/youngandroid/ProjectListItem.java
@@ -54,7 +54,7 @@ public class ProjectListItem extends Composite {
     dateCreatedLabel.setText(dateTimeFormat.format(dateCreated));
     this.project = project;
     this.depth = depth;
-    nameLabel.getElement().setAttribute("style", "margin-left: " + ((depth-1) * 40) + "px");
+    checkBox.getElement().setAttribute("style", "margin-left: " + ((depth-1) * 40) + "px");
   }
 
   public void setSelectionChangeHandler(ProjectSelectionChangeHandler changeHandler) {
