c7788516851ea3644455a35bf9ca2421943778e2
ReshuKumari <reshukumari786@gmail.com>
2023-05-25 21:30:38 +0530

    solving error for ant tests

--- appinventor/appengine/tests/com/google/appinventor/client/boxes/AdminUserListBoxTest.java
+++ appinventor/appengine/tests/com/google/appinventor/client/boxes/AdminUserListBoxTest.java
@@ -1,7 +1,8 @@
 package com.google.appinventor.client.boxes;
 
+import appengine.src.com.google.appinventor.client.AdminUserList;
 import junit.framework.TestCase;
 
 public class AdminUserListBoxTest extends TestCase {
-
+    
 }
