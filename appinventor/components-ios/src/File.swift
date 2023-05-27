b9b830a2596593ce5d48ddf08c886eb8772bf169
dhruvshrivastava18 <dhruvshrivastava18@gmail.com>
2023-05-27 12:30:53 +0530

    Update File.swift

--- appinventor/components-ios/src/File.swift
+++ appinventor/components-ios/src/File.swift
@@ -134,6 +134,132 @@ open class File: NonvisibleComponent {
     }
   }
   
+  // Copy the contents from the first file to the second file.
+  func copyFile(fromPath: String, toPath: String) -> Bool {
+      let fileManager = FileManager.default
+      
+      do {
+          // Check if source file exists
+          guard fileManager.fileExists(atPath: fromPath) else {
+              print("Source file does not exist")
+              return false
+          }
+
+          // Check if destination file already exists
+          guard !fileManager.fileExists(atPath: toPath) else {
+              print("Destination file already exists")
+              return false
+          }
+          
+          // Create intermediate directories if necessary
+          let toDirectory = URL(fileURLWithPath: toPath).deletingLastPathComponent().path
+          try fileManager.createDirectory(atPath: toDirectory, withIntermediateDirectories: true, attributes: nil)
+
+          // Copy the file
+          try fileManager.copyItem(atPath: fromPath, toPath: toPath)
+          
+          print("File copied successfully")
+          return true
+      } catch let error as NSError {
+          print("Error copying file: \(error.localizedDescription)")
+          return false
+      }
+  }
+  
+  // Move a file from one location to another.
+  func moveFile(fromPath: String, toPath: String) -> Bool {
+      let fileManager = FileManager.default
+      
+      do {
+          // Check if source file exists
+          guard fileManager.fileExists(atPath: fromPath) else {
+              print("Source file does not exist")
+              return false
+          }
+          
+          // Check if destination file already exists
+          guard !fileManager.fileExists(atPath: toPath) else {
+              print("Destination file already exists")
+              return false
+          }
+
+          // Create intermediate directories if necessary
+          let toDirectory = URL(fileURLWithPath: toPath).deletingLastPathComponent().path
+          try fileManager.createDirectory(atPath: toDirectory, withIntermediateDirectories: true, attributes: nil)
+
+          // Move the file
+          try fileManager.moveItem(atPath: fromPath, toPath: toPath)
+          
+          print("File moved successfully")
+          return true
+      } catch let error as NSError {
+          print("Error moving file: \(error.localizedDescription)")
+          return false
+      }
+  }
+  
+  //
+  func makeDirectory(directoryName: String, completion: @escaping (Bool) -> Void) {
+      let fileManager = FileManager.default
+      let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+      let directoryURL = documentsDirectory.appendingPathComponent(directoryName)
+      
+      if fileManager.fileExists(atPath: directoryURL.path) {
+          if fileManager.isDirectory(url: directoryURL) {
+              completion(true)
+          } else {
+              // cannot make a directory if there's a regular file there
+              completion(false)
+          }
+      } else {
+          do {
+              try fileManager.createDirectory(at: directoryURL, withIntermediateDirectories: true, attributes: nil)
+              completion(true)
+          } catch {
+              // cannot make directory, probably because an ancestor is read-only
+              completion(false)
+          }
+      }
+  }
+  
+  func removeDirectory(directoryName: String, completion: @escaping (Bool) -> Void) {
+      let fileManager = FileManager.default
+      let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+      let directoryURL = documentsDirectory.appendingPathComponent(directoryName)
+      
+      do {
+          try fileManager.removeItem(at: directoryURL)
+          completion(true)
+      } catch {
+          print("Error removing directory: \(error)")
+          completion(false)
+      }
+  }
+
+  func listDirectories(path: String) -> [String]? {
+      let fileManager = FileManager.default
+      let url = URL(fileURLWithPath: path)
+      
+      do {
+          let contents = try fileManager.contentsOfDirectory(at: url, includingPropertiesForKeys: nil, options: [.skipsHiddenFiles, .skipsSubdirectoryDescendants])
+          var dirs: [String] = []
+          
+          for item in contents {
+              var isDir: ObjCBool = false
+              if fileManager.fileExists(atPath: item.path, isDirectory: &isDir), isDir.boolValue {
+                  dirs.append(item.lastPathComponent)
+              }
+          }
+          
+          return dirs
+      } catch {
+          print("Error listing directories: \(error)")
+          return nil
+      }
+  }
+  
+  
+  
   private func normalizeNewLineChars(text: String) -> String {
     return text.replace(target: "\r\n", withString: "\n")
   }
@@ -147,3 +273,13 @@ open class File: NonvisibleComponent {
         arguments: fileName as NSString)
   }
 }
+
+
+extension FileManager {
+    func isDirectory(url: URL) -> Bool {
+        var isDirectory: ObjCBool = false
+        guard fileExists(atPath: url.path, isDirectory: &isDirectory) else { return false }
+        return isDirectory.boolValue
+    }
+}
+
