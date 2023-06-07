012ff2f27002f30151f73ed00feda01fe58769ff
Evan W. Patton <ewpatton@mit.edu>
2023-06-07 08:43:06 -0400

    Put GWT tests into a test suite
    Change-Id: I1b190f55a2b8ec78da7ff6169747346622100093

--- appinventor/appengine/tests/com/google/appinventor/client/OdeTestCase.java
+++ appinventor/appengine/tests/com/google/appinventor/client/OdeTestCase.java
@@ -1,8 +1,13 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2019-2023 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 package com.google.appinventor.client;
 
 import com.google.gwt.junit.client.GWTTestCase;
 
-public class OdeTest extends GWTTestCase {
+public class OdeTestCase extends GWTTestCase {
   public void testCompareLocales() {
     assertTrue("Handles default case one is null",
         Ode.compareLocales(null, "en", "en"));
---
+++ appinventor/appengine/tests/com/google/appinventor/client/settings/GeneralSettingsTestCase.java
@@ -0,0 +1,35 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2023 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+package com.google.appinventor.client.settings;
+
+import com.google.appinventor.client.settings.user.UserSettings;
+import com.google.appinventor.shared.rpc.user.User;
+import com.google.appinventor.shared.settings.SettingsConstants;
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class GeneralSettingsTestCase extends GWTTestCase {
+  @Override
+  public String getModuleName() {
+    return "com.google.appinventor.YaClient";
+  }
+
+  public void testUserDyslexicFontTrue() {
+    User user = new User("1","abc@email.com",true,true,null);
+    UserSettings userSettings = new UserSettings(user);
+
+    userSettings.getSettings(SettingsConstants.USER_GENERAL_SETTINGS).
+      changePropertyValue(SettingsConstants.USER_DYSLEXIC_FONT,
+                          "" + true);
+
+    String value = userSettings.getSettings(SettingsConstants.USER_GENERAL_SETTINGS).
+      getPropertyValue(SettingsConstants.USER_DYSLEXIC_FONT);
+    assertTrue(Boolean.parseBoolean(value));
+  }
+
+  public void testFailure() {
+    fail("This should fail");
+  }
+}
