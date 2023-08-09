273da54c4988a6656836ed933bd3bbe7fb7b8500
Kashish Sharma <kashish6011@gmail.com>
2023-08-09 12:46:52 +0530

    charts background feature initiated

---
+++ appinventor/components-ios/src/Chart.swift
@@ -0,0 +1,10 @@
+//
+//  Chart.swift
+//  AIComponentKit
+//
+//  Created by Kashish Sharma on 09/08/23.
+//  Copyright © 2023 Massachusetts Institute of Technology. All rights reserved.
+//
+
+import Foundation
+
