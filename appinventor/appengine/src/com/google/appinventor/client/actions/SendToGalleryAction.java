1b690c7689885492447c9683630feeb8bd3cecf5
Evan W. Patton <ewpatton@mit.edu>
2023-08-11 09:39:19 -0400

    Add missing copyright headers to actions package
    Change-Id: I8fa196565dceb90e7abcf09e10a14d7f935031f7

--- appinventor/appengine/src/com/google/appinventor/client/actions/SendToGalleryAction.java
+++ appinventor/appengine/src/com/google/appinventor/client/actions/SendToGalleryAction.java
@@ -1,5 +1,10 @@
-package com.google.appinventor.client.actions;
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2009-2011 Google, All Rights reserved
+// Copyright 2011-2023 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 
+package com.google.appinventor.client.actions;
 
 import com.google.appinventor.client.ErrorReporter;
 import com.google.appinventor.client.Ode;
@@ -13,7 +18,6 @@ import com.google.gwt.user.client.Window;
 import java.util.List;
 
 import static com.google.appinventor.client.Ode.MESSAGES;
-
 
 // Send to the New Gallery
 public class SendToGalleryAction implements Command {
