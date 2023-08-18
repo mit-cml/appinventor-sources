df288294d15d6db94a55d0ab0c963b8fe613e05c
Evan W. Patton <ewpatton@mit.edu>
2023-08-18 18:20:17 -0400

    Add more missing copyrights
    Change-Id: I8d501f5f95b18c578a3608cb57d2d5e56616fa4b

--- appinventor/appengine/src/com/google/appinventor/client/editor/youngandroid/actions/SwitchScreenAction.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/youngandroid/actions/SwitchScreenAction.java
@@ -1,7 +1,13 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2009-2011 Google, All Rights reserved
+// Copyright 2011-2023 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 package com.google.appinventor.client.editor.youngandroid.actions;
 
-import com.google.appinventor.client.editor.youngandroid.DesignToolbar;
 import com.google.appinventor.client.Ode;
+import com.google.appinventor.client.editor.youngandroid.DesignToolbar;
 import com.google.gwt.user.client.Command;
 
 public class SwitchScreenAction implements Command {
