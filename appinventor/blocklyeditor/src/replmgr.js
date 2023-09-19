6238a4b7664b955b2441abd4e2a128f1edbac9cc
Evan W. Patton <ewpatton@mit.edu>
2023-09-19 12:43:05 -0400

    Fix USB connections not loading extensions
    Change-Id: I80277f357eacb4d326fb28f67d63c987c5027265

--- appinventor/blocklyeditor/src/replmgr.js
+++ appinventor/blocklyeditor/src/replmgr.js
@@ -828,6 +828,7 @@ Blockly.ReplMgr.putYail = (function() {
             console.log("reseting companion");
             rs.state = Blockly.ReplMgr.rsState.IDLE;
             rs.connection = null;
+            rs.extensionurl = undefined;
             context.resetYail(false);
 //   hardreset is now done in the handler for the network error dialog OK
 //   button.
@@ -1385,6 +1386,7 @@ Blockly.ReplMgr.startRepl = function(already, chromebook, emulator, usb) {
             rs.rurl = 'http://127.0.0.1:8001/_values';
             rs.versionurl = 'http://127.0.0.1:8001/_getversion';
             rs.baseurl = 'http://127.0.0.1:8001/';
+            rs.extensionurl = rs.baseurl + '_extensions';
             rs.seq_count = 1;
             rs.count = 0;
             return;             // startAdbDevice callbacks will continue the connection process
@@ -1716,10 +1718,29 @@ Blockly.ReplMgr.loadExtensions = function() {
         rs.state = Blockly.ReplMgr.rsState.EXTENSIONS;
         var extensionJson = JSON.stringify(top.AssetManager_getExtensions());
         extensionJson = Blockly.Yail.quotifyForREPL(extensionJson);
-        var yailstring = "(AssetFetcher:loadExtensions "  +
-            extensionJson + ")";
+        var yailstring = "(AssetFetcher:loadExtensions " +
+          extensionJson + ")";
         console.log("Blockly.ReplMgr.loadExtensions: Yail = " + yailstring);
         this.putYail.putAsset(yailstring);
+    } else if (rs.extensionurl) {
+        rs.state = Blockly.ReplMgr.rsState.EXTENSIONS;
+        var xmlhttp = goog.net.XmlHttp();
+        var encoder = new goog.Uri.QueryData();
+        encoder.add('extensions', JSON.stringify(top.AssetManager_getExtensions()));
+        xmlhttp.open('POST', rs.extensionurl, true);
+        xmlhttp.onreadystatechange = function() {
+            /* Older companions do not support ReplMgr providing an extension list. This check below
+             * handles older companions as well as new companions so that we don't break old clients.
+             */
+            if (xmlhttp.readyState === 4 && (this.status === 200 || this.status === 404 || !this.status)) {
+                rs.state = Blockly.ReplMgr.rsState.CONNECTED;
+                Blockly.mainWorkspace.fireChangeListener(new AI.Events.CompanionConnect());
+            } else if (xmlhttp.readyState === 4) {
+                rs.state = Blockly.ReplMgr.rsState.IDLE;
+                top.BlocklyPanel_indicateDisconnect();
+            }
+        };
+        xmlhttp.send(encoder.toString());
     } else {
         rs.state = Blockly.ReplMgr.rsState.CONNECTED;
         Blockly.mainWorkspace.fireChangeListener(new AI.Events.CompanionConnect());
