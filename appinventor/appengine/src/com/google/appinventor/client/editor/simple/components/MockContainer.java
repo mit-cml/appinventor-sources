47310a0fcb13c162bf95c3b1733133d472c0cc96
Diego Barreiro Perez <diego.barreiro.perez@gmail.com>
2023-10-24 22:11:23 +0100

    Fix Tree Structure (#3038)

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockContainer.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockContainer.java
@@ -87,22 +87,27 @@ public abstract class MockContainer extends MockVisibleComponent implements Drop
     return layout;
   }
 
+  @Override
+  protected TreeItem buildTree() {
+    return this.buildTree(1);
+  }
+
   protected TreeItem buildTree(Integer view) {
     TreeItem itemNode = super.buildTree();
     //hide all containers except form if only nonvisible components are to be shown
     //in such a case, we need only the form's treeItem because all non-visible components are attached to it
-    if (view == 3 && !isForm()) {
-      itemNode.setVisible(false);
-    }
+    itemNode.setVisible(view != 3 || isForm());
 
     // Recursively build the tree for child components
     for (MockComponent child : children) {
       TreeItem childNode = child.buildTree();
+      boolean isVisible = true;
       if (view == 2 && child instanceof MockNonVisibleComponent) {
-        childNode.setVisible(false);
+        isVisible = false;
       } else if (view == 3 && child instanceof MockVisibleComponent) {
-        childNode.setVisible(false);
+        isVisible = false;
       }
+      childNode.setVisible(isVisible);
       itemNode.addItem(childNode);
     }
 
