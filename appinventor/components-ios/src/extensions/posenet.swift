99c5c5798be38c95369d1358b1c21e578e9a36d9
elliotchen02 <echen02@mit.edu>
2023-11-07 23:34:54 -0500

    add WKScriptMessageHandlers

--- appinventor/components-ios/src/extensions/posenet.swift
+++ appinventor/components-ios/src/extensions/posenet.swift
@@ -6,18 +6,21 @@
 import Foundation
 import WebKit
 
-open class PosenetExtension: NonvisibleComponent {
-  // fileprivate let LOG_TAG: String = PosenetExtension
+
+open class PosenetExtension: NonvisibleComponent, WKScriptMessageHandler {
   fileprivate final let _ERROR_WEBVIEWER_NOT_SET: String =
-  "You must specify a WebViewer using the WebViewer designer property before you can call %1s"
+  "You must specify a WebViewer using the WebViewer designer property before you can call"
   fileprivate final let _ERROR_JSON_PARSE_FAILED: Int = 101
   fileprivate final let _MODEL_URL: String =
   "https://storage.googleapis.com/tfjs-models/savedmodel/posenet/mobilenet/quant2/050/"
   fileprivate final let _BACK_CAMERA: String = "Back"
   fileprivate final let _FRONT_CAMERA: String = "Front"
   
-  fileprivate var _webview: WKWebView!
-  fileprivate var _keyPoints = Dictionary<String, [String]>()
+  fileprivate var _webview: WKWebView?  // Double check ! syntax
+  private var _webviewerApiSource: String = ""
+  private var _webviewerApi: WKUserScript? // double check ! syntax
+  
+  fileprivate var _keyPoints: [String: [String]] = [:]
   fileprivate var _minPoseConfidence: Double = 0.1
   fileprivate var _minPartConfidence: Double = 0.5
   fileprivate lazy var _cameraMode: String = _FRONT_CAMERA
@@ -25,28 +28,69 @@ open class PosenetExtension: NonvisibleComponent {
   fileprivate var _enabled: Bool = true
   fileprivate var _backgroundImage: String = ""
   
-  
-//  public override init(_ parent: ComponentContainer) {
-//    // TODO
-//  }
-  
-  
-  private func configureWebView(_ webview: WKWebView) -> Void {
-    let contentController = WKUserContentController()
-    let tfjsSource = getJavascript(path: "/assets/index", pathType: "html")
-    let tfjsScript = WKUserScript(
-      source: tfjsSource,
-      injectionTime: .atDocumentEnd,
-      forMainFrameOnly: true)
-    contentController.addUserScript(tfjsScript)
-    let webConfiguration = WKWebViewConfiguration()
-    webConfiguration.userContentController = contentController
-    // Create a new webview?
-    _webview = WKWebView(frame: CGRect.zero, configuration: webConfiguration)
-
+  private enum IllegalStateError: Error {
+    case webviewerNotSet
   }
   
+  
+  public override init(_ parent: ComponentContainer) {
+    super.init(parent)
+    _keyPoints["nose"] = []
+    _keyPoints["leftEye"] = []
+    _keyPoints["rightEye"] = []
+    _keyPoints["leftEar"] = []
+    _keyPoints["rightEar"] = []
+    _keyPoints["leftShoulder"] = []
+    _keyPoints["rightShoulder"] = []
+    _keyPoints["leftElbow"] = []
+    _keyPoints["rightElbow"] = []
+    _keyPoints["leftWrist"] = []
+    _keyPoints["rightWrist"] = []
+    _keyPoints["leftHip"] = []
+    _keyPoints["rightHip"] = []
+    _keyPoints["leftKnee"] = []
+    _keyPoints["rightKnee"] = []
+    _keyPoints["leftAnkle"] = []
+    _keyPoints["rightAnkle"] = []
+  }
+  
+  /**
+   Configure the current webview. Add a WKScriptMessageHandler
+   to handle the following Javascript calls necessary for
+   Posenet Extension:
+          ready()
+          reportImage(String dataURL)
+          reportResult(String result)
+          error(int errorCode, String errorMessage)
+   Equivalent to the @Javascript interface in the Android Posenet Extension.
+   */
+  private func configureWebView(_ webview: WKWebView) {
+    self._webview = webview
+    let config = webview.configuration
+    // config.defaultWebpagePreferences.allowsContentJavaScript = true
+    let controller = config.userContentController
+    controller.add(self, name: "ready")
+    controller.add(self, name: "reportImage")
+    controller.add(self, name: "reportResult")
+    controller.add(self, name: "error")
+  }
+  
+  
   // MARK: PoseNet Properties
+  
+  
+//  @objc open var WebViewer: WebViewer {
+//    get {
+//      return _webviewer
+//    }
+//    set {
+//      if 
+//    }
+//  }
+//  
+  
+  
+  
   
   @objc open var MinPoseConfidence: Double {
     get {
@@ -55,7 +99,11 @@ open class PosenetExtension: NonvisibleComponent {
     set {
       _minPoseConfidence = newValue
       if _initialized {
-        // TODO
+        do {
+          try assertWebView("MinPoseConfidence")
+        } catch {
+          print(_ERROR_WEBVIEWER_NOT_SET, "MinPoseConfidence")
+        }
       }
     }
   }
@@ -67,7 +115,11 @@ open class PosenetExtension: NonvisibleComponent {
     set {
       _minPartConfidence = newValue
       if _initialized {
-        // TODO
+        do {
+          try assertWebView("MinPartConfidence")
+        } catch {
+          print(_ERROR_WEBVIEWER_NOT_SET, "MinPartConfidence")
+        }
       }
     }
   }
@@ -80,6 +132,7 @@ open class PosenetExtension: NonvisibleComponent {
           keyPoints.append(point)
         }
       }
+      // TODO: build yaillist?
       // HELP: what is YailList.makelist returning?
       // returning flat array?
       return keyPoints.flatMap { $0 }
@@ -100,13 +153,17 @@ open class PosenetExtension: NonvisibleComponent {
     let rKnee = _keyPoints["rightKnee"]
     let rAnkle = _keyPoints["rightAnkle"]
     
-    // TODO
+    // TODO: generate correct return value
+    return []
   }
   
+  // TODO: add all body part properties and find YailList representation
+  
+  
+  
+  
   @objc open var BackgroundImage: String {
-    get {
       return _backgroundImage
-    }
   }
   
   @objc open var Enabled: Bool {
@@ -116,55 +173,83 @@ open class PosenetExtension: NonvisibleComponent {
     set {
       _enabled = newValue
       if _initialized {
-        // WEBVIEW TODO
+        do {
+          try assertWebView("Enabled")
+        } catch {
+          print(_ERROR_WEBVIEWER_NOT_SET, "Enabled")
+        }
       }
     }
   }
   
-  // MARK: PoseNet Methods
-  
-  func getJavascript(path: String, pathType: String) -> String {
-     if let filepath = Bundle.main.path(forResource: path, ofType: pathType) {
-         do {
-             return try String(contentsOfFile: filepath)
-         } catch {
-             return ""
-         }
-     } else {
-        return ""
-     }
+  @objc open var UseCamera: String {
+    get {
+      return _cameraMode
+    }
+    set {
+      if newValue == _BACK_CAMERA || newValue == _FRONT_CAMERA {
+        _cameraMode = newValue
+        if _initialized {
+          do {
+            let frontFacing = (newValue == _FRONT_CAMERA)
+            try assertWebView("UseCamera", frontFacing)
+          } catch {
+            print(_ERROR_WEBVIEWER_NOT_SET, "UseCamera")
+          }
+        }
+      } else {
+          // TODO: figure out what form is
+      }
+    }
   }
   
-  @objc open func Initialize() -> Void {
-    if let unwrapped = _webview {
+  
+  // MARK: PoseNet Methods
+  
+  
+  open func userContentController(
+    _ userContentController: WKUserContentController,
+    didReceive message: WKScriptMessage
+  ) {
+    if message.name == "ready" {
+      if _enabled {
+        MinPartConfidence = _minPartConfidence
+        MinPoseConfidence = _minPoseConfidence
+        UseCamera = _cameraMode
+      }
+    }
+    if message.name == "reportImage" {
+      // TODO: Finish out the javascript interface here
+    }
+  }
+  
+  @objc open func initialize() {
+    if _webview != nil {
       _initialized = true
     }
   }
   
-  // NO Mutability?
-//  @objc open func AddIfValid(
-//    _ point1: [String],
-//    _ point2: [String],
-//    _ skeleton: [[String]]
-//  ) {
-//    if point1.count == 2 && point2.count == 2 {
-//      var newPoint = [point1, point2].flatMap { $0 }
-//      skeleton.append(newPoint)
-//    }
-//  }
+  private func assertWebView(_ method: String, _ frontFacing: Bool = true) throws {
+    guard let _webview = _webview else {
+      throw IllegalStateError.webviewerNotSet
+    }
+    switch method {
+    case "Enabled":
+      _webview.evaluateJavaScript(_enabled ? "startVideo();" : "stopVideo();")
+    case "MinPoseConfidence":
+      _webview.evaluateJavaScript("minPoseConfidence = \(_minPoseConfidence) ;")
+    case "MinPartConfidence":
+      _webview.evaluateJavaScript("minPartConfidence = \(_minPartConfidence) ;")
+    case "UseCamera":
+      _webview.evaluateJavaScript("setCameraFacingMode( \(frontFacing) );")
+    default:
+      print("Error: Not a valid method")
+    }
+  }
   
   
   
-//  fileprivate class AppinventorTFJS {
-//    
-//    open func ready() -> Void {
-//      
-//    }
-//  }
-//  
-//  
-//  
+  
   
   
 }
-
