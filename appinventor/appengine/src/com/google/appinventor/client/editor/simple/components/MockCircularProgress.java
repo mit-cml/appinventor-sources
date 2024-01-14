2a1b4fedf9319cbce0ff1bc370f33e88dafd4ea7
Diego Barreiro Perez <diego.barreiro.perez@gmail.com>
2024-01-14 22:43:57 +0000

    Add LICENSE header

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockCircularProgress.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockCircularProgress.java
@@ -1,6 +1,11 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2009-2011 Google, All Rights reserved
+// Copyright 2011-2012 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 package com.google.appinventor.client.editor.simple.components;
 
-import static com.google.appinventor.client.Ode.MESSAGES;
 import com.google.appinventor.client.editor.simple.SimpleEditor;
 import com.google.gwt.user.client.ui.InlineHTML;
 
@@ -8,34 +13,34 @@ import com.google.gwt.user.client.ui.InlineHTML;
 public final class MockCircularProgress extends MockVisibleComponent {
 
 
-    public static final String TYPE = "CircularProgress";
-    private static final String PROPERTY_NAME_COLOR = "Color";
-    private InlineHTML progressBarWidget;
+  public static final String TYPE = "CircularProgress";
+  private static final String PROPERTY_NAME_COLOR = "Color";
+  private InlineHTML progressBarWidget;
 
-    public MockCircularProgress(SimpleEditor editor) {
-        super(editor, TYPE, images.circularProgress());
+  public MockCircularProgress(SimpleEditor editor) {
+    super(editor, TYPE, images.circularProgress());
 
-        progressBarWidget = new InlineHTML();
-        progressBarWidget.setStylePrimaryName("ode-SimpleMockComponent");
-        progressBarWidget.setText("\u25ef");
-        MockComponentsUtil.setWidgetFontSize(progressBarWidget, "35");
-        MockComponentsUtil.setWidgetTextAlign(progressBarWidget, "1");
-        MockComponentsUtil.setWidgetFontBold(progressBarWidget, "bold");
-        initComponent(progressBarWidget);
+    progressBarWidget = new InlineHTML();
+    progressBarWidget.setStylePrimaryName("ode-SimpleMockComponent");
+    progressBarWidget.setText("\u25ef");
+    MockComponentsUtil.setWidgetFontSize(progressBarWidget, "35");
+    MockComponentsUtil.setWidgetTextAlign(progressBarWidget, "1");
+    MockComponentsUtil.setWidgetFontBold(progressBarWidget, "bold");
+    initComponent(progressBarWidget);
+  }
+
+  private void setIndeterminateColorProperty(String text) {
+    if (MockComponentsUtil.isDefaultColor(text)) {
+      text = "&HFFFFFFFF";  //white
     }
+    MockComponentsUtil.setWidgetTextColor(progressBarWidget, text);
+  }
 
-    private void setIndeterminateColorProperty(String text) {
-        if (MockComponentsUtil.isDefaultColor(text)) {
-            text = "&HFFFFFFFF";  //white
-        }
-        MockComponentsUtil.setWidgetTextColor(progressBarWidget, text);
+  @Override
+  public void onPropertyChange(String propertyName, String newValue) {
+    super.onPropertyChange(propertyName, newValue);
+    if (propertyName.equals(PROPERTY_NAME_COLOR)) {
+      setIndeterminateColorProperty(newValue);
     }
-
-    @Override
-    public void onPropertyChange(String propertyName, String newValue) {
-        super.onPropertyChange(propertyName, newValue);
-        if (propertyName.equals(PROPERTY_NAME_COLOR)) {
-            setIndeterminateColorProperty(newValue);
-        }
-    }
+  }
 }
--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockLinearProgress.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockLinearProgress.java
@@ -1,6 +1,11 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2009-2011 Google, All Rights reserved
+// Copyright 2011-2012 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 package com.google.appinventor.client.editor.simple.components;
 
-import static com.google.appinventor.client.Ode.MESSAGES;
 import com.google.appinventor.client.editor.simple.SimpleEditor;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
