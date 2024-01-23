01ad3d7a254c9c7478ffaf006b750c5feda14827
Mark Friedman <mark-friedman@users.noreply.github.com>
2024-01-23 15:58:09 -0800

    Add Lexical Variable mixin
    This is a general mixin which can be used by any block in order to satisfy the requirements of the Lexical Variable plugin.

--- appinventor/blocklyeditor/src/mixins/mixins.js
+++ appinventor/blocklyeditor/src/mixins/mixins.js
@@ -1,6 +1,7 @@
 goog.provide('AI.Blockly.Mixins');
 
 goog.require('AI.Blockly.Mixins.DynamicConnections');
+goog.require('AI.Blockly.Mixins.LexicalVariableMethods');
 
 AI.Blockly.Mixins.extend = function (target, source) {
   for (var prop in source) {
