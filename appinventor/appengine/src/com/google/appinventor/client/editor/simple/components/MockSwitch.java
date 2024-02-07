8459666d987450cd1e6fba483857f000ddbf9024
Gordon Lu <gordonlu0749@gmail.com>
2024-02-07 14:58:09 +0800

    Updated copyright years

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockSwitch.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockSwitch.java
@@ -1,5 +1,5 @@
 // -*- mode: java; c-basic-offset: 2; -*-
-// Copyright 2018-2020 MIT, All rights reserved
+// Copyright 2018-2024 MIT, All rights reserved
 // Released under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 
