a692f348b357889a2811f28e56e0c2fb2f57923d
Linda Xue <lexue28@gmail.com>
2024-02-08 00:46:54 -0500

    added a few functions

--- appinventor/components-ios/src/ChartData2D.swift
+++ appinventor/components-ios/src/ChartData2D.swift
@@ -12,13 +12,37 @@ import Foundation
     sheetColumns = [" ", " "]
     webColumns = [" ", " "]
   }
+  
   @objc func AddEntry(_ x: String, _ y: String){
     // create a 2-tuple, and add the tuple to the Data series
     DispatchQueue.main.async {
-      var pair: YailList<AnyObject> = [x, y]
+      let pair: YailList<AnyObject> = [x, y]
       self._chartDataModel?.addEntryFromTuple(pair)
       // refresh chart with new data
       self.refreshChart()
     }
   }
+  
+  @objc func RemoveEntry(_ x: String, _ y: String){
+    // create a 2-tuple, and remove the tuple from the Data series
+    DispatchQueue.main.async {
+      let pair: YailList<AnyObject> = [x, y]
+      self._chartDataModel?.removeEntryFromTuple(pair)
+      // refresh chart with new data
+      self.refreshChart()
+    }
+  }
+  
+  // checks whether an (x, y) entry exists in the Coordinate Data. Returns true if the Entry exists, and false otherwise.
+  // TODO: how do i return something in DispatchQueue main
+  @objc func DoesEntryExist(_ x: String, _ y: String) -> Bool{
+    DispatchQueue.main.async {
+      func call() -> Bool {
+        var pair: YailList<AnyObject> = [x, y]
+        return self._chartDataModel!.doesEntryExist(pair) // TODO: is the ! okay?
+      }
+    }
+    return false
+  }
+  
 }
