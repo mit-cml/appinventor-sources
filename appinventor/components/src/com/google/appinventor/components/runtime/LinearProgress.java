ed581a9679637856d4fb3489c927a1a3f804797a
Diego Barreiro Perez <diego.barreiro.perez@gmail.com>
2024-02-11 18:49:47 +0000

    Updated Copyright notice

--- appinventor/components/src/com/google/appinventor/components/runtime/LinearProgress.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/LinearProgress.java
@@ -1,6 +1,6 @@
 // -*- mode: java; c-basic-offset: 2; -*-
 // Copyright 2017-2023 Kodular, All rights reserved
-// Copyright 2023 MIT, All rights reserved
+// Copyright 2023-2024 MIT, All rights reserved
 // Released under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 
