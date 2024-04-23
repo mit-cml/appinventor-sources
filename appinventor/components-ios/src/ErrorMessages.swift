9e03c73c2ef11825be1762d021f8ab46bcd5109a
Evan W. Patton <ewpatton@mit.edu>
2024-04-23 08:28:01 -0400

    Add proper errors for unsupported iOS texting features
    Change-Id: Ia6a6be16ee0908646615b9e0fc95f260316329a2

--- appinventor/components-ios/src/ErrorMessages.swift
+++ appinventor/components-ios/src/ErrorMessages.swift
@@ -149,7 +149,11 @@ import Foundation
 
   // iOS Specific OrientationSensor Errors
   case ERROR_IOS_ORIENTATION_SENSOR_DATA_ERROR = 100300
-  
+
+  // iOS Specific Texting Errors
+  case ERROR_IOS_GOOGLEVOICE_NOT_SUPPORTED = 101701
+  case ERROR_IOS_RECEIVING_NOT_SUPPORTED = 101702
+
   var code: Int32 {
     return Int32(self.rawValue)
   }
@@ -377,6 +381,11 @@ import Foundation
 
     case .ERROR_IOS_ORIENTATION_SENSOR_DATA_ERROR:
       return "The orientation sensor is no longer recording data due to the following error %s"
+
+    case .ERROR_IOS_GOOGLEVOICE_NOT_SUPPORTED:
+      return "Google Voice integration is unavailable."
+    case .ERROR_IOS_RECEIVING_NOT_SUPPORTED:
+      return "Receiving text messages is unavailable."
     }
   }
 }
