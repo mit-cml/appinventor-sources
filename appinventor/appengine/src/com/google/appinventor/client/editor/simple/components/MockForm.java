3aa6ca62dc3546acac741406c30ee31acef81167
SusanRatiLane <srlane@mit.edu>
2024-05-15 13:37:05 -0400

    Fix scrollbar and alignment for MockForm in Windows

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockForm.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockForm.java
@@ -555,6 +555,11 @@ public final class MockForm extends MockContainer {
   private void resizePanel(int newWidth, int newHeight){
     screenWidth = newWidth;
     screenHeight = newHeight;
+    int scrollbarWidth = 0;
+    if (Boolean.parseBoolean(getPropertyValue(PROPERTY_NAME_SCROLLABLE))) {
+      // only display space for a scrollbar if the form is scrollable
+      scrollbarWidth = getVerticalScrollbarWidth();
+    }
 
     if (landscape) {
       String val = editor.getProjectEditor().getProjectSettingsProperty(
@@ -571,11 +576,13 @@ public final class MockForm extends MockContainer {
       usableScreenHeight = screenHeight - phoneBar.getHeight() - titleBar.getHeight() - navigationBar.getHeight();
     }
     rootPanel.setPixelSize(usableScreenWidth, usableScreenHeight);
-    scrollPanel.setPixelSize(usableScreenWidth + getVerticalScrollbarWidth(), usableScreenHeight);
-    formWidget.setPixelSize(screenWidth + getVerticalScrollbarWidth(), screenHeight);
-    // TODO: This is a hack to fix the phone portrait mockup stretching when phone widget panel
-    // is forced larger than 320px.
-    phoneWidget.setWidth(screenWidth + "px");
+    // This margin is to ensure the mockform aligns to the left when there is space for a scrollbar
+    rootPanel.getElement().getStyle().setProperty("margin-right", scrollbarWidth + "px");
+    scrollPanel.setPixelSize(usableScreenWidth + scrollbarWidth, usableScreenHeight);
+    formWidget.setPixelSize(screenWidth + scrollbarWidth, screenHeight);
+    // Added width to phoneWidget to prevent it from expanding wider than intended
+    // if the SimpleComponentsPanel menu is wider than the phonebar.
+    phoneWidget.setWidth(usableScreenWidth + scrollbarWidth + "px");
     // Store properties
     changeProperty(PROPERTY_NAME_WIDTH, "" + usableScreenWidth);
     boolean scrollable = Boolean.parseBoolean(getPropertyValue(PROPERTY_NAME_SCROLLABLE));
@@ -642,6 +649,7 @@ public final class MockForm extends MockContainer {
     }
     changePreviewFlag = false;
   }
+
   /*
    * Returns the width of a vertical scroll bar, calculating it if necessary.
    */
@@ -859,6 +867,7 @@ public final class MockForm extends MockContainer {
       int heightHint = scrollable ? LENGTH_PREFERRED : usableScreenHeight;
       changeProperty(PROPERTY_NAME_HEIGHT, "" + heightHint);
     }
+    resizePanel(screenWidth, screenHeight);
   }
 
   private void setIconProperty(String icon) {
