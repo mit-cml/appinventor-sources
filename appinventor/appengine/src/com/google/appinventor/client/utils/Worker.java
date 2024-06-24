3bbdf27d859c3dcc5aa1cb3ef7a66662b094ac28
Shreyash Saitwal <40118018+shreyashsaitwal@users.noreply.github.com>
2024-06-24 19:45:36 +0530

    chore(poc): enhance `Worker`'s API

---
+++ appinventor/appengine/src/com/google/appinventor/client/utils/Worker.java
@@ -0,0 +1,34 @@
+package com.google.appinventor.client.utils;
+
+import jsinterop.annotations.JsFunction;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+
+@JsType(isNative = true, namespace = JsPackage.GLOBAL)
+public class Worker {
+
+  @JsFunction
+  public interface EventListener<T extends Event> {
+    void handleEvent(T event);
+  }
+
+  @JsType(isNative = true, namespace = JsPackage.GLOBAL)
+  public interface Event {}
+
+  @JsType(isNative = true, namespace = JsPackage.GLOBAL)
+  public interface MessageEvent extends Event {
+    @JsProperty(name = "data")
+    Object getData();
+  }
+
+  public Worker(String scriptURL) {}
+
+  public native void postMessage(Object message);
+
+  public native void terminate();
+
+  public native <T extends Event> void addEventListener(String type, EventListener<T> listener);
+
+  public native <T extends Event> void removeEventListener(String type, EventListener<T> listener);
+}
