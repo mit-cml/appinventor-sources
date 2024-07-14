4e2a97976d1a20188f9ae4857f71f4d693cff1c3
Venkata Sai <venkata1.mitmpl2022@learner.manipal.edu>
2024-07-14 20:30:07 +0530

    neo

--- appinventor/appengine/src/com/google/appinventor/client/style/neo/ProjectFolderNeo.java
+++ appinventor/appengine/src/com/google/appinventor/client/style/neo/ProjectFolderNeo.java
@@ -16,7 +16,6 @@ import com.google.gwt.event.dom.client.KeyDownEvent;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.FocusPanel;
@@ -71,13 +70,13 @@ public class ProjectFolderNeo extends ProjectFolder {
     super.toggleFolderSelection(e);
   }
 
-  @UiHandler("expandbuttonFocusPanel")
+  @SuppressWarnings("unused")
   @Override
   protected void toggleExpandedState(KeyDownEvent e) {
     super.toggleExpandedState(e);
   }
 
-  @UiHandler("expandbuttonFocusPanel")
+  @SuppressWarnings("unused")
   @Override
   protected void toggleExpandedState(ClickEvent e) {
     super.toggleExpandedState(e);
