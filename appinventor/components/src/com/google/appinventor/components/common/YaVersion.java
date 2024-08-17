62cef9ac0b92befa024a8de1700c993fd83ba20e
Jeffrey I. Schiller <jis@mit.edu>
2024-08-16 20:24:28 -0400

    Setup Companion Version 2.72 (for nb198)
    Change-Id: I9a42151bbea04da723ed0a2a39628f4a83561bb3

--- appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
+++ appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
@@ -1679,12 +1679,12 @@ public class YaVersion {
 
   public static final String ACCEPTABLE_COMPANION_PACKAGE = "edu.mit.appinventor.aicompanion3";
 
-  public static final String PREFERRED_COMPANION = "2.71";
+  public static final String PREFERRED_COMPANION = "2.72";
   public static final String EMULATOR_UPDATE_URL = ""; // Should be an APK
   public static final String COMPANION_UPDATE_URL = "";
   public static final String COMPANION_UPDATE_URL1 = "";
   public static final String COMPANION_UPDATE_EMULATOR_URL = "";
-  public static final String [] ACCEPTABLE_COMPANIONS = { "2.71", "2.71u" };
+  public static final String [] ACCEPTABLE_COMPANIONS = { "2.72", "2.72u" };
 
   // Splash Screen Values
   public static final int SPLASH_SURVEY = 1;
