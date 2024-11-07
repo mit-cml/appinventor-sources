0350ea5c0c8aeec1a93c909bbbd112c3d3f5d63a
YashSachdeva <yashsachdeva314@gmail.com>
2024-11-08 01:07:11 +0530

    Update CheckBoxList.java

--- appinventor/aimerger_for_appinv_classic/src/edu/mills/feeney/thesis/aimerger/CheckBoxList.java
+++ appinventor/aimerger_for_appinv_classic/src/edu/mills/feeney/thesis/aimerger/CheckBoxList.java
@@ -7,47 +7,48 @@ import java.awt.event.*;
 import java.util.LinkedList;
 
 /**
- * Displays the list of checkboxes. 
+ * Displays a list of checkboxes.
  * 
- * Informed by code at www.devx.com/tips/Tip/5342 by Trevor Harmon, Febuary 10, 1999.
+ * Informed by code at www.devx.com/tips/Tip/5342 by Trevor Harmon, February 10, 1999.
  * 
- * @author feeney.kate@gmail.com (Kate Feeney)
- *
+ * @author feeney.kate
  */
-public class CheckBoxList extends JList {
-  private LinkedList<String> checked = new LinkedList<String>();
-  protected static Border noFocusBorder = new EmptyBorder(1, 1, 1, 1);
+public class CheckBoxList extends JList<JCheckBox> {
+  private static final String UNSELECTABLE_SCREEN = "Screen1";
+  private final LinkedList<String> checked = new LinkedList<>();
+  protected static final Border noFocusBorder = new EmptyBorder(1, 1, 1, 1);
 
   public CheckBoxList() {
     setCellRenderer(new CellRenderer());
-    // A mouse listener for when the mouse clicks a checkbox
+    setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+
     addMouseListener(new MouseAdapter() {
+      @Override
       public void mousePressed(MouseEvent e) {
-        // Get index of box clicked.
         int index = locationToIndex(e.getPoint());
         if (index != -1) {
-          JCheckBox checkbox = (JCheckBox) getModel().getElementAt(index);
-          // For all checkboxes other than Screen1 toggle. Screen1 is not selectable since
-          // Screen1 from the main project is automatically merged.
-          if (!checkbox.getText().equals("Screen1")) {
+          JCheckBox checkbox = getModel().getElementAt(index);
+          String text = checkbox.getText();
+
+          if (!text.equals(UNSELECTABLE_SCREEN)) {
             checkbox.setSelected(!checkbox.isSelected());
-            if (checkbox.isEnabled() && !checked.contains(checkbox.getText())) {
-              checked.add(checkbox.getText());
-            } else if (checked.contains(checkbox.getText())) {
-              checked.remove(checkbox.getText());
+
+            if (checkbox.isSelected() && !checked.contains(text)) {
+              checked.add(text);
+            } else if (!checkbox.isSelected()) {
+              checked.remove(text);
             }
+            repaint();
           }
-          repaint();
         }
       }
     });
-    setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
   }
 
   public LinkedList<String> getChecked() {
     return checked;
   }
-  
+
   /**
    * Clears the list of checked items.
    */
@@ -55,22 +56,23 @@ public class CheckBoxList extends JList {
     checked.clear();
   }
 
-  protected class CellRenderer implements ListCellRenderer {
-    public Component getListCellRendererComponent(JList list, Object value, int index,
-                                                  boolean isSelected, boolean cellHasFocus) {
-      JCheckBox checkbox = (JCheckBox) value;
-      checkbox.setBackground(isSelected ? getSelectionBackground() : getBackground());
-      checkbox.setForeground(isSelected ? getSelectionForeground() : getForeground());
-      checkbox.setEnabled(isEnabled());
-      checkbox.setFont(getFont());
-      checkbox.setFocusPainted(false);
-      checkbox.setBorderPainted(true);
-      checkbox.setBorder(isSelected ? UIManager.getBorder("List.focusCellHighlightBorder")
-          : noFocusBorder);
-      if (checkbox.getText().equals("Screen1")) {
-        checkbox.setForeground(Color.gray);
+  private class CellRenderer implements ListCellRenderer<JCheckBox> {
+    @Override
+    public Component getListCellRendererComponent(JList<? extends JCheckBox> list, JCheckBox value,
+                                                  int index, boolean isSelected, boolean cellHasFocus) {
+      value.setBackground(isSelected ? getSelectionBackground() : getBackground());
+      value.setForeground(isSelected ? getSelectionForeground() : getForeground());
+      value.setEnabled(isEnabled());
+      value.setFont(getFont());
+      value.setFocusPainted(false);
+      value.setBorderPainted(true);
+      value.setBorder(isSelected ? UIManager.getBorder("List.focusCellHighlightBorder") : noFocusBorder);
+
+      if (value.getText().equals(UNSELECTABLE_SCREEN)) {
+        value.setForeground(Color.GRAY);
       }
-      return checkbox;
+      
+      return value;
     }
   }
 }
