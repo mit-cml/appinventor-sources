a9590b2b543cf4e5e88ab826c184001998e519cc
Evan W. Patton <ewpatton@mit.edu>
2024-11-20 18:58:20 -0500

    Add DGCharts dependency for iOS
    Change-Id: I1695ee4786ceba4626172897be73449a7e10c72c

---
+++ appinventor/Pods/DGCharts/Source/Charts/Data/Implementations/Standard/LineChartData.swift
@@ -0,0 +1,31 @@
+//
+//  LineChartData.swift
+//  Charts
+//
+//  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
+//  A port of MPAndroidChart for iOS
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+
+/// Data object that encapsulates all data associated with a LineChart.
+open class LineChartData: ChartData
+{
+    public required init()
+    {
+        super.init()
+    }
+    
+    public override init(dataSets: [ChartDataSetProtocol])
+    {
+        super.init(dataSets: dataSets)
+    }
+
+    public required init(arrayLiteral elements: ChartDataSetProtocol...)
+    {
+        super.init(dataSets: elements)
+    }
+}
