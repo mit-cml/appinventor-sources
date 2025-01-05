5a846d45aed6c5bd1c51e506529f11104c7ab7a4
Evan W. Patton <ewpatton@mit.edu>
2025-01-05 11:53:50 -0500

    Add missing copyright in mixins.js
    Change-Id: I1a045a222f3ab389edbf7e36fedafff176548cd1

--- appinventor/blocklyeditor/src/mixins/mixins.js
+++ appinventor/blocklyeditor/src/mixins/mixins.js
@@ -1,3 +1,8 @@
+// -*- mode: javascript; js-indent-level: 2; -*-
+// Copyright 2020 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 goog.provide('AI.Blockly.Mixins');
 
 goog.require('AI.Blockly.Mixins.DynamicConnections');
