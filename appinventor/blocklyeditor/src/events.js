25af8a1b581893597b70ff4a7ab86c82643b1747
Evan W. Patton <ewpatton@mit.edu>
2025-01-05 23:03:45 -0500

    Fix copyright header in events.js
    Change-Id: I3e792c018250c0c73262de8f4a472625b31b7c7b

--- appinventor/blocklyeditor/src/events.js
+++ appinventor/blocklyeditor/src/events.js
@@ -1,3 +1,7 @@
+// -*- mode: javascript; js-indent-level: 2; -*-
+// Copyright © 2016-2025 Massachusetts Institute of Technology. All rights reserved.
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 // noinspection ES6ConvertVarToLetConst
 
 'use strict';
