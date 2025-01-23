55c8216407f87720eae746632fdf740702e0e8cd
Evan W. Patton <ewpatton@mit.edu>
2025-01-23 14:56:12 -0500

    Fix block workspace not saving after component deletion
    Change-Id: Ifafabe97ae1ab0258a5a4c187f2af6ca2245e90f

--- appinventor/blocklyeditor/src/blocklyeditor.js
+++ appinventor/blocklyeditor/src/blocklyeditor.js
@@ -1099,6 +1099,12 @@ Blockly.BlocklyEditor['create'] = function(container, formName, readOnly, rtl) {
   workspace.addChangeListener(function(e) {
     if (e.type == Blockly.Events.BLOCK_MOVE && e.newParentId !== e.oldParentId) {
       const block = workspace.getBlockById(e.blockId);
+      if (!block) {
+        // This seems to be the case when the block has been deleted since it is first moved from
+        // its parent then removed from the workspace, but both events will be run back to back
+        // after the deletion has already happened.
+        return;
+      }
       block.getDescendants().forEach(function(block) {
         if (block.type === 'lexical_variable_get' || block.type === 'lexical_variable_set') {
           // If the block is a lexical variable, then we need to rebuild the options for the field
