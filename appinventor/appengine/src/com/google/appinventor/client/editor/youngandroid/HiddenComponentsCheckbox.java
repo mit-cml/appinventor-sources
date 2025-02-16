1d31e225ccb093399629e929c181c23912706dbb
divyanshu_Kumar <154233802+d1vyanshu-kumar@users.noreply.github.com>
2025-02-17 03:09:45 +0530

    need help

--- appinventor/appengine/src/com/google/appinventor/client/editor/youngandroid/HiddenComponentsCheckbox.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/youngandroid/HiddenComponentsCheckbox.java
@@ -5,62 +5,101 @@
 
 package com.google.appinventor.client.editor.youngandroid;
 
-import com.google.appinventor.client.ErrorReporter;
 import com.google.appinventor.client.Ode;
+import com.google.appinventor.client.editor.simple.components.MockComponent;
 import com.google.appinventor.client.editor.simple.components.MockForm;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.CheckBox;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 public class HiddenComponentsCheckbox extends CheckBox {
   private static final Logger LOG = Logger.getLogger(HiddenComponentsCheckbox.class.getName());
 
   private static HiddenComponentsCheckbox INSTANCE;
-  private MockForm form;
+  private static final Map<String, MockForm> formInstances = new HashMap<>();
+  private static final Map<String, Boolean> screenCheckboxState = new HashMap<>();
+  private static String currentFormTitle;
 
-  // This is an attempt to create a Singleton class that does not have to be referenced
-  // by Ode and thus can be placed anywhere in a UIBinder layout.
   private HiddenComponentsCheckbox() {
-    if (INSTANCE != null) {
-      LOG.warning("Attempted to create second instance of HiddenComponentsCheckbox singleton");
-      ErrorReporter.reportError("Attempted to create second instance of HiddenComponentsCheckbox singleton");
-    } else {
-      addClickHandler(new ClickHandler() {
-        @Override
-        public void onClick(ClickEvent event) {
-          toggleHiddenComponents(event);
-        }
-      });
-      INSTANCE = this;
-    }
-  }
-
-  public static void checkInstance() {
-    if (INSTANCE == null) {
-      INSTANCE = new HiddenComponentsCheckbox();
-    }
+    addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        toggleHiddenComponents();
+      }
+    });
   }
 
   public static void show(MockForm form) {
+    if (form == null) {
+      LOG.severe("❌ Attempted to show HiddenComponentsCheckbox with a null form.");
+      return;
+    }
+
     checkInstance();
-    INSTANCE.form = form;
-    INSTANCE.setValue(Ode.getCurrentProjectEditor().getScreenCheckboxState(form.getTitle()));
+    String formTitle = form.getTitle();
+    currentFormTitle = formTitle;
+
+    formInstances.put(formTitle, form);
+    screenCheckboxState.putIfAbsent(formTitle, Ode.getCurrentProjectEditor().getScreenCheckboxState(formTitle));
+
+    LOG.info("📌 [show] HiddenComponentsCheckbox for screen: " + formTitle + " | State: " + screenCheckboxState.get(formTitle));
+
+    INSTANCE.setValue(screenCheckboxState.get(formTitle));
   }
 
+  public static boolean getCheckboxStateForScreen(String formTitle) {
+    checkInstance();
+    boolean state = screenCheckboxState.getOrDefault(formTitle, false);
+    LOG.info("📌 [getCheckboxStateForScreen] Screen: " + formTitle + " | State: " + state);
+    return state;
+  }
+
+  public static void toggleHiddenComponents() {
+    checkInstance();
+    LOG.info("✅ [toggleHiddenComponents] Checkbox clicked!");
+
+    if (currentFormTitle == null) {
+      LOG.warning("❌ No current form title set.");
+      return;
+    }
+
+    MockForm currentForm = formInstances.get(currentFormTitle);
+    if (currentForm == null) {
+      LOG.warning("❌ Form not found for screen: " + currentFormTitle);
+      return;
+    }
+
+    boolean newState = INSTANCE.getValue();
+    LOG.info("✅ [toggleHiddenComponents] Toggling hidden components for screen: " + currentFormTitle + " to " + newState);
+
+    screenCheckboxState.put(currentFormTitle, newState);
+    Ode.getCurrentProjectEditor().setScreenCheckboxState(currentFormTitle, newState);
+
+    LOG.info("📌 [toggleHiddenComponents] Checkbox state updated: " + currentFormTitle + " = " + newState);
+
+    for (MockForm form : formInstances.values()) {
+      LOG.info("🔄 Refreshing screen: " + form.getTitle());
+      for (MockComponent child : form.getChildren()) {
+        LOG.info("🔄 Updating visibility for component: " + child.getName());
+        child.updateVisibility();
+      }
+      form.doRefresh();
+    }
+  }
+
+  // ✅ Fixed: Define `setVisibility(boolean)`
   public static void setVisibility(boolean visible) {
     checkInstance();
     INSTANCE.setVisible(visible);
   }
 
-  // TODO: This should not require navigating through Ode
-  public static void toggleHiddenComponents(ClickEvent e) {
-    LOG.info("Received checkbox click");
-    if (INSTANCE.form != null) {
-      LOG.info("Updating hidden components");
-      Ode.getCurrentProjectEditor().setScreenCheckboxState(INSTANCE.form.getTitle(), INSTANCE.getValue());
-      INSTANCE.form.doRefresh();
+  private static void checkInstance() {
+    if (INSTANCE == null) {
+      INSTANCE = new HiddenComponentsCheckbox();
     }
   }
 }
