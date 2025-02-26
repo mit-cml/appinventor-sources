b35f301fe140d53524908d9b73559b4cd5f14396
patryk84a <62844519+patryk84a@users.noreply.github.com>
2025-02-26 22:24:48 +0100

    Adapting Mock ListView to new ListView properties. (#3280)

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockComponentsUtil.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockComponentsUtil.java
@@ -1,6 +1,6 @@
 // -*- mode: java; c-basic-offset: 2; -*-
 // Copyright 2009-2011 Google, All Rights reserved
-// Copyright 2011-2020 MIT, All rights reserved
+// Copyright 2011-2024 MIT, All rights reserved
 // Released under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 
@@ -15,15 +15,16 @@ import com.google.appinventor.shared.rpc.project.youngandroid.YoungAndroidAssets
 import com.google.appinventor.shared.rpc.project.youngandroid.YoungAndroidProjectNode;
 import com.google.appinventor.shared.storage.StorageUtil;
 import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.Style.FontStyle;
+import com.google.gwt.dom.client.Style.FontWeight;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.dom.client.Style.WhiteSpace;
 import com.google.gwt.dom.client.StyleElement;
-import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.widgetideas.graphics.client.Color;
-
-import static com.google.appinventor.client.Ode.MESSAGES;
 
 /**
  * Helper methods for working with mock components.
@@ -43,9 +44,9 @@ public final class MockComponentsUtil {
    */
   static void setWidgetBackgroundColor(Widget widget, String color) {
     if (isNoneColor(color)) {
-      DOM.setStyleAttribute(widget.getElement(), "backgroundColor", "transparent");
+      widget.getElement().getStyle().setBackgroundColor("transparent");
     } else {
-      DOM.setStyleAttribute(widget.getElement(), "backgroundColor", "#" + getHexString(color, 6));
+      widget.getElement().getStyle().setBackgroundColor("#" + getAlphaHexString(color));
     }
   }
 
@@ -69,13 +70,13 @@ public final class MockComponentsUtil {
    */
   static void setWidgetBackgroundImage(Widget widget, String image) {
     if (image.isEmpty()) {
-      DOM.setStyleAttribute(widget.getElement(), "backgroundImage", "none");
+      widget.getElement().getStyle().setBackgroundImage("none");
     } else {
-      DOM.setStyleAttribute(widget.getElement(), "backgroundImage", "url(" + image + ')');
+      widget.getElement().getStyle().setBackgroundImage("url(" + image + ')');
     }
-    DOM.setStyleAttribute(widget.getElement(), "backgroundRepeat", "no-repeat");
-    DOM.setStyleAttribute(widget.getElement(), "backgroundPosition", "center");
-    DOM.setStyleAttribute(widget.getElement(), "backgroundSize", "100% 100%");
+    widget.getElement().getStyle().setProperty("backgroundRepeat", "no-repeat");
+    widget.getElement().getStyle().setProperty("backgroundPosition", "center");
+    widget.getElement().getStyle().setProperty("backgroundSize", "100% 100%");
   }
 
   /**
@@ -121,8 +122,10 @@ public final class MockComponentsUtil {
    * @param value  {@code true} for bold font and {@code false} for normal font
    */
   static void setWidgetFontBold(Widget widget, String value) {
-    DOM.setStyleAttribute(widget.getElement(), "fontWeight",
-        Boolean.parseBoolean(value) ? "bold" : "normal");
+    widget
+        .getElement()
+        .getStyle()
+        .setFontWeight(Boolean.parseBoolean(value) ? FontWeight.BOLD : FontWeight.NORMAL);
   }
 
   /**
@@ -133,9 +136,9 @@ public final class MockComponentsUtil {
    */
   static void setWidgetTextColor(Widget widget, String color) {
     if (isNoneColor(color)) {
-      DOM.setStyleAttribute(widget.getElement(), "color", "transparent");
+      widget.getElement().getStyle().setColor("transparent");
     } else {
-      DOM.setStyleAttribute(widget.getElement(), "color", "#" + getHexString(color, 6));
+      widget.getElement().getStyle().setColor("#" + getAlphaHexString(color));
     }
   }
 
@@ -158,8 +161,10 @@ public final class MockComponentsUtil {
    * @param value  {@code true} for italic font and {@code false} for normal font
    */
   static void setWidgetFontItalic(Widget widget, String value) {
-    DOM.setStyleAttribute(widget.getElement(), "fontStyle",
-        Boolean.parseBoolean(value) ? "italic" : "normal");
+    widget
+        .getElement()
+        .getStyle()
+        .setFontStyle(Boolean.parseBoolean(value) ? FontStyle.ITALIC : FontStyle.NORMAL);
   }
 
   /**
@@ -171,8 +176,7 @@ public final class MockComponentsUtil {
   static void setWidgetFontSize(Widget widget, String size) {
     // Fonts on Android are in scaled pixels...
     try {
-      DOM.setStyleAttribute(widget.getElement(), "fontSize",
-          (int)(Float.parseFloat(size) * 0.9) + "px");
+      widget.getElement().getStyle().setFontSize((int) (Float.parseFloat(size) * 0.9), Unit.PX);
     } catch (NumberFormatException e) {
       // Ignore this. If we throw an exception here, the project is unrecoverable.
     }
@@ -189,7 +193,7 @@ public final class MockComponentsUtil {
     Project project = Ode.getInstance().getProjectManager().getProject(editor.getProjectId());
     if (project != null) {
       HasAssetsFolder<YoungAndroidAssetsFolder> hasAssetsFolder =
-        (YoungAndroidProjectNode) project.getRootNode();
+          (YoungAndroidProjectNode) project.getRootNode();
       for (ProjectNode asset : hasAssetsFolder.getAssetsFolder().getChildren()) {
         if (asset.getName().equals(name)) {
           return asset;
@@ -223,7 +227,7 @@ public final class MockComponentsUtil {
    * @param fontResourceURL font resource url
    * @param resourceId      uniq ID of font resource.
    */
-  static void createFontResource(String fontFamily, String fontResourceURL, String resourceId)  {
+  static void createFontResource(String fontFamily, String fontResourceURL, String resourceId) {
     StyleElement resourceElement = Document.get().createStyleElement();
     resourceElement.setId(resourceId);
     String resource = "@font-face {";
@@ -245,7 +249,7 @@ public final class MockComponentsUtil {
    */
   static void setWidgetFontTypeface(SimpleEditor editor, Widget widget, String typeface) {
     String fontFamily = "";
-    if (typeface.equals("0") || typeface.equals("1")){
+    if (typeface.equals("0") || typeface.equals("1")) {
       fontFamily = "sans-serif";
     } else if (typeface.equals("2")) {
       fontFamily = "serif";
@@ -255,11 +259,11 @@ public final class MockComponentsUtil {
       fontFamily = typeface.substring(0, typeface.lastIndexOf("."));
       String resourceID = typeface.toLowerCase().substring(0, typeface.lastIndexOf("."));
       String resourceURL = convertFontPropertyValueToUrl(editor, typeface);
-      if (Document.get().getElementById(resourceID) == null)  {
+      if (Document.get().getElementById(resourceID) == null) {
         createFontResource(fontFamily, resourceURL, resourceID);
       }
     }
-    DOM.setStyleAttribute(widget.getElement(), "fontFamily", fontFamily);
+    widget.getElement().getStyle().setProperty("fontFamily", fontFamily);
   }
 
   /**
@@ -271,10 +275,10 @@ public final class MockComponentsUtil {
   static void updateTextAppearances(Widget widget, String width) {
     if (width.equals("-1")) {
       // for width = Automatic
-      DOM.setStyleAttribute(widget.getElement(), "whiteSpace", "nowrap");
+      widget.getElement().getStyle().setWhiteSpace(WhiteSpace.NOWRAP);
     } else {
       // for width = Fill Parent, Pixels or Percentage
-      DOM.setStyleAttribute(widget.getElement(), "whiteSpace", "normal");
+      widget.getElement().getStyle().setWhiteSpace(WhiteSpace.NORMAL);
     }
   }
 
@@ -302,7 +306,7 @@ public final class MockComponentsUtil {
         align = "right";
         break;
     }
-    DOM.setStyleAttribute(widget.getElement(), "textAlign", align);
+    widget.getElement().getStyle().setProperty("textAlign", align);
   }
 
   /**
@@ -352,6 +356,20 @@ public final class MockComponentsUtil {
   }
 
   /*
+   * Converts the hex string representing the color &HAARRGGBB to a hex color in the format RRGGBBAA
+   */
+  static String getAlphaHexString(String color) {
+    color = color.startsWith("&H") ? color.substring(2) : Long.toHexString(Long.parseLong(color));
+    int len = color.length();
+    if (len < 8) {
+      do {
+        color = 'F' + color;
+      } while (++len < 8);
+    }
+    return color.substring(2) + color.substring(0, 2);
+  }
+
+  /*
    * Retrieves the size style attributes of the given widgets and then clears
    * them.
    *
@@ -364,19 +382,19 @@ public final class MockComponentsUtil {
   }
 
   static String[] clearSizeStyle(Element element) {
-    String widthStyle = DOM.getStyleAttribute(element, "width");
-    String heightStyle = DOM.getStyleAttribute(element, "height");
-    String lineHeightStyle = DOM.getStyleAttribute(element, "lineHeight");
+    String widthStyle = element.getStyle().getWidth();
+    String heightStyle = element.getStyle().getHeight();
+    String lineHeightStyle = element.getStyle().getLineHeight();
     if (widthStyle != null) {
-      DOM.setStyleAttribute(element, "width", null);
+      element.getStyle().clearWidth();
     }
     if (heightStyle != null) {
-      DOM.setStyleAttribute(element, "height", null);
+      element.getStyle().clearHeight();
     }
     if (lineHeightStyle != null) {
-      DOM.setStyleAttribute(element, "lineHeight", "initial");
+      element.getStyle().setProperty("lineHeight", "initial");
     }
-    return new String[] { widthStyle, heightStyle, lineHeightStyle };
+    return new String[] {widthStyle, heightStyle, lineHeightStyle};
   }
 
   /*
@@ -392,13 +410,13 @@ public final class MockComponentsUtil {
 
   static void restoreSizeStyle(Element element, String[] style) {
     if (style[0] != null) {
-      DOM.setStyleAttribute(element, "width", style[0]);
+      element.getStyle().setProperty("width", style[0]);
     }
     if (style[1] != null) {
-      DOM.setStyleAttribute(element, "height", style[1]);
+      element.getStyle().setProperty("width", style[1]);
     }
     if (style[2] != null) {
-      DOM.setStyleAttribute(element, "lineHeight", style[2]);
+      element.getStyle().setProperty("width", style[2]);
     }
   }
 
