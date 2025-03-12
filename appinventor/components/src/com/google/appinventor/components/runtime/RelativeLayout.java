85b218c99d92f1f097cf44741b2e2cb3e26a99b8
Evan W. Patton <ewpatton@mit.edu>
2025-03-12 18:21:53 -0400

    Remove errant SimpleObject annotation on RelativeLayout
    Change-Id: Ia9d3f64afa416229f02379103fada8cbd12f2551

--- appinventor/components/src/com/google/appinventor/components/runtime/RelativeLayout.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/RelativeLayout.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout.LayoutParams;
 
-import com.google.appinventor.components.annotations.SimpleObject;
 import com.google.appinventor.components.common.ComponentConstants;
 import com.google.appinventor.components.runtime.util.ViewUtil;
 import java.util.LinkedList;
@@ -22,7 +21,6 @@ import java.util.List;
  * layout. Closely follows the implementation of LinearLayout with certain
  * changes.
  */
-@SimpleObject
 public class RelativeLayout implements Layout {
 
   private final android.widget.RelativeLayout layoutManager;
