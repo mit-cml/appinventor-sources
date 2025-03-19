8de02f8d962800c19ae7cbbb46d85e4bd904c2ca
Evan W. Patton <ewpatton@mit.edu>
2025-03-19 10:27:47 -0400

    Implement editor refactor from IOT/Alexa repository
    Change-Id: Ifce1edb840171f05a210b42034a9bbf9ff87c48c

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/palette/SimplePalettePanel.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/palette/SimplePalettePanel.java
@@ -1,12 +1,13 @@
 // -*- mode: java; c-basic-offset: 2; -*-
 // Copyright 2009-2011 Google, All Rights reserved
-// Copyright 2011-2012 MIT, All rights reserved
+// Copyright 2011-2017 MIT, All rights reserved
 // Released under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 
 package com.google.appinventor.client.editor.simple.palette;
 
 import com.google.appinventor.client.editor.simple.components.MockComponent;
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  * An interface that a palette can implement in order to support showing Simple
@@ -35,4 +36,22 @@ public interface SimplePalettePanel {
   void clearComponents();
 
   void reloadComponents();
+
+  /**
+   * Get the widget that visually represents the panel in the UI.
+   *
+   * @return the UI widget fot the palette
+   */
+  Widget getWidget();
+
+  /**
+   * Create an empty copy of same panel class. This is a utility method to have multiple palettes
+   * between the designer and blocks editors without needing to know the concrete types of the
+   * palettes.
+   *
+   * @return a new palette of the same type as the target of the method call
+   */
+  SimplePalettePanel copy();
+
+  MockComponent createMockComponent(String name, String type);
 }
