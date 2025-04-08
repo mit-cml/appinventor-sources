3f7217c3651e8a56cd70424da9d8e665733311c3
Priyansu <priyansu5577@gmail.com>
2025-04-08 11:17:45 +0530

    Fixed indentation in Spreadsheet.java

--- appinventor/components/src/com/google/appinventor/components/runtime/Spreadsheet.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/Spreadsheet.java
@@ -244,17 +244,17 @@ public class Spreadsheet extends AndroidNonvisibleComponent implements Component
 
   @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_STRING,
     defaultValue = "")
-@SimpleProperty(description = "The SpreadsheetID is part of the web address of the Google Sheets "
-+ "spreadsheet you want to use. The web address (URL) starts https://docs.google.com/spreadsheets . . . . "
-+ "Go to the spreadsheet in your browser and paste its web address here.")
-public void SpreadsheetID(String spreadsheetID) {
-  if (spreadsheetID.startsWith("https:")) {
+  @SimpleProperty(description = "The SpreadsheetID is part of the web address of the Google Sheets "
+  + "spreadsheet you want to use. The web address (URL) starts https://docs.google.com/spreadsheets . . . . "
+  + "Go to the spreadsheet in your browser and paste its web address here.")
+  public void SpreadsheetID(String spreadsheetID) {
+    if (spreadsheetID.startsWith("https:")) {
     // URL: https://docs.google.com/spreadsheets/d/<id>/edit#gid=0
-    String[] parts = spreadsheetID.substring(8).split("/");
-    spreadsheetID = parts[3];
-  }
-  this.spreadsheetID = spreadsheetID;
-}
+      String[] parts = spreadsheetID.substring(8).split("/");
+      spreadsheetID = parts[3];
+    }
+    this.spreadsheetID = spreadsheetID;
+  } 
 
   /**
    * Specifies the name of the application given when doing an API call.
