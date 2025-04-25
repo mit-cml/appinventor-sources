13113cc5a3ac50aab49c6097490cefdce109c5a6
Jeffrey I. Schiller <jis@mit.edu>
2025-04-24 20:23:49 -0400

    Update Companion Version to 2.75
    Change-Id: I73b39459fcd20087358280fbd9735655545e5f1c

--- appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
+++ appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
@@ -1713,12 +1713,12 @@ public class YaVersion {
 
   public static final String ACCEPTABLE_COMPANION_PACKAGE = "edu.mit.appinventor.aicompanion3";
 
-  public static final String PREFERRED_COMPANION = "2.74";
+  public static final String PREFERRED_COMPANION = "2.75";
   public static final String EMULATOR_UPDATE_URL = ""; // Should be an APK
   public static final String COMPANION_UPDATE_URL = "";
   public static final String COMPANION_UPDATE_URL1 = "";
   public static final String COMPANION_UPDATE_EMULATOR_URL = "";
-  public static final String [] ACCEPTABLE_COMPANIONS = { "2.74", "2.74u" };
+  public static final String [] ACCEPTABLE_COMPANIONS = { "2.75", "2.75u" };
 
   // Splash Screen Values
   public static final int SPLASH_SURVEY = 1;
