2d4100f8487848d239d6e08db7685fb8949353c8
Lucas Roman <lroman10@mit.edu>
2025-06-20 20:00:36 -0400

    changed console icon

--- appinventor/components/src/com/google/appinventor/components/runtime/AppInventorCompatActivity.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/AppInventorCompatActivity.java
@@ -218,9 +218,10 @@ public class AppInventorCompatActivity extends Activity implements AppCompatCall
   }
 
   public static boolean isEmulator() {
-     return android.os.Build.PRODUCT.contains("google_sdk") ||  // Old emulator build (2.x)
-         android.os.Build.PRODUCT.equals("sdk") ||              // Honeycomb image (for testing)
-         android.os.Build.PRODUCT.contains("sdk_gphone");       // New emulator build (3.x)
+    //  return android.os.Build.PRODUCT.contains("google_sdk") ||  // Old emulator build (2.x)
+    //      android.os.Build.PRODUCT.equals("sdk") ||              // Honeycomb image (for testing)
+    //      android.os.Build.PRODUCT.contains("sdk_gphone");       // New emulator build (3.x)
+    return false;
   }
 
   @SuppressWarnings("unused") // Potentially useful for extensions adding custom activities
