a049b4f0729656e4bf9d66fa611f80ad1bf68e32
Diego Barreiro Perez <diego@barreiro.dev>
2025-07-14 20:58:52 +0100

    Fix YalEvalTest

--- appinventor/buildserver/tests/com/google/appinventor/buildserver/YailEvalTest.java
+++ appinventor/buildserver/tests/com/google/appinventor/buildserver/YailEvalTest.java
@@ -1458,6 +1458,8 @@ public class YailEvalTest extends TestCase {
       public HandlesEventDispatching getDispatchDelegate() {
         return null;
       }
+      @Override
+      public void setComponentName(final String componentName) {}
     };
     scheme.define(sym, fakeComponent);
     String code = readTestCode("testComponentsAsDictKeys");
