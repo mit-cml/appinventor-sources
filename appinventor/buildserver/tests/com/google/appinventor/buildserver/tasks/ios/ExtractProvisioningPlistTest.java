e967da9f80a7a5c8d7586a5e83089fc8d69a6136
Evan W. Patton <ewpatton@mit.edu>
2025-07-25 17:31:53 -0400

    Fix failing provisioning profile tests
    Change-Id: Icf2a0b9094d3c784d4e513224dc45bd1f17a9c64

--- appinventor/buildserver/tests/com/google/appinventor/buildserver/tasks/ios/ExtractProvisioningPlistTest.java
+++ appinventor/buildserver/tests/com/google/appinventor/buildserver/tasks/ios/ExtractProvisioningPlistTest.java
@@ -6,7 +6,9 @@
 package com.google.appinventor.buildserver.tasks.ios;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import com.google.appinventor.buildserver.TaskResult;
 import com.google.appinventor.buildserver.context.IosCompilerContext;
 import com.google.appinventor.common.testutils.TestUtils;
 import java.io.File;
@@ -22,9 +24,15 @@ public class ExtractProvisioningPlistTest {
         return "ipa";
       }
     };
-    context.getPaths().setAssetsDir(new File(TestUtils.APP_INVENTOR_ROOT_DIR + "/buildserver/tests/com/google/appinventor/buildserver/tasks/ios"));
+    File appDir = new File(TestUtils.APP_INVENTOR_ROOT_DIR + "/buildserver/build/tests/");
+    if (!appDir.exists()) {
+      assertTrue(appDir.mkdirs());
+    }
+    context.getPaths().setAssetsDir(new File(TestUtils.APP_INVENTOR_ROOT_DIR + "/buildserver/tests/com/google/appinventor/buildserver/tasks/ios/"));
+    context.getPaths().setAppDir(appDir);
     ExtractProvisioningPlist task = new ExtractProvisioningPlist();
-    task.execute(context);
+    TaskResult result = task.execute(context);
+    assertTrue(result.isSuccess());
     String sha1 = context.getCertificateSignature();
     assertEquals("728165C584BBCF2A62A1BD2600C7E4AC4A45EC09", sha1);
   }
@@ -37,9 +45,15 @@ public class ExtractProvisioningPlistTest {
         return "ips";
       }
     };
-    context.getPaths().setAssetsDir(new File(TestUtils.APP_INVENTOR_ROOT_DIR + "/buildserver/tests/com/google/appinventor/buildserver/tasks/ios"));
+    File appDir = new File(TestUtils.APP_INVENTOR_ROOT_DIR + "/buildserver/build/tests/");
+    if (!appDir.exists()) {
+      assertTrue(appDir.mkdirs());
+    }
+    context.getPaths().setAssetsDir(new File(TestUtils.APP_INVENTOR_ROOT_DIR + "/buildserver/tests/com/google/appinventor/buildserver/tasks/ios/"));
+    context.getPaths().setAppDir(appDir);
     ExtractProvisioningPlist task = new ExtractProvisioningPlist();
-    task.execute(context);
+    TaskResult result = task.execute(context);
+    assertTrue(result.isSuccess());
     String sha1 = context.getCertificateSignature();
     assertEquals("728165C584BBCF2A62A1BD2600C7E4AC4A45EC09", sha1);
   }
