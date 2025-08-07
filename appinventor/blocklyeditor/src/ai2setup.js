2be8efecb3e724329f915b042dec43430ee5d948
josmas <josmasflores@gmail.com>
2025-08-07 15:27:29 +0100

    generators and changes to serialisation with added tests

--- appinventor/blocklyeditor/src/ai2setup.js
+++ appinventor/blocklyeditor/src/ai2setup.js
@@ -6,6 +6,12 @@
 top.Blockly = Blockly;
 top.LexicalVariablesPlugin = LexicalVariablesPlugin;
 
+// Compatibility shim for Blockly v11: expose inputTypes directly on Blockly for legacy AI2 code
+// TODO: might not be needed anymore after changing it in a number of other places
+if (!Blockly.inputTypes && Blockly.inputs && Blockly.inputs.inputTypes) {
+  Blockly.inputTypes = Blockly.inputs.inputTypes;
+}
+
 // Install the translations from AI2.Msg into Blockly.Msg
 Object.keys(top.AI2.Msg).forEach(function (key) {
   Blockly.Msg[key] = top.AI2.Msg[key];
