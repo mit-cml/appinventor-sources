d46626e905da1f39fb4e32a9aee19872767dd95b
Diego Barreiro Perez <diego@barreiro.dev>
2025-08-13 16:48:38 +0100

    Reduce calls to Datastore to assert ownership
    Leverage memcache to store the project ownership, rather than going to Datastore for each single assertion.

--- appinventor/appengine/src/com/google/appinventor/server/storage/ObjectifyStorageIo.java
+++ appinventor/appengine/src/com/google/appinventor/server/storage/ObjectifyStorageIo.java
@@ -135,6 +135,9 @@ public class ObjectifyStorageIo implements StorageIo {
   private static final String GCS_BUCKET_NAME;
   private static final String APK_BUCKET_NAME;
 
+  private static final String BUILD_STATUS_CACHE_KEY_PREFIX = "40bae275-070f-478b-9a5f-d50361809b99";
+  private static final String PROJECT_OWNER_CACHE_KEY_PREFIX = "cf452c52-839a-48e2-a3fc-ef77c87e09c2";
+
   private static final long TWENTYFOURHOURS = 24*3600*1000; // 24 hours in milliseconds
 
   private static final boolean DEBUG = Flag.createFlag("appinventor.debugging", false).get();
@@ -2675,15 +2678,13 @@ public class ObjectifyStorageIo implements StorageIo {
 
   @Override
   public void storeBuildStatus(String userId, long projectId, int progress) {
-    String prelim = "40bae275-070f-478b-9a5f-d50361809b99";
-    String cacheKey = prelim + userId + projectId;
+    final String cacheKey = BUILD_STATUS_CACHE_KEY_PREFIX + userId + projectId;
     memcache.put(cacheKey, progress);
   }
 
   @Override
   public int getBuildStatus(String userId, long projectId) {
-    String prelim = "40bae275-070f-478b-9a5f-d50361809b99";
-    String cacheKey = prelim + userId + projectId;
+    final String cacheKey = BUILD_STATUS_CACHE_KEY_PREFIX + userId + projectId;
     Integer ival = (Integer) memcache.get(cacheKey);
     if (ival == null) {         // not in memcache (or memcache service down)
       return 50;
@@ -2694,6 +2695,20 @@ public class ObjectifyStorageIo implements StorageIo {
 
   @Override
   public void assertUserHasProject(final String userId, final long projectId) {
+    final String cacheKey = PROJECT_OWNER_CACHE_KEY_PREFIX + "|" + projectId;
+    final String ownerUserId = (String) memcache.get(cacheKey);
+
+    if (ownerUserId != null) {
+      if (ownerUserId.equals(userId)) {
+        // The user in the cache owns the project, hence we don't need to throw anything
+        return;
+      }
+      // Whoops, it seems like someone is being sneaky :)
+      throw new SecurityException("Unauthorized access");
+    }
+
+    // Now, if the cache does not contain the project, we will fallback to datastore as
+    //   source of truth...
     try {
       runJobWithRetries(new JobRetryHelper() {
         @SuppressWarnings("RedundantThrows")
@@ -2706,6 +2721,9 @@ public class ObjectifyStorageIo implements StorageIo {
             throw new SecurityException("Unauthorized access");
           }
           // User has data for project, so everything checks out.
+          // We just store it now in the cache for future access, as we know the user requesting
+          //   this project owns it.
+          memcache.put(cacheKey, userId);
         }
       }, false);
     } catch(ObjectifyException e) {
