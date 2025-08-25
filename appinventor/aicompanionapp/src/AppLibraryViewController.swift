2f35851163d4219946affd39bb847bb8442a9067
Evan W. Patton <ewpatton@mit.edu>
2025-08-25 13:52:06 -0400

    Fix copyright preambles
    Change-Id: I389fe4cdea823e94402cc9cf5ef15f34cdc718f4

--- appinventor/aicompanionapp/src/AppLibraryViewController.swift
+++ appinventor/aicompanionapp/src/AppLibraryViewController.swift
@@ -1,10 +1,7 @@
-//
-//  AppLibraryViewController.swift
-//  AICompanionApp
-//
-//  Created by Mark Razanau on 4/8/25.
-//  Copyright © 2025 Massachusetts Institute of Technology. All rights reserved.
-//
+// -*- mode: swift; swift-mode:basic-offset: 2; -*-
+// Copyright 2025 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 
 import UIKit
 import AIComponentKit
