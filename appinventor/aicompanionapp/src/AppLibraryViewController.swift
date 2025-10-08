9352d4f780939edc432c9c9986d7b068690b7e9e
Evan W. Patton <ewpatton@mit.edu>
2025-10-08 09:48:40 -0400

    Fixes to the iOS app library (#3602)
    This fixes issues with multiple screens in playback of apps in the library.
    
    Co-authored-by: cb <cindyb@mit.edu>

--- appinventor/aicompanionapp/src/AppLibraryViewController.swift
+++ appinventor/aicompanionapp/src/AppLibraryViewController.swift
@@ -388,8 +388,11 @@ class AppLibraryViewController: UIViewController, UITableViewDelegate, UITableVi
       .appendingPathComponent("samples/\(app.title).aia", isDirectory: false))
     newapp.makeCurrent()
     newapp.loadScreen1(self.form)
-    self.navigationController?.popViewController(animated: true)
-    (self.navigationController as? ViewController)?.showMenuButton()
+    guard let vc = self.navigationController as? ViewController else {
+      return
+    }
+    self.navigationController?.popViewController(animated: false)
+    vc.showMenuButton()
   }
   
   func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
