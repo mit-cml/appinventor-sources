josmas/app-inventor

generators and changes to serialisation with added tests

appinventor/appengine/war/index.jsp
@@ -108,14 +108,29 @@
     <script src="static/js/tutorial.js"></script>
     <script type="text/javascript" src="static/closure-library/closure/goog/base.js"></script>
     <script type="text/javascript" src="<%= odeBase %>ode/aiblockly-@blocklyeditor_BlocklyChecksum@.js"></script>
-    <script type="text/javascript" src="static/js/scroll-options-5.0.11.min.js"></script>
+<%--    FIXME: (v11 upgrade) set plugins back --%>
+<%--    <script type="text/javascript" src="static/js/scroll-options-5.0.11.min.js"></script>--%>
     <script type="text/javascript" src="static/js/workspace-search.min.js"></script>
     <script type="text/javascript" src="static/js/block-dynamic-connection-0.6.0.min.js"></script>
-    <script type="text/javascript" src="static/js/workspace-multiselect-0.1.14-beta1.min.js"></script>
+<%--    <script type="text/javascript" src="static/js/workspace-multiselect-0.1.14-beta1.min.js"></script>--%>
     <script type="text/javascript" src="static/js/keyboard-navigation-0.5.13.min.js"></script>
     <script type="text/javascript" src="<%= odeBase %>ode/cdnok.js"></script>
     <script type="text/javascript" src="static/js/jszip.min.js"></script>
     <script type="text/javascript" src="<%= odeBase %>ode/ode.nocache.js"></script>
+    <script type="text/javascript" src="static/js/blockly-field-colour-6.0.4.min.js"></script>
+    <script type="text/javascript">
+      // Register FieldColour plugin for Blockly v11 compatibility
+      if (typeof window.registerFieldColour === 'function') {
+        window.registerFieldColour();
+      } else if (typeof registerFieldColour === 'function') {
+        registerFieldColour();
+      }
+
+      // Make FieldColour available as Blockly.FieldColour
+      if (typeof window.FieldColour === 'function') {
+        Blockly.FieldColour = window.FieldColour;
+      }
+    </script>
     <script src="static/leaflet/leaflet.js"></script>
     <script src="static/leaflet/leaflet.toolbar.js"></script>
     <script src="static/leaflet/leaflet-vector-markers.min.js"></script>

mit-cml/appinventor-sources

appinventor/appengine/war/index.jsp
@@ -109,15 +109,30 @@
     <script src="static/js/tutorial.js"></script>
     <script type="text/javascript" src="static/closure-library/closure/goog/base.js"></script>
     <script type="text/javascript" src="<%= odeBase %>ode/aiblockly-@blocklyeditor_BlocklyChecksum@.js"></script>
-    <script type="text/javascript" src="static/js/scroll-options-5.0.11.min.js"></script>
+<%--    FIXME: (v11 upgrade) set plugins back --%>
+<%--    <script type="text/javascript" src="static/js/scroll-options-5.0.11.min.js"></script>--%>
     <script type="text/javascript" src="static/js/workspace-search.min.js"></script>
     <script type="text/javascript" src="static/js/block-dynamic-connection-0.6.0.min.js"></script>
     <script type="text/javascript" src="static/js/pickr.min.js"></script>
-    <script type="text/javascript" src="static/js/workspace-multiselect-0.1.14-beta2.min.js"></script>
+<%--    <script type="text/javascript" src="static/js/workspace-multiselect-0.1.14-beta2.min.js"></script>--%>
     <script type="text/javascript" src="static/js/keyboard-navigation-0.5.13.min.js"></script>
     <script type="text/javascript" src="<%= odeBase %>ode/cdnok.js"></script>
     <script type="text/javascript" src="static/js/jszip.min.js"></script>
     <script type="text/javascript" src="<%= odeBase %>ode/ode.nocache.js"></script>
+    <script type="text/javascript" src="static/js/blockly-field-colour-6.0.4.min.js"></script>
+    <script type="text/javascript">
+      // Register FieldColour plugin for Blockly v11 compatibility
+      if (typeof window.registerFieldColour === 'function') {
+        window.registerFieldColour();
+      } else if (typeof registerFieldColour === 'function') {
+        registerFieldColour();
+      }
+
+      // Make FieldColour available as Blockly.FieldColour
+      if (typeof window.FieldColour === 'function') {
+        Blockly.FieldColour = window.FieldColour;
+      }
+    </script>
     <script src="static/leaflet/leaflet.js"></script>
     <script src="static/leaflet/leaflet.toolbar.js"></script>
     <script src="static/leaflet/leaflet-vector-markers.min.js"></script>