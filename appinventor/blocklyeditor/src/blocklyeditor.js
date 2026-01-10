653e2e276b9417dfa034f1cbd88acb7a415d77e1
Michael Gallagher <mjgallag@gmail.com>
2026-01-09 19:10:11 -0800

    Blockly 12 renamed setEnabled to setDisabledReason

--- appinventor/blocklyeditor/src/blocklyeditor.js
+++ appinventor/blocklyeditor/src/blocklyeditor.js
@@ -693,7 +693,7 @@ AI.Blockly.ContextMenuItems.registerEnableDisableAllBlocksOption = function() {
       let allBlocks = scope.workspace.getAllBlocks();
       try {
         Blockly.Events.setGroup(true);
-        allBlocks.forEach(block => block.setEnabled(true));
+        allBlocks.forEach(block => block.setDisabledReason(false, Blockly.constants.MANUALLY_DISABLED));
       } finally {
         Blockly.Events.setGroup(false);
       }
@@ -714,7 +714,7 @@ AI.Blockly.ContextMenuItems.registerEnableDisableAllBlocksOption = function() {
       let allBlocks = scope.workspace.getAllBlocks();
       try {
         Blockly.Events.setGroup(true);
-        allBlocks.forEach(block => block.setEnabled(false));
+        allBlocks.forEach(block => block.setDisabledReason(true, Blockly.constants.MANUALLY_DISABLED));
       } finally {
         Blockly.Events.setGroup(false);
       }
