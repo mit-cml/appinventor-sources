1408f491950ded4b203c14db229ba46bd4740a16
Jewel Shikder Jony <jewelshkjony@gmail.com>
2026-03-08 19:27:53 +0600

    Add comments

--- appinventor/buildserver/src/com/google/appinventor/buildserver/tasks/android/RunL8.java
+++ appinventor/buildserver/src/com/google/appinventor/buildserver/tasks/android/RunL8.java
@@ -1,3 +1,8 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2026 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0\
+
 package com.google.appinventor.buildserver.tasks.android;
 
 import java.io.File;
@@ -13,6 +18,9 @@ import java.util.List;
 import com.google.appinventor.buildserver.context.AndroidCompilerContext;
 import com.google.appinventor.buildserver.util.Execution;
 
+/**
+ * Runs L8 to dex the `desugar_jdk_libs` library and bundle it with Apk/Aab
+ */
 public class RunL8 {
 
   public boolean execute(AndroidCompilerContext context, Path configJson) throws IOException {
@@ -57,7 +65,7 @@ public class RunL8 {
         }
         Files.copy(classesDex, resolveNextClassesDex(context.getPaths().getTmpDir()));
 
-        // Clear caches
+        // Delete L8 relevant caches
         classesDex.toFile().delete();
         tempOutputDir.toFile().delete();
         return true;
@@ -69,7 +77,7 @@ public class RunL8 {
 
   private Path resolveNextClassesDex(File dir) {
     Path dirPath = dir.toPath();
-    // classes2.dex, classes3.dex, ... — find first gap
+    // classes2.dex, classes3.dex, classesN.dex ... — find first gap
     int index = 2;
     while (true) {
       Path candidate = dirPath.resolve("classes" + index + ".dex");
