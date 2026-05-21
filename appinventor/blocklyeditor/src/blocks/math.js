dbb332e254ba2bdfdd38310106b82f2578535229
Yash <yashteaches@gmail.com>
2026-05-21 23:23:03 +0530

    Update docs for compare with tolerance block
    - Add comparetolerance.png in images/math/
    - Fix typo in i18n string in messages.json
    - Reorder math_compare_tolerance block after math_compare block

--- appinventor/blocklyeditor/src/blocks/math.js
+++ appinventor/blocklyeditor/src/blocks/math.js
@@ -110,26 +110,6 @@ Blockly.Blocks.math_number_radix.RADIX = {
   'HEX': 16
 };
 
-Blockly.Blocks['math_compare_tolerance'] = {
-  category: 'Math',
-  helpUrl: Blockly.Msg.LAND_MATH_COMPARE_TOLERANCE_HELPURL,
-  init: function() {
-  	this.setColour(Blockly.MATH_CATEGORY_HUE);
-  	this.setOutput(true, AI.BlockUtils.YailTypeToBlocklyType("boolean", AI.BlockUtils.OUTPUT));
-  	this.appendValueInput('A')
-				.setCheck(AI.BlockUtils.YailTypeToBlocklyType("number", AI.BlockUtils.INPUT));
-		this.appendValueInput('B')
-				.setCheck(AI.BlockUtils.YailTypeToBlocklyType("number", AI.BlockUtils.INPUT))
-				.appendField(Blockly.Msg.LANG_MATH_COMPARE_TOLERANCE);
-		this.appendValueInput('TOL')
-				.setCheck(AI.BlockUtils.YailTypeToBlocklyType("number", AI.BlockUtils.INPUT))
-				.appendField(Blockly.Msg.LANG_MATH_COMPARE_TOLERANCE_TITLE);
-		this.setInputsInline(true);
-		this.setTooltip(Blockly.Msg.LANG_MATH_COMPARE_TOLERANCE_TOOLTIP);
-  },
-	typeblock: [{translatedName: Blockly.Msg.LANG_MATH_COMPARE_TOLERANCE_TITLE}]
-};
-
 Blockly.Blocks['math_compare'] = {
   // Basic arithmetic operator.
   // TODO(Andrew): equality block needs to have any on the sockets.
@@ -238,6 +218,26 @@ Blockly.Blocks.math_compare.HELPURLS = function () {
     GT: Blockly.Msg.LANG_MATH_COMPARE_HELPURL_GT,
     GTE: Blockly.Msg.LANG_MATH_COMPARE_HELPURL_GTE
   }
+};
+
+Blockly.Blocks['math_compare_tolerance'] = {
+  category: 'Math',
+  helpUrl: Blockly.Msg.LANG_MATH_COMPARE_TOLERANCE_HELPURL,
+  init: function() {
+  	this.setColour(Blockly.MATH_CATEGORY_HUE);
+  	this.setOutput(true, AI.BlockUtils.YailTypeToBlocklyType("boolean", AI.BlockUtils.OUTPUT));
+  	this.appendValueInput('A')
+				.setCheck(AI.BlockUtils.YailTypeToBlocklyType("number", AI.BlockUtils.INPUT));
+		this.appendValueInput('B')
+				.setCheck(AI.BlockUtils.YailTypeToBlocklyType("number", AI.BlockUtils.INPUT))
+				.appendField(Blockly.Msg.LANG_MATH_COMPARE_TOLERANCE);
+		this.appendValueInput('TOL')
+				.setCheck(AI.BlockUtils.YailTypeToBlocklyType("number", AI.BlockUtils.INPUT))
+				.appendField(Blockly.Msg.LANG_MATH_COMPARE_TOLERANCE_TITLE);
+		this.setInputsInline(true);
+		this.setTooltip(Blockly.Msg.LANG_MATH_COMPARE_TOLERANCE_TOOLTIP);
+  },
+	typeblock: [{translatedName: Blockly.Msg.LANG_MATH_COMPARE_TOLERANCE_TITLE}]
 };
 
 Blockly.Blocks['math_add'] = {
