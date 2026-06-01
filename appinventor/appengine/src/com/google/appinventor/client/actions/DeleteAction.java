a7e131d3e3decbfa2e7a9264fbb028d727b92699
Yash <yashteaches@gmail.com>
2026-06-01 18:30:01 +0530

    Remove redundant saveAllFolders call

--- appinventor/appengine/src/com/google/appinventor/client/actions/DeleteAction.java
+++ appinventor/appengine/src/com/google/appinventor/client/actions/DeleteAction.java
@@ -41,7 +41,6 @@ public class DeleteAction implements Command {
             if (deleteConfirmation(true, projectsToDelete, foldersToDelete)) {
               Ode.getInstance().getFolderManager().moveItemsToFolder(selectedProjects, selectedFolders,
                   Ode.getInstance().getFolderManager().getTrashFolder());
-              Ode.getInstance().getFolderManager().saveAllFolders();
             }
           } else {
             // The user can select a project to resolve the
